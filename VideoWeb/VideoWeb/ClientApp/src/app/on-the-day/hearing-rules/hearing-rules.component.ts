57bd2ce5dfb7ba092e70f80230c10da94501a4a7
VenkatRangala <VenkatRangala@gmail.com>
2019-03-20 10:13:22 +0000

    VIH-4037 hearing rules changes

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/hearing-rules/hearing-rules.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/hearing-rules/hearing-rules.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PageUrls } from 'src/app/shared/page-url.constants';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-hearing-rules',
   templateUrl: './hearing-rules.component.html'
 })
 export class HearingRulesComponent implements OnInit {
-  constructor(private router: Router) {
+  hearingRulesForm: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.hearingRulesForm = fb.group({
+      nextButton: new FormControl()
+    });
   }
 
   ngOnInit() {
