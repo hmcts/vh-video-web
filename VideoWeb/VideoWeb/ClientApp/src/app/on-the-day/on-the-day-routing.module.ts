b363ac75b1514b5325519deab5f8185c4c370732
VenkatRangala <VenkatRangala@gmail.com>
2019-03-22 11:39:09 +0000

    Lint space fix

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/on-the-day-routing.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/on-the-day-routing.module.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   { path: 'hearing-rules', component: HearingRulesComponent },
   { path: 'equipment-check', component: EquipmentCheckComponent },
   { path: 'camera-and-microphone', component: CameraAndMicrophoneComponent }
-  
+
 ];
 
 @NgModule({
