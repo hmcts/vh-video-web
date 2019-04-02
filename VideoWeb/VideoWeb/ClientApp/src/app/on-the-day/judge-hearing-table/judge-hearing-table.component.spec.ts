3e03cc9dab260dc1591f276fd37157d5afc0fc42
VenkatRangala <VenkatRangala@gmail.com>
2019-04-02 09:24:27 +0100

    Added Tests for Judge hearing list

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-table/judge-hearing-table.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-table/judge-hearing-table.component.spec.ts
@@ -1,7 +1,7 @@
 import { JudgeHearingTableComponent } from './judge-hearing-table.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConferenceTestData } from 'src/app/testing/mocks/data/conference-test-data';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import * as moment from 'moment';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
@@ -13,7 +13,7 @@ describe('JudgeHearingTableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [ReactiveFormsModule, FormsModule, RouterTestingModule],
       declarations: [JudgeHearingTableComponent]
     })
       .compileComponents();
--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/no-hearings/no-hearings.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/no-hearings/no-hearings.component.spec.ts
@@ -1,16 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NoHearingsComponent } from './no-hearings.component';
+import { DebugElement } from '@angular/core';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { PageUrls } from 'src/app/shared/page-url.constants';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 describe('NoHearingsComponent', () => {
   let component: NoHearingsComponent;
   let fixture: ComponentFixture<NoHearingsComponent>;
+  let debugElement: DebugElement;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NoHearingsComponent ]
+      declarations: [NoHearingsComponent],
+      imports: [ReactiveFormsModule, FormsModule, RouterTestingModule, SharedModule],
     })
     .compileComponents();
+
+    fixture = TestBed.createComponent(NoHearingsComponent);
+    debugElement = fixture.debugElement;
+    component = debugElement.componentInstance;
+    router = TestBed.get(Router);
   }));
 
   beforeEach(() => {
@@ -22,4 +36,5 @@ describe('NoHearingsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
