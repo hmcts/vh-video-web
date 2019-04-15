b59814b9e0c72eb8615abb5f15f0396a806ad5c3
Shaed Parkar <shaed.parkar@hmcts.net>
2019-04-15 15:30:38 +0100

    add test to ensure case number is displayed

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/contact-us-folding/contact-us-folding.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/contact-us-folding/contact-us-folding.component.spec.ts
@@ -5,17 +5,23 @@ import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { ConferenceTestData } from 'src/app/testing/mocks/data/conference-test-data';
 import { SharedModule } from '../shared.module';
 import { HttpClientModule } from '@angular/common/http';
+import { VideoWebService } from 'src/app/services/video-web.service';
+import { of } from 'rxjs';
 
 describe('ContactUsFoldingComponent', () => {
   let component: ContactUsFoldingComponent;
   let fixture: ComponentFixture<ContactUsFoldingComponent>;
   const conference = new ConferenceTestData().getConferenceDetail();
+  let videoWebServiceSpy: jasmine.SpyObj<VideoWebService>;
 
   beforeEach(async(() => {
+    videoWebServiceSpy = jasmine.createSpyObj<VideoWebService>('VideoWebService', ['getConferenceById']);
+
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
       declarations: [ContactUsFoldingComponent],
       providers: [
+        { provide: VideoWebService, useValue: videoWebServiceSpy },
         {
           provide: ActivatedRoute,
           useValue: {
@@ -32,10 +38,17 @@ describe('ContactUsFoldingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactUsFoldingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should return blank case number', () => {
+    videoWebServiceSpy.getConferenceById.and.returnValue(of(null));
+    component.ngOnInit();
+    expect(component.caseNumber).toBe('');
+  });
+
+  it('should return case number', () => {
+    videoWebServiceSpy.getConferenceById.and.returnValue(of(conference));
+    component.ngOnInit();
+    expect(component.caseNumber).toBe(conference.case_number);
   });
 });
