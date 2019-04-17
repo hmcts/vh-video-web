2342ee3b2d7e78a645408e4799973b502fb5996b
NPoxon <sendnickmessages@hotmail.com>
2019-04-17 18:33:38 +0200

    fixed locator change

--- VideoWeb/VideoWeb.AcceptanceTests/Pages/HearingListPage.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Pages/HearingListPage.cs
@@ -11,7 +11,7 @@ namespace VideoWeb.AcceptanceTests.Pages
         public By NoHearingsWarningMessage => CommonLocators.ElementContainingText("You have no video hearings");
         public By _hearingWithCaseNumber(string caseNumber) => CommonLocators.ElementContainingText(caseNumber);
         public By _waitToSignInText(string caseNumber) => By.XPath($"//tr//*[contains(text(),'{caseNumber}')]/../../td/p[contains(text(),'Sign in Today')]");
-        public By _signInButton(string caseNumber) => By.XPath($"//tr//*[contains(text(),'{caseNumber}')]/../../td/input[@role='button' and @value='Sign into hearing']");        
+        public By _signInButton(string caseNumber) => By.XPath($"//tr//*[contains(text(),'{caseNumber}')]/../../../td/input[@role='button' and @value='Sign into hearing']");        
         public By _startHearingButton(string caseNumber) => By.XPath($"//tr//*[contains(text(),'{caseNumber}')]/../../..//input[@role='button' and @value='Start hearing']");
 
         public By ParticipantHearingDate(string caseNumber) => By.XPath($"//strong[contains(text(),'{caseNumber}')]/../../td[contains(text(),'20')]");
