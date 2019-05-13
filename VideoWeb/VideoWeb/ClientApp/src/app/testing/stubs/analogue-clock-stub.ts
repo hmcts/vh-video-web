d448a434bff7a2a467de0d82e000d052af76ecc5
Shaed Parkar <shaed.parkar@hmcts.net>
2019-05-13 14:38:56 +0100

    add input to analogue stub to match component

--- VideoWeb/VideoWeb/ClientApp/src/app/testing/stubs/analogue-clock-stub.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/testing/stubs/analogue-clock-stub.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hearing } from 'src/app/waiting-space/models/hearing';
 
 @Component({ selector: 'app-analogue-clock', template: '' })
 export class AnalogueClockStubComponent {
+    @Input() hearing: Hearing;
 }
