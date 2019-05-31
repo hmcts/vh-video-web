21a0f6a3fb5eb31cff6c4e356fb3e24437a68d63
Shaed Parkar <shaed.parkar@hmcts.net>
2019-05-31 09:42:08 +0100

    fix duration as text

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/models/hearing.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/models/hearing.ts
@@ -33,10 +33,12 @@ export class Hearing {
         const m = duration % 60;
         const hours = h < 1 ? `${h} hours` : `${h} hour`;
         const minutes = m < 1 ? `${m} minute` : `${m} minutes`;
-        if (h > 0) {
-          return `${hours} and ${minutes}`;
+        if (h > 0 && m > 0) {
+            return `${hours} and ${minutes}`;
+        } else if (h > 0 && m === 0) {
+            return `${hours}`;
         } else {
-          return `${minutes}`;
+            return `${minutes}`;
         }
     }
 
