72858c3ac5a51f54e996fe81585e37f61a0a3e7a
muralikaturi <murali.katuri@hmcts.net>
2019-06-07 15:32:00 +0100

    Added token hash,expireson to the pexip call to selftest node

--- VideoWeb/VideoWeb/ClientApp/src/app/services/video-web.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/video-web.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiClient, ConferenceForUserResponse, ConferenceResponse, ConferenceEventRequest,
-  TaskResponse, AddMediaEventRequest, TestCallScoreResponse } from './clients/api-client';
+  TaskResponse, AddMediaEventRequest, TestCallScoreResponse, TokenResponse } from './clients/api-client';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -37,5 +37,9 @@ export class VideoWebService {
 
   getTestCallScore(conferenceId: string, participantId: string): Observable<TestCallScoreResponse> {
     return this.apiClient.getTestCallResult(conferenceId, participantId);
+  }
+
+  getToken(participantId: string): Observable<TokenResponse> {
+    return this.apiClient.getToken(participantId);
   }
 }
