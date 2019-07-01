16a84f27d66d2e884d460b4318b2494752fe9529
Lionel Roche <lionel.roche@hmcts.net>
2019-07-01 12:16:41 +0100

    added test for the mobile check.

--- VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.spec.ts
@@ -9,20 +9,24 @@ import { ProfileService } from '../services/api/profile.service';
 import { of } from 'rxjs';
 import { UserProfileResponse, UserRole } from '../services/clients/api-client';
 import { PageUrls } from '../shared/page-url.constants';
+import { NavigatorService } from '../services/navigator.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let router: Router;
   let profileServiceSpy: jasmine.SpyObj<ProfileService>;
+  let navigatorServiceSpy: jasmine.SpyObj<NavigatorService>;
 
   configureTestSuite(() => {
     profileServiceSpy = jasmine.createSpyObj<ProfileService>('ProfileService', ['getUserProfile']);
+    navigatorServiceSpy = jasmine.createSpyObj<NavigatorService>('NavigatorService', ['isDeviceComputer', 'navigatorDeviceInfo']);
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, SharedModule],
       declarations: [HomeComponent],
       providers: [
-        { provide: ProfileService, useValue: profileServiceSpy }
+        { provide: ProfileService, useValue: profileServiceSpy },
+        { provide: NavigatorService, useValue: navigatorServiceSpy }
       ]
     });
   });
@@ -37,6 +41,7 @@ describe('HomeComponent', () => {
   it('should go to judge hearing list', () => {
     const profile = new UserProfileResponse({ role: UserRole.Judge });
     profileServiceSpy.getUserProfile.and.returnValue(of(profile));
+    navigatorServiceSpy.isDeviceComputer.and.returnValue(true);
     fixture.detectChanges();
     expect(router.navigate).toHaveBeenCalledWith([PageUrls.JudgeHearingList]);
   });
@@ -44,6 +49,7 @@ describe('HomeComponent', () => {
   it('should go to admin hearing list', () => {
     const profile = new UserProfileResponse({ role: UserRole.VideoHearingsOfficer });
     profileServiceSpy.getUserProfile.and.returnValue(of(profile));
+    navigatorServiceSpy.isDeviceComputer.and.returnValue(true);
     fixture.detectChanges();
     expect(router.navigate).toHaveBeenCalledWith([PageUrls.AdminHearingList]);
   });
@@ -51,7 +57,16 @@ describe('HomeComponent', () => {
   it('should go to participant hearing list', () => {
     const profile = new UserProfileResponse({ role: UserRole.Representative });
     profileServiceSpy.getUserProfile.and.returnValue(of(profile));
+    navigatorServiceSpy.isDeviceComputer.and.returnValue(true);
     fixture.detectChanges();
     expect(router.navigate).toHaveBeenCalledWith([PageUrls.ParticipantHearingList]);
+  });
+
+  it('it should redirect to Signon-a-computer screen if on a mobile device', () => {
+    const profile = new UserProfileResponse({ role: UserRole.Representative });
+    profileServiceSpy.getUserProfile.and.returnValue(of(profile));
+    navigatorServiceSpy.isDeviceComputer.and.returnValue(false);
+    fixture.detectChanges();
+    expect(router.navigate).toHaveBeenCalledWith([PageUrls.SignonAComputer]);
   });
 });
--- VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.ts
@@ -15,7 +15,8 @@ export class HomeComponent implements OnInit {
   constructor(
     private router: Router,
     private profileService: ProfileService,
-    private errorService: ErrorService, private navigatorService: NavigatorService
+    private errorService: ErrorService, 
+    private navigatorService: NavigatorService
   ) {
   }
 
