6351888ac69ae4cba611d7f5cde4e9bd73ca828d
NPoxon <sendnickmessages@hotmail.com>
2019-07-16 14:59:04 +0100

    Updated Clerk Waiting Room to new layout. Fixed ClerkWaitingRoom test.

--- VideoWeb/Testing.Common/Helpers/DateFormats.cs
+++ VideoWeb/Testing.Common/Helpers/DateFormats.cs
@@ -6,7 +6,8 @@
         public const string ClerkHearingListTime = "HH:mm";
         public const string HearingListPageDate = "dd MMM yyyy";
         public const string WaitingRoomPageDate = "dddd dd MMMM yyyy";
-        public const string JudgeWaitingRoomPageTime = "HH:mm";
+        public const string ClerkWaitingRoomPageTime = "dd MMMM yyyy HH:mm tt";
+        public const string ClerkWaitingRoomPageTimeEnd = "HH:mm tt";
         public const string WaitingRoomPageTime = "HH:mm tt";
         public const string AlertMessageTimestamp = "HH:mm";
     }
