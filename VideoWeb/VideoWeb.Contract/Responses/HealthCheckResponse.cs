b92a30294fa196e8dcaf8ac94fb6ce65cfe3bd0c
Shaed Parkar <shaed.parkar@hmcts.net>
2019-07-16 18:59:27 +0100

    Add health endpoint checking connectivity to bookings, user and video api

---
+++ VideoWeb/VideoWeb.Contract/Responses/HealthCheckResponse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+
+namespace VideoWeb.Contract.Responses
+{
+    public class HealthCheckResponse
+    {
+        public HealthCheckResponse()
+        {
+            BookingsApiHealth = new HealthCheck();
+            UserApiHealth = new HealthCheck();
+            VideoApiHealth = new HealthCheck();
+        }
+        public HealthCheck BookingsApiHealth { get; set; }
+        public HealthCheck UserApiHealth { get; set; }
+        public HealthCheck VideoApiHealth { get; set; }
+
+    }
+
+    public class HealthCheck
+    {
+        public bool Successful { get; set; }
+        public string ErrorMessage { get; set; }
+        public IDictionary Data { get; set; }
+    }
+}
