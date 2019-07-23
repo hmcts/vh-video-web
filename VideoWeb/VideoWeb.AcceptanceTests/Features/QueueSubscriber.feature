b880f42880fdbf6bbb3baa6f7ff60eb704637c0b
NPoxon <sendnickmessages@hotmail.com>
2019-07-23 15:49:14 +0200

    Added feature files to begin writing queue subscriber tests

---
+++ VideoWeb/VideoWeb.AcceptanceTests/Features/QueueSubscriber.feature
@@ -0,0 +1,46 @@
+﻿@VIH-4256
+Feature: QueueSubscriber
+	In order to not duplicate data and keep the video-api in sync with the bookings-api
+	As a queue subscriber service
+	I want to be told of any changes required to sync
+
+Scenario: Hearing Created
+	Given I have a hearing
+	When I attempt to retrieve the new conference details from the video api
+	Then the conference has been created from the booking
+
+Scenario: Hearing Details Updated
+	Given I have a hearing and a conference
+	When I attempt to update the hearing details
+	Then the conference details have been updated
+
+Scenario: Hearing Cancelled
+	Given I have a hearing and a conference
+	When I attempt to cancel the hearing
+	Then the conference has been cancelled
+	
+@VIH-4702
+Scenario: Hearing Deleted
+	Given I have a hearing and a conference
+	When I attempt to delete the hearing
+	Then the conference has been deleted 
+
+Scenario: Hearing Is Ready For Video Updates the main hearing details
+	Given I have a hearing and a conference
+	When I attempt to update the hearing details
+	Then the conference details have been updated
+
+Scenario: Participant Added
+	Given I have a hearing and a conference
+	When I add a participant from the hearing
+	Then the participant details have been updated
+
+Scenario: Participant Updated
+	Given I have a hearing and a conference
+	When I update a participant from the hearing
+	Then the participant details have been updated
+
+Scenario: Participant Removed
+	Given I have a hearing and a conference
+	When I remove a participant from the hearing
+	Then the participant details have been updated
---
+++ VideoWeb/VideoWeb.AcceptanceTests/Steps/QueueSubscriberSteps.cs
@@ -0,0 +1,64 @@
+﻿using TechTalk.SpecFlow;
+using VideoWeb.AcceptanceTests.Contexts;
+
+namespace VideoWeb.AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class QueueSubscriberSteps
+    {
+        private readonly TestContext _context;
+
+        public QueueSubscriberSteps(TestContext context)
+        {
+            _context = context;
+        }
+
+        [When(@"I attempt to update the hearing details")]
+        public void WhenIAttemptToUpdateTheHearingDetails()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I attempt to (.*) the hearing")]
+        public void WhenIAttemptToCancelTheHearing(string action)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I (.*) a participant from the hearing")]
+        public void WhenIAttemptToAddAParticipantToTheHearing(string action)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the conference has been created from the booking")]
+        public void ThenTheConferenceHasBeenCreatedFromTheBooking()
+        {
+            ScenarioContext.Current.Pending();
+        }      
+
+        [Then(@"the conference details have been updated")]
+        public void ThenTheConferenceDetailsHaveBeenUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the conference has been cancelled")]
+        public void ThenTheConferenceHasBeenCancelled()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the conference has been deleted")]
+        public void ThenTheConferenceHasBeenDeleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the participant details have been updated")]
+        public void ThenTheParticipantDetailsHaveBeenUpdated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
