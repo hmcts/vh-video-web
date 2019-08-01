a23f14bfd74fc28296155aaa49a689ce087e2492
muralikaturi <murali.katuri@hmcts.net>
2019-08-01 11:50:26 +0100

    Changed token expire to 4 hours

--- VideoWeb/VideoWeb/Controllers/TokenController.cs
+++ VideoWeb/VideoWeb/Controllers/TokenController.cs
@@ -54,7 +54,7 @@ namespace VideoWeb.Controllers
             }
 
             var expiresOn = DateTime.UtcNow.AddMinutes(180).ToUniversalTime().ToString(CultureInfo.InvariantCulture);
-            var token = _customJwtTokenProvider.GenerateToken(participantId.ToString(), 180);
+            var token = _customJwtTokenProvider.GenerateToken(participantId.ToString(), 240);
             var tokenResponse = new TokenResponse {ExpiresOn = expiresOn, Token = token};
             return Ok(tokenResponse);
         }
