f9025268096941e9bd7f5184bf168783c9a590cd
Shaed Parkar <shaed.parkar@hmcts.net>
2019-08-07 09:29:05 +0100

    add individual private consultation control stub

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/individual-consultation-controls/individual-consultation-controls.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/individual-consultation-controls/individual-consultation-controls.component.spec.ts
@@ -1,16 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IndividualConsultationControlsComponent } from './individual-consultation-controls.component';
+import { configureTestSuite } from 'ng-bullet';
+import { Logger } from 'src/app/services/logging/logger-base';
+import { MockLogger } from 'src/app/testing/mocks/MockLogger';
 
 describe('IndividualConsultationControlsComponent', () => {
   let component: IndividualConsultationControlsComponent;
   let fixture: ComponentFixture<IndividualConsultationControlsComponent>;
 
-  beforeEach(async(() => {
+  configureTestSuite((() => {
     TestBed.configureTestingModule({
-      declarations: [ IndividualConsultationControlsComponent ]
+      declarations: [IndividualConsultationControlsComponent],
+      providers: [
+        { provide: Logger, useClass: MockLogger }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +25,9 @@ describe('IndividualConsultationControlsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit when consultation has been closed', () => {
+    spyOn(component.cancelConsulation, 'emit').and.callFake(() => { });
+    component.closeConsultation();
+    expect(component.cancelConsulation.emit).toHaveBeenCalled();
   });
 });
