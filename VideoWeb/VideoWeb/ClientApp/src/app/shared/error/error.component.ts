c5c6a5296dfe87c3596773ad5ec00e5acb9ecef6
Shaed Parkar <shaed.parkar@hmcts.net>
2019-08-28 16:11:27 +0100

    revert to previous page when on service error page

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/error/error.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/error/error.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html'
 })
-export class ErrorComponent implements OnInit {
+export class ErrorComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  returnTimeout: NodeJS.Timer;
+  private readonly CALL_TIMEOUT = 30000;
+
+  constructor(private location: Location) { }
 
   ngOnInit() {
+    this.returnTimeout = setTimeout(async () => {
+      this.location.back();
+    }, this.CALL_TIMEOUT);
+  }
+
+  ngOnDestroy(): void {
+    clearTimeout(this.returnTimeout);
   }
 
 }
