b630f4f84abb83864eaef988a014e15614a8074f
NPoxon <sendnickmessages@hotmail.com>
2019-09-06 14:48:20 +0200

    Cleaned up more useless POs

--- VideoWeb/VideoWeb.AcceptanceTests/Pages/HearingRoomPage.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Pages/HearingRoomPage.cs
@@ -6,12 +6,10 @@ namespace VideoWeb.AcceptanceTests.Pages
     {
         public const string JudgeIframeId = "judgeIframe";
         public By JudgeIframe => By.XPath($"//iframe[@id='{JudgeIframeId}']");
-
         public By TechnicalIssues = By.Id("technicalIssuesButton");
         public By PauseButton = By.Id("pauseButton");
         public By CloseButton = By.Id("closeButton");
         public By ToggleSelfview = By.Id("selfViewButton");
-
         public By IncomingVideo = By.Id("incomingVideo");
         public By ParticipantIncomingVideo = By.Id("incomingFeed");
         public By SelfView = By.Id("selfviewVideo");
