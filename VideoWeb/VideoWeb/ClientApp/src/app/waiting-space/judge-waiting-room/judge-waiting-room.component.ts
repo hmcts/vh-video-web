7a1c77c5c43e5ef60b00e7778e6a9d8722d9e8d9
Lionel Roche <lionel.roche@hmcts.net>
2019-10-29 12:33:58 +0000

    removed console logs

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
@@ -95,10 +95,7 @@ export class JudgeWaitingRoomComponent implements OnInit {
   }
 
   handleParticipantStatusChange(message: ParticipantStatusMessage): any {
-    console.log(message);
-    console.log(this.conference);
     const participant = this.conference.participants.find(p => p.id === message.participantId);
-    console.log(participant);
     const status = <ParticipantStatus>message.status;
     participant.status = status;
   }
