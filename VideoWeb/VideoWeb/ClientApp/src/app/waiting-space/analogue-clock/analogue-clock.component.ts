ba32f9b09302a1559fff77378a9c54305fce931a
Lionel Roche <lionel.roche@hmcts.net>
2019-11-06 16:23:54 +0000

    removed code duplication.

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/analogue-clock/analogue-clock.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/analogue-clock/analogue-clock.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ClockService } from 'src/app/services/clock.service';
 import { Hearing } from '../../shared/models/hearing';
-import * as moment from 'moment';
 
 @Component({
   selector: 'app-analogue-clock',
@@ -24,9 +23,6 @@ export class AnalogueClockComponent implements OnInit {
   ngOnInit() {
     this.setCurrentTime();
 
-    this.hourHand = document.getElementById('hour-hand');
-    this.minuteHand = document.getElementById('minute-hand');
-    this.secondHand = document.getElementById('second-hand');
     this.clockService.getClock().subscribe((time) => {
       this.currentTime = time;
       this.updateclock();
@@ -37,12 +33,7 @@ export class AnalogueClockComponent implements OnInit {
     if (!this.hearing) {
       return;
     }
-    const hour_as_degree = (this.currentTime.getHours() + this.currentTime.getMinutes() / 60) / 12 * 360;
-    const minute_as_degree = this.currentTime.getMinutes() / 60 * 360;
-    const second_as_degree = (this.currentTime.getSeconds() + this.currentTime.getMilliseconds() / 1000) / 60 * 360;
-    this.hourHand.style.transform = `rotate(${hour_as_degree}deg)`;
-    this.minuteHand.style.transform = `rotate(${minute_as_degree}deg)`;
-    this.secondHand.style.transform = `rotate(${second_as_degree}deg)`;
+    this.updateClockUI(this.currentTime);
   }
 
   private setCurrentTime(): void {
@@ -51,15 +42,15 @@ export class AnalogueClockComponent implements OnInit {
     this.minuteHand = document.getElementById('minute-hand');
     this.secondHand = document.getElementById('second-hand');
 
-    // get utc time now.
-    const timeNow = moment.utc();
-    const init_hour_as_degree = (timeNow.hour() + timeNow.minute() / 60) / 12 * 360;
-    const init_minute_as_degree = timeNow.minute() / 60 * 360;
-    const init_second_as_degree = (timeNow.second() + timeNow.millisecond() / 1000) / 60 * 360;
-
-    // set the time.
-    this.hourHand.style.transform = `rotate(${init_hour_as_degree}deg)`;
-    this.minuteHand.style.transform = `rotate(${init_minute_as_degree}deg)`;
-    this.secondHand.style.transform = `rotate(${init_second_as_degree}deg)`;
+    const timeNow = new Date();
+    this.updateClockUI(timeNow);
+  }
+  private updateClockUI(newTime: Date): void {
+    const hour_as_degree = (newTime.getUTCHours() + newTime.getUTCMinutes() / 60) / 12 * 360;
+    const minute_as_degree = newTime.getUTCMinutes() / 60 * 360;
+    const second_as_degree = (newTime.getUTCSeconds() + newTime.getUTCMilliseconds() / 1000) / 60 * 360;
+    this.hourHand.style.transform = `rotate(${hour_as_degree}deg)`;
+    this.minuteHand.style.transform = `rotate(${minute_as_degree}deg)`;
+    this.secondHand.style.transform = `rotate(${second_as_degree}deg)`;
   }
 }
