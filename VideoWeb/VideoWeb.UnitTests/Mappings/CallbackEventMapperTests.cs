6d332bf8285dd43c0745b6769e4a409840b7923e
Saravanan RadhaKrishnan <shravan_krish@yahoo.com>
2020-01-31 15:29:29 +0000

    Fixed surviving mutants and added test coverage

---
+++ VideoWeb/VideoWeb.UnitTests/Mappings/CallbackEventMapperTests.cs
@@ -0,0 +1,57 @@
+﻿
+using FizzWare.NBuilder;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using VideoWeb.EventHub.Models;
+using VideoWeb.Mappings;
+using VideoWeb.Services.Video;
+using Role = VideoWeb.EventHub.Enums.UserRole;
+
+namespace VideoWeb.UnitTests.Mappings
+{
+    public class CallbackEventMapperTests
+    {
+        public CallbackEventMapper callbackEventMapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            callbackEventMapper = new CallbackEventMapper();
+        }
+
+        [Test]
+        public void should_map_conferenceevent_to_callbackevent()
+        {
+            var _testConference = new Conference {
+                                        Id = Guid.NewGuid(),
+                                        HearingId = Guid.NewGuid(),
+                                        Participants = new List<Participant>()
+                                        {
+                                            Builder<Participant>.CreateNew()
+                                                .With(x => x.Role = Role.Judge).With(x => x.Id = Guid.NewGuid())
+                                                .Build(),
+                                            Builder<Participant>.CreateNew().With(x => x.Role = Role.Individual)
+                                                .With(x => x.Id = Guid.NewGuid()).Build(),
+                                            Builder<Participant>.CreateNew().With(x => x.Role = Role.Representative)
+                                                .With(x => x.Id = Guid.NewGuid()).Build(),
+                                            Builder<Participant>.CreateNew().With(x => x.Role = Role.Individual)
+                                                .With(x => x.Id = Guid.NewGuid()).Build(),
+                                            Builder<Participant>.CreateNew().With(x => x.Role = Role.Representative)
+                                                .With(x => x.Id = Guid.NewGuid()).Build()
+                                        }
+                                    };
+            var conferenceEventRequest = Builder<ConferenceEventRequest>.CreateNew()
+                .With(x => x.Conference_id = _testConference.Id.ToString())
+                .With(x => x.Participant_id = _testConference.Participants[0].Id.ToString())
+                .With(x => x.Event_type = EventType.Joined)
+                .Build();
+
+            var result = callbackEventMapper.MapConferenceEventToCallbackEventModel(conferenceEventRequest);
+            result.Should().NotBeNull();
+            result.TransferFrom.Should().Be(conferenceEventRequest.Transfer_from);
+            result.TransferTo.Should().Be(conferenceEventRequest.Transfer_to);
+        }
+    }
+}
