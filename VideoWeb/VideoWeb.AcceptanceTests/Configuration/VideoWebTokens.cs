ebeb9bbc4a0159d2fafede72138bd836e411ea44
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2020-02-03 16:50:39 +0000

    Update config settings for video web api url (#357)
    Bug fixes

--- VideoWeb/VideoWeb.AcceptanceTests/Configuration/VideoWebTokens.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Configuration/VideoWebTokens.cs
@@ -4,7 +4,6 @@
     {
         public string BookingsApiBearerToken { get; set; }
         public string CallbackBearerToken { get; set; }
-        public string CustomToken { get; set; }
         public string UserApiBearerToken { get; set; }
         public string VideoApiBearerToken { get; set; }
         public string VideoWebBearerToken { get; set; }
