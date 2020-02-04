a5475e82cccce1c38cf865f39b6f3d74827be2d3
Valentina <Valentina.bennett@hmcts.net>
2020-02-04 13:40:04 +0000

    Added event JudgeUnavailable and JudgeAvailable

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-list/judge-hearing-list.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-list/judge-hearing-list.component.ts
@@ -3,12 +3,18 @@ import { FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProfileService } from 'src/app/services/api/profile.service';
 import { VideoWebService } from 'src/app/services/api/video-web.service';
-import { ConferenceForUserResponse, UserProfileResponse } from 'src/app/services/clients/api-client';
+import {
+  ConferenceForUserResponse, UserProfileResponse,
+  UpdateParticipantStatusEventRequest, EventType
+}
+  from 'src/app/services/clients/api-client';
 import { ErrorService } from 'src/app/services/error.service';
 import { VhContactDetails } from 'src/app/shared/contact-information';
 import { PageUrls } from 'src/app/shared/page-url.constants';
 import { Logger } from 'src/app/services/logging/logger-base';
 import { Subscription } from 'rxjs';
+import { SessionStorage } from 'src/app/services/session-storage';
+import { EventStatusModel } from 'src/app/services/models/event-status.model';
 
 import * as $ from 'jquery';
 
@@ -31,21 +37,25 @@ export class JudgeHearingListComponent implements OnInit, OnDestroy {
   interval: any;
   today = new Date();
   profile: UserProfileResponse;
+  private readonly eventStatusCache: SessionStorage<EventStatusModel>;
+  readonly JUDGE_STATUS_KEY = 'vh.judge.status';
 
   constructor(
     private videoWebService: VideoWebService,
     private errorService: ErrorService,
     private router: Router,
     private profileService: ProfileService,
-    private logger: Logger
+    private logger: Logger,
   ) {
     this.loadingData = true;
+    this.eventStatusCache = new SessionStorage(this.JUDGE_STATUS_KEY);
   }
 
   ngOnInit() {
     this.profileService.getUserProfile().then((profile) => {
       this.profile = profile;
     });
+    this.postJudgeEvent();
     this.retrieveHearingsForUser();
     this.interval = setInterval(() => {
       this.retrieveHearingsForUser();
@@ -64,9 +74,9 @@ export class JudgeHearingListComponent implements OnInit, OnDestroy {
     this.conferencesSubscription = this.videoWebService.getConferencesForJudge().subscribe((data: ConferenceForUserResponse[]) => {
       this.loadingData = false;
       this.conferences = data;
-        if (this.conferences.length > 0) {
-           this.enableFullScreen(true);
-        }
+      if (this.conferences.length > 0) {
+        this.enableFullScreen(true);
+      }
     },
       (error) => {
         this.loadingData = false;
@@ -74,6 +84,19 @@ export class JudgeHearingListComponent implements OnInit, OnDestroy {
         this.errorService.handleApiError(error);
       });
   }
+
+  postJudgeEvent() {
+    const eventStatusDetails = this.eventStatusCache.get();
+    if (eventStatusDetails) {
+      this.videoWebService.raiseParticipantEvent(eventStatusDetails.ConferenceId,
+        new UpdateParticipantStatusEventRequest({
+          participant_id: eventStatusDetails.ParticipantId, event_type: EventType.JudgeUnavailable
+        })).subscribe(x => { this.eventStatusCache.clear();  },
+          (error) => {
+            console.error(error);
+          });
+    }
+  } 
 
   get courtName(): string {
     return (this.profile) ? `${this.profile.first_name}, ${this.profile.last_name}` : '';
