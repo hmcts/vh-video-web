b18e808d7dd246baa13368669988319283603cc9
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2020-02-11 10:45:25 +0000

    Feature/vih 5499 upgrade signal r fe client (#367)
    * [VIH-5499]
    Upgrade Signal R client and retrieve conference details on re-connect
    
    * [VIH-5427] Get latest data on eventhub disruptions

--- VideoWeb/VideoWeb/ClientApp/src/app/testing/mocks/MockEventService.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/testing/mocks/MockEventService.ts
@@ -4,12 +4,16 @@ import { ConsultationMessage } from 'src/app/services/models/consultation-messag
 import { HelpMessage } from 'src/app/services/models/help-message';
 import { ParticipantStatusMessage } from 'src/app/services/models/participant-status-message';
 
-import { ConferenceStatus, ParticipantStatus } from '../../services/clients/api-client';
+import {
+  ConferenceStatus,
+  ParticipantStatus,
+  RoomType,
+  ConsultationAnswer
+} from '../../services/clients/api-client';
 import { EventsService } from 'src/app/services/events.service';
 import { AdminConsultationMessage } from 'src/app/services/models/admin-consultation-message';
 
 export class MockEventsService {
-
   nextParticipantStatusMessage: ParticipantStatusMessage;
   nextHearingStatusMessage: ConferenceStatusMessage;
   nextHelpMessage: HelpMessage;
@@ -17,15 +21,34 @@ export class MockEventsService {
   nextAdminConsultationMessage: AdminConsultationMessage;
 
   constructor() {
-    this.nextParticipantStatusMessage = new ParticipantStatusMessage('9F681318-4955-49AF-A887-DED64554429D', ParticipantStatus.Available);
-    this.nextHearingStatusMessage = new ConferenceStatusMessage('612AB52C-BDA5-4F4D-95B8-3F49065219A6', ConferenceStatus.InSession);
-    this.nextHelpMessage = new HelpMessage('612AB52C-BDA5-4F4D-95B8-3F49065219A6', 'chris.green@hearings.net');
-    this.nextConsultationMessage = new ConsultationMessage('612AB52C-BDA5-4F4D-95B8-3F49065219A6', 'chris.green@hearings.net',
-      'james.green@hearings.net', null);
+    this.nextParticipantStatusMessage = new ParticipantStatusMessage(
+      '9F681318-4955-49AF-A887-DED64554429D',
+      ParticipantStatus.Available
+    );
+    this.nextHearingStatusMessage = new ConferenceStatusMessage(
+      '612AB52C-BDA5-4F4D-95B8-3F49065219A6',
+      ConferenceStatus.InSession
+    );
+    this.nextHelpMessage = new HelpMessage(
+      '612AB52C-BDA5-4F4D-95B8-3F49065219A6',
+      'chris.green@hearings.net'
+    );
+    this.nextConsultationMessage = new ConsultationMessage(
+      '612AB52C-BDA5-4F4D-95B8-3F49065219A6',
+      'chris.green@hearings.net',
+      'james.green@hearings.net',
+      null
+    );
+    this.nextAdminConsultationMessage = new AdminConsultationMessage(
+      '612AB52C-BDA5-4F4D-95B8-3F49065219A6',
+      RoomType.ConsultationRoom1,
+      'james.green@hearings.net',
+      ConsultationAnswer.None
+    );
   }
 
-  start() { }
-  stop() { }
+  start() {}
+  stop() {}
 
   getParticipantStatusMessage(): Observable<ParticipantStatusMessage> {
     return of(this.nextParticipantStatusMessage);
@@ -46,9 +69,17 @@ export class MockEventsService {
   getAdminConsultationMessage(): Observable<AdminConsultationMessage> {
     return of(this.nextAdminConsultationMessage);
   }
+
+  getServiceDisconnected(): Observable<any> {
+    return of(true);
+  }
+
+  getServiceReconnected(): Observable<any> {
+    return of(true);
+  }
 }
 
 export class MockEventsNonHttpService extends EventsService {
-  start() { }
-  stop() { }
+  start() {}
+  stop() {}
 }
