0ff7f08f1ecc0dc666746a732710c65e3031deda
Valentina <Valentina.bennett@hmcts.net>
2020-02-11 11:28:34 +0000

    unit test change

--- VideoWeb/VideoWeb.UnitTests/Mappings/EventTypeReasonMapperTest.cs
+++ VideoWeb/VideoWeb.UnitTests/Mappings/EventTypeReasonMapperTest.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using NUnit.Framework;
 using VideoWeb.Mappings;
 using VideoWeb.Services.Video;
@@ -10,7 +10,7 @@ namespace VideoWeb.UnitTests.Mappings
         [Test]
         public void Should_set_reason_for_event_type_joined()
         {
-            var eventType = EventType.Joined;
+            var eventType = EventType.ParticipantJoining
             EventTypeReasonMapper.Map(eventType).Should().Be("participant joining");
         }
         [Test]
