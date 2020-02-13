44a62127d6611238f609a4b8f40551ef41fcca87
Shaed Parkar <shaed.parkar@hmcts.net>
2020-02-13 12:15:15 +0000

    Refactor unit tests where code is duplicated

--- VideoWeb/VideoWeb.UnitTests/Hub/ChatHubTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Hub/ChatHubTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,16 +16,8 @@ namespace VideoWeb.UnitTests.Hub
         public async Task should_subscribe_admin_to_all_conferences()
         {
             var numOfConferences = 10;
-            var conferences = Builder<ConferenceSummaryResponse>.CreateListOfSize(numOfConferences).All()
-                .With(x => x.Id = Guid.NewGuid())
-                .Build().ToList();
-
+            var conferences = SetupAdminConferences(numOfConferences);
             var conferenceIds = conferences.Select(c => c.Id.ToString()).ToArray();
-
-            UserProfileServiceMock.Setup(x => x.IsVhOfficerAsync(It.IsAny<string>()))
-                .ReturnsAsync(true);
-
-            VideoApiClientMock.Setup(x => x.GetConferencesTodayAsync()).ReturnsAsync(conferences);
 
             await Hub.OnConnectedAsync();
             
@@ -37,16 +30,8 @@ namespace VideoWeb.UnitTests.Hub
         public async Task should_unsubscribe_admin_from_all_conferences()
         {
             var numOfConferences = 10;
-            var conferences = Builder<ConferenceSummaryResponse>.CreateListOfSize(numOfConferences).All()
-                .With(x => x.Id = Guid.NewGuid())
-                .Build().ToList();
-
+            var conferences = SetupAdminConferences(numOfConferences);
             var conferenceIds = conferences.Select(c => c.Id.ToString()).ToArray();
-
-            UserProfileServiceMock.Setup(x => x.IsVhOfficerAsync(It.IsAny<string>()))
-                .ReturnsAsync(true);
-
-            VideoApiClientMock.Setup(x => x.GetConferencesTodayAsync()).ReturnsAsync(conferences);
 
             await Hub.OnDisconnectedAsync(null);
             
@@ -55,32 +40,26 @@ namespace VideoWeb.UnitTests.Hub
                     CancellationToken.None), Times.Exactly(numOfConferences));
         }
 
-        [Test]
-        public async Task should_subscribe_judge_to_conferences_they_are_assigned_to()
+        private List<ConferenceSummaryResponse> SetupAdminConferences(int numOfConferences)
         {
-            var numOfConferences = 10;
-            var numOfConferencesWithUser = 2;
-            var participantsWithUser = Builder<ParticipantSummaryResponse>.CreateListOfSize(3)
-                .TheFirst(1).With(x => x.Username = Claims.Identity.Name).With(x=> x.User_role = UserRole.Judge)
-                .Build().ToList();
-            var participantsWithoutUser = Builder<ParticipantSummaryResponse>.CreateListOfSize(3)
-                .TheFirst(1).With(x=> x.User_role = UserRole.Judge)
-                .Build().ToList();
-            
             var conferences = Builder<ConferenceSummaryResponse>.CreateListOfSize(numOfConferences).All()
                 .With(x => x.Id = Guid.NewGuid())
-                .TheFirst(numOfConferencesWithUser).With(x => x.Participants = participantsWithUser)
-                .TheRest().With(x => x.Participants = participantsWithoutUser)
                 .Build().ToList();
             
             UserProfileServiceMock.Setup(x => x.IsVhOfficerAsync(It.IsAny<string>()))
-                .ReturnsAsync(false);
-
-            VideoApiClientMock.Setup(x => x.GetConferencesTodayAsync()).ReturnsAsync(conferences);
+                .ReturnsAsync(true);
             
-            var conferenceIds = conferences
-                .Where(x => x.Participants.Any(p => p.Username == Claims.Identity.Name))
-                .Select(c => c.Id.ToString()).ToArray();
+            VideoApiClientMock.Setup(x => x.GetConferencesTodayAsync()).ReturnsAsync(conferences);
+
+            return conferences;
+        }
+        
+        [Test]
+        public async Task should_subscribe_judge_to_conferences_they_are_assigned_to()
+        {
+            const int numOfConferences = 10;
+            const int numOfConferencesWithUser = 2;
+            var conferenceIds = SetupJudgeConferences(numOfConferences, numOfConferencesWithUser);
             
             await Hub.OnConnectedAsync();
             
@@ -92,8 +71,34 @@ namespace VideoWeb.UnitTests.Hub
         [Test]
         public async Task should_unsubscribe_judge_from_conferences_they_are_assigned_to()
         {
-            var numOfConferences = 10;
-            var numOfConferencesWithUser = 2;
+            const int numOfConferences = 10;
+            const int numOfConferencesWithUser = 2;
+            var conferenceIds = SetupJudgeConferences(numOfConferences, numOfConferencesWithUser);
+            
+            await Hub.OnDisconnectedAsync(null);
+            
+            GroupManagerMock.Verify(
+                x => x.RemoveFromGroupAsync(HubCallerContextMock.Object.ConnectionId, It.IsIn(conferenceIds),
+                    CancellationToken.None), Times.Exactly(numOfConferencesWithUser));
+        }
+        
+        [Test]
+        public async Task should_log_critical_when_exception_on_disconnect()
+        {
+            const int numOfConferences = 10;
+            const int numOfConferencesWithUser = 2;
+            var conferenceIds = SetupJudgeConferences(numOfConferences, numOfConferencesWithUser);
+   
+            var exception = new InconclusiveException("Some test");
+            await Hub.OnDisconnectedAsync(exception);
+            
+            GroupManagerMock.Verify(
+                x => x.RemoveFromGroupAsync(HubCallerContextMock.Object.ConnectionId, It.IsIn(conferenceIds),
+                    CancellationToken.None), Times.Exactly(numOfConferencesWithUser));
+        }
+        
+        private string[] SetupJudgeConferences(int numOfConferences, int numOfConferencesWithUser)
+        {
             var participantsWithUser = Builder<ParticipantSummaryResponse>.CreateListOfSize(3)
                 .TheFirst(1).With(x => x.Username = Claims.Identity.Name).With(x=> x.User_role = UserRole.Judge)
                 .Build().ToList();
@@ -111,16 +116,10 @@ namespace VideoWeb.UnitTests.Hub
                 .ReturnsAsync(false);
 
             VideoApiClientMock.Setup(x => x.GetConferencesTodayAsync()).ReturnsAsync(conferences);
-            
-            var conferenceIds = conferences
+
+            return conferences
                 .Where(x => x.Participants.Any(p => p.Username == Claims.Identity.Name))
                 .Select(c => c.Id.ToString()).ToArray();
-            
-            await Hub.OnDisconnectedAsync(null);
-            
-            GroupManagerMock.Verify(
-                x => x.RemoveFromGroupAsync(HubCallerContextMock.Object.ConnectionId, It.IsIn(conferenceIds),
-                    CancellationToken.None), Times.Exactly(numOfConferencesWithUser));
         }
     }
 }
