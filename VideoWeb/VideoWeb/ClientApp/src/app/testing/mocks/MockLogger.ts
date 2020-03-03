bd1aa4cf58ce9335f5670fa685e2ec33a4d3870f
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2020-03-03 10:38:54 +0000

    added tests to judge conference status change (#405)
    * use the correct conference status variable to determine judge's expected location

--- VideoWeb/VideoWeb/ClientApp/src/app/testing/mocks/MockLogger.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/testing/mocks/MockLogger.ts
@@ -1,9 +1,23 @@
 import { Logger } from 'src/app/services/logging/logger-base';
 
 export class MockLogger implements Logger {
-    warn(message: string): void { }
-    debug(message: string): void { }
-    info(message: string): void { }
-    event(event: string, properties?: any): void { }
-    error(message: string, err: Error, properties?: any): void { }
+    warn(message: string): void {
+        console.warn(message);
+    }
+    debug(message: string): void {
+        console.debug(message);
+    }
+    info(message: string): void {
+        console.info(message);
+    }
+    event(event: string, properties?: any): void {
+        const propertiesFormatted = properties ? JSON.stringify(properties) : '';
+        console.log(`[EVENT:${event}] ${propertiesFormatted}`.trim());
+    }
+    error(message: string, err: Error, properties?: any): void {
+        console.error(`[ERROR] ${message}`, err);
+        if (properties) {
+            console.log(`Properties: ${JSON.stringify(properties)}`);
+        }
+    }
 }
