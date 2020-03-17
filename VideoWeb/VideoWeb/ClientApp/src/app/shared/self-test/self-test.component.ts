a213471ad89576d85f45a669fe63fe7ac477a910
Lionel Roche <lionel.roche@hmcts.net>
2020-03-17 11:53:28 +0000

    - update

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/self-test/self-test.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/self-test/self-test.component.ts
@@ -54,7 +54,6 @@ export class SelfTestComponent implements OnInit, OnDestroy {
 
     private maxBandwidth = 768;
     subscription: Subscription;
-    pexRTC: any;
 
     constructor(
         private logger: Logger,
@@ -155,7 +154,6 @@ export class SelfTestComponent implements OnInit, OnDestroy {
     setupPexipClient() {
         const self = this;
         this.pexipAPI = new PexRTC();
-        (<any>window).pexRTC = this.pexipAPI;
         this.updatePexipAudioVideoSource();
         this.pexipAPI.onSetup = function (stream, pin_status, conference_extension) {
             self.logger.info('running pexip test call setup');
