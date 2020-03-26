be7d4315bb3b3a287d19ac03dcb16654bd4bc0ba
Shaed Parkar <shaed.parkar@hmcts.net>
2020-03-26 09:30:46 +0000

    Revert "override participant joining since notification is only for VHO"
    This reverts commit 10c503ff358cc60cf605cc40d4d2c4f3c4fce967.

--- VideoWeb/VideoWeb.EventHub/Handlers/ParticipantJoiningEventHandler.cs
+++ VideoWeb/VideoWeb.EventHub/Handlers/ParticipantJoiningEventHandler.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using VideoWeb.Common.Caching;
-using VideoWeb.Common.Models;
 using VideoWeb.EventHub.Enums;
 using VideoWeb.EventHub.Handlers.Core;
 using VideoWeb.EventHub.Hub;
@@ -18,17 +16,6 @@ namespace VideoWeb.EventHub.Handlers
         }
 
         public override EventType EventType => EventType.ParticipantJoining;
-
-        public override async Task HandleAsync(CallbackEvent callbackEvent)
-        {
-            SourceConference = new Conference
-            {
-                Id = callbackEvent.ConferenceId,
-                Participants = new List<Participant>()
-            };
-            SourceParticipant = new Participant {Id = callbackEvent.ParticipantId};
-            await PublishStatusAsync(callbackEvent);
-        }
 
         protected override async Task PublishStatusAsync(CallbackEvent callbackEvent)
         {
