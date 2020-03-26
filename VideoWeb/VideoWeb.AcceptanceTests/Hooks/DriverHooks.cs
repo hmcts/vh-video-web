d524c0a681987553720e4a286c80e1aeea7fceb8
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-03-26 15:45:32 +0100

    testfix/VIH-5552 : Added fixes for alert tests, Added hooks changes to allow testfix and bugfix

--- VideoWeb/VideoWeb.AcceptanceTests/Hooks/DriverHooks.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Hooks/DriverHooks.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using AcceptanceTests.Common.Driver;
 using AcceptanceTests.Common.Driver.Browser;
+using AcceptanceTests.Common.Driver.Helpers;
+using AcceptanceTests.Common.PageObject.Pages;
 using BoDi;
+using FluentAssertions;
 using TechTalk.SpecFlow;
 using VideoWeb.AcceptanceTests.Helpers;
 
@@ -38,8 +41,35 @@ namespace VideoWeb.AcceptanceTests.Hooks
                 context.VideoWebConfig.TestConfig.TargetBrowser);
         }
 
-        [AfterScenario]
-        public void AfterScenario(TestContext context, ScenarioContext scenarioContext)
+        [AfterScenario(Order = (int) HooksSequence.SignOutHooks)]
+        public void SignOutIfPossible(TestContext context)
+        {
+            if (_browsers?[context.CurrentUser.Key].Driver == null) return;
+            if (SignOutLinkIsPresent(context.CurrentUser.Key))
+                SignOut(context.CurrentUser.Key);
+        }
+
+        public bool SignOutLinkIsPresent(string key)
+        {
+            try
+            {
+                _browsers[key].Driver.FindElement(CommonPages.SignOutLink, 2);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void SignOut(string key)
+        {
+            _browsers[key].ClickLink(CommonPages.SignOutLink, 2);
+            _browsers[key].Driver.WaitUntilVisible(CommonPages.SignOutMessage).Displayed.Should().BeTrue();
+        }
+
+        [AfterScenario(Order = (int)HooksSequence.LogResultHooks)]
+        public void LogResult(TestContext context, ScenarioContext scenarioContext)
         {
             if (_browsers != null)
             {
@@ -47,8 +77,14 @@ namespace VideoWeb.AcceptanceTests.Hooks
                     context.VideoWebConfig.SauceLabsConfiguration.RunningOnSauceLabs(),
                     _browsers.Count > 0 ? _browsers[context.CurrentUser.Key].Driver : context.Driver.GetDriver(""),
                     scenarioContext.TestError == null);
-                DriverManager.TearDownBrowsers(_browsers);
             }
+        }
+
+        [AfterScenario(Order = (int)HooksSequence.TearDownBrowserHooks)]
+        public void TearDownBrowser(TestContext context, ScenarioContext scenarioContext)
+        {
+            if (_browsers != null)
+                DriverManager.TearDownBrowsers(_browsers);
 
             DriverManager.KillAnyLocalDriverProcesses();
         }
