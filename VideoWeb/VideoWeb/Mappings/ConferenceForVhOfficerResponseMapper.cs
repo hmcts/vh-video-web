4b60aa024bd6d4aa5bae2ec15052eff2745275ea
Shaed Parkar <shaed.parkar@hmcts.net>
2020-03-26 09:09:56 +0000

    remove unused contract merge ConferenceForUser and ConferenceForVhOfficer mappers

--- VideoWeb/VideoWeb/Mappings/ConferenceForVhOfficerResponseMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ConferenceForVhOfficerResponseMapper.cs
@@ -11,8 +11,38 @@ namespace VideoWeb.Mappings
         public static ConferenceForVhOfficerResponse MapConferenceSummaryToResponseModel(ConferenceSummaryResponse conference,
             IList<InstantMessageResponse> messageResponses)
         {
-            var response = ConferenceForUserResponseMapper.MapConferenceSummaryToResponseModel<ConferenceForVhOfficerResponse>(conference);
+            var response = new ConferenceForVhOfficerResponse
+            {
+                Id = conference.Id,
+                CaseName = conference.Case_name,
+                CaseNumber = conference.Case_number,
+                CaseType = conference.Case_type,
+                ScheduledDateTime = conference.Scheduled_date_time,
+                ScheduledDuration = conference.Scheduled_duration,
+                Status = Enum.Parse<ConferenceStatus>(conference.Status.ToString()),
+                NoOfPendingTasks = conference.Pending_tasks,
+                HearingVenueName = conference.Hearing_venue_name
+            };
+            
+            MapMessages(response, conference, messageResponses);
+            MapTasks(response, conference);
+            
+            return response;
+        }
 
+        private static void MapTasks(ConferenceForVhOfficerResponse response, ConferenceSummaryResponse conference)
+        {
+            if (conference.Tasks == null) return;
+            var conferenceTasks = conference.Tasks
+                .Select(x => new TaskUserResponse { Id = x.Id, Body = x.Body })
+                .ToList();
+                
+            response.Tasks = conferenceTasks;
+        }
+
+        private static void MapMessages(ConferenceForVhOfficerResponse response, ConferenceSummaryResponse conference,
+            IList<InstantMessageResponse> messageResponses)
+        {
             if (messageResponses == null || !messageResponses.Any())
             {
                 response.NumberOfUnreadMessages = 0;
@@ -24,8 +54,6 @@ namespace VideoWeb.Mappings
                 response.NumberOfUnreadMessages =
                     vhoMessage == null ? messageResponses.Count() : messageResponses.IndexOf(vhoMessage);
             }
-
-            return response;
         }
 
         private static bool IsNonParticipantMessage(ConferenceSummaryResponse conference, InstantMessageResponse message)
