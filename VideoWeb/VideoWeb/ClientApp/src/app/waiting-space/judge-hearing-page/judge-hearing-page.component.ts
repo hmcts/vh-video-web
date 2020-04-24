f03c2f543e001e7b7e0ed0853310e0ebc705b493
Sunil DSouza <sunil.dsouza111@hmcts.net>
2020-04-24 11:58:09 +0100

    bugfix/VIH-5869 : Feature policy changes for camera and microphone

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-hearing-page/judge-hearing-page.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-hearing-page/judge-hearing-page.component.ts
@@ -93,8 +93,6 @@ export class JudgeHearingPageComponent implements OnInit, OnDestroy {
     this.judgeUri = `${this.conference.judge_i_frame_uri}?display_name=${encodedDisplayName}&cam=${cam}&mic=${mic}`;
     this.selectedHearingUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.judgeUri);
 
-    const iFrameElem = <HTMLIFrameElement>document.getElementById('judgeIframe');
-    iFrameElem.setAttribute('allow', this.allowPermissions);
   }
 
   private setupSubscribers() {
