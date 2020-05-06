8ceafd5e736ff8b738aff7aa5ab0279c87c359f4
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2020-05-06 12:58:26 +0100

    VIH-5727 monitoring graph position (#575)
    * display graph on hover and close on leave

--- VideoWeb/VideoWeb/ClientApp/src/app/testing/stubs/perfect-scrollbar-stub.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input  } from '@angular/core';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-
-@Component({ selector: 'perfect-scrollbar', template: '' })
-export class PerfectScrollbarStubComponent {
-  @Input() config: PerfectScrollbarConfigInterface;
-}
-
-
--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/participant-network-status/participant-network-status.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/participant-network-status/participant-network-status.component.ts
@@ -1,23 +1,59 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { VideoWebService } from 'src/app/services/api/video-web.service';
 import { ParticipantStatus } from 'src/app/services/clients/api-client';
-import { ParticipantSummary } from '../../shared/models/participant-summary';
+import { Logger } from 'src/app/services/logging/logger-base';
 import { HeartbeatHealth } from '../../services/models/participant-heartbeat';
+import { ParticipantSummary } from '../../shared/models/participant-summary';
+import { PackageLost } from '../services/models/package-lost';
+import { ParticipantGraphInfo } from '../services/models/participant-graph-info';
 
 @Component({
     selector: 'app-participant-network-status',
     templateUrl: './participant-network-status.component.html',
     styleUrls: ['./participant-network-status.component.scss', '../vho-global-styles.scss']
 })
-export class ParticipantNetworkStatusComponent {
+export class ParticipantNetworkStatusComponent implements OnInit {
     @Input() participant: ParticipantSummary;
+    @Input() conferenceId: string;
 
-    @Output()
-    showMonitorGraph: EventEmitter<ParticipantSummary> = new EventEmitter<ParticipantSummary>();
+    displayGraph: boolean;
+    loading: boolean;
+    monitoringParticipant: ParticipantGraphInfo;
+    packageLostArray: PackageLost[];
 
-    constructor() {}
+    constructor(private videoWebService: VideoWebService, private logger: Logger) {}
+    ngOnInit(): void {
+        this.displayGraph = false;
+        this.packageLostArray = [];
+    }
 
-    showParticipantGraph() {
-        this.showMonitorGraph.emit(this.participant);
+    async showParticipantGraph() {
+        this.logger.debug('showParticipantGraph');
+        if (this.displayGraph || this.loading) {
+            return;
+        }
+        try {
+            this.loading = true;
+            const heartbeatHistory = await this.videoWebService.getParticipantHeartbeats(this.conferenceId, this.participant.id);
+
+            this.loading = false;
+            this.packageLostArray = heartbeatHistory.map(x => {
+                return new PackageLost(x.recent_packet_loss, x.browser_name, x.browser_version, x.timestamp.getTime());
+            });
+
+            this.monitoringParticipant = new ParticipantGraphInfo(
+                this.participant.displayName,
+                this.participant.status,
+                this.participant.representee
+            );
+            this.setGraphVisibility(true);
+        } catch (err) {
+            this.loading = false;
+            this.logger.error(
+                `Failed to get heartbeat history for particpant ${this.participant.id} in conference ${this.conferenceId}`,
+                err
+            );
+        }
     }
 
     getParticipantNetworkStatus(): string {
@@ -30,27 +66,36 @@ export class ParticipantNetworkStatusComponent {
                 return 'not-signed-in.png';
             }
         } else {
-            if (
-                this.participant.participantHertBeatHealth.browserName.toLowerCase() === 'ms-edge' ||
-                this.participant.participantHertBeatHealth.browserName.toLowerCase() === 'safari'
-            ) {
+            if (this.isUnsupportedBrowser(this.participant.participantHertBeatHealth.browserName)) {
                 return 'incompatible-browser-signal.png';
             } else {
                 if (this.participant.status === ParticipantStatus.Disconnected) {
                     return 'disconnected.png';
                 } else {
-                    switch (this.participant.participantHertBeatHealth.heartbeatHealth) {
-                        case HeartbeatHealth.Good:
-                            return 'good-signal.png';
-                        case HeartbeatHealth.Bad:
-                            return 'bad-signal.png';
-                        case HeartbeatHealth.Poor:
-                            return 'poor-signal.png';
-                        case HeartbeatHealth.None:
-                            return 'incompatible-browser-signal.png';
-                    }
+                    return this.getIconForParticipantNetworkStatus();
                 }
             }
         }
+    }
+
+    getIconForParticipantNetworkStatus() {
+        switch (this.participant.participantHertBeatHealth.heartbeatHealth) {
+            case HeartbeatHealth.Good:
+                return 'good-signal.png';
+            case HeartbeatHealth.Bad:
+                return 'bad-signal.png';
+            case HeartbeatHealth.Poor:
+                return 'poor-signal.png';
+            case HeartbeatHealth.None:
+                return 'incompatible-browser-signal.png';
+        }
+    }
+
+    isUnsupportedBrowser(browserName: string) {
+        return browserName.toLowerCase() === 'ms-edge' || browserName.toLowerCase() === 'safari';
+    }
+
+    setGraphVisibility(visible: boolean) {
+        this.displayGraph = visible;
     }
 }
