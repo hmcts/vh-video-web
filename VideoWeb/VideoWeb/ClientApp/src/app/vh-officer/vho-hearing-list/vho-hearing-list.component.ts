4c951f6a328a75a6d9b770815c9ddddb0ea5542a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-05-14 12:20:43 +0100

    fix status text

--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/vho-hearing-list/vho-hearing-list.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/vho-hearing-list/vho-hearing-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ClipboardService } from 'ngx-clipboard';
-import { ConferenceResponseVho, ConferenceStatus } from 'src/app/services/clients/api-client';
+import { ConferenceResponseVho } from 'src/app/services/clients/api-client';
 import { Hearing } from 'src/app/shared/models/hearing';
 import { HearingSummary } from 'src/app/shared/models/hearing-summary';
 import { ParticipantSummary } from '../../shared/models/participant-summary';
@@ -22,49 +22,6 @@ export class VhoHearingListComponent implements OnInit {
 
     isCurrentConference(conference: HearingSummary): boolean {
         return this.currentConference != null && this.currentConference.id === conference.id;
-    }
-
-    isOnTime(conference: HearingSummary): boolean {
-        return conference.isOnTime() || conference.isStarting();
-    }
-
-    isSuspended(conference: HearingSummary): boolean {
-        return conference.status === ConferenceStatus.Suspended;
-    }
-
-    isDelayed(conference: HearingSummary): boolean {
-        return conference.isDelayed();
-    }
-
-    isPaused(conference: HearingSummary): boolean {
-        return conference.isPaused();
-    }
-
-    isInSession(conference: HearingSummary): boolean {
-        return conference.isInSession();
-    }
-
-    isClosed(conference: HearingSummary): boolean {
-        return conference.isClosed();
-    }
-
-    getConferenceStatusText(conference: HearingSummary): string {
-        const hearing = conference;
-        if (hearing.getConference().status === ConferenceStatus.NotStarted) {
-            if (hearing.isDelayed()) {
-                return 'Delayed';
-            } else {
-                return 'Not Started';
-            }
-        } else if (hearing.isSuspended()) {
-            return 'Suspended';
-        } else if (hearing.isPaused()) {
-            return 'Paused';
-        } else if (hearing.isClosed()) {
-            return 'Closed';
-        } else if (hearing.isInSession()) {
-            return 'In Session';
-        }
     }
 
     getDuration(conference: HearingSummary): string {
