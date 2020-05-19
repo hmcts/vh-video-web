f5f27f843f366e2cecb6be08dec3a9638d753b0f
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-05-19 14:29:48 +0100

    VIH-5994 Replace venue selection component

--- VideoWeb/VideoWeb.AcceptanceTests/Pages/VhoVenueAllocationPage.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Pages/VhoVenueAllocationPage.cs
@@ -4,7 +4,7 @@ namespace VideoWeb.AcceptanceTests.Pages
 {
     public static class VhoVenueAllocationPage
     {
-        public static By VenueCheckbox(string venue) => By.XPath($"//div[contains(text(),'{venue}')]/preceding-sibling::input");
+        public static By VenueCheckbox(string venue) => By.XPath($"//input[contains(text(),'{venue}')]");
         public static By VenuesDropdown = By.Id("venue-allocation-list");
         public static By VenueConfirmButton = By.Id("select-venue-allocation-btn");
     }
--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/venue-list/venue-list.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/venue-list/venue-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { pageUrls } from 'src/app/shared/page-url.constants';
 import { HearingVenueResponse } from 'src/app/services/clients/api-client';
-import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { VideoWebService } from 'src/app/services/api/video-web.service';
 import { SessionStorage } from 'src/app/services/session-storage';
 import { VhoStorageKeys } from '../services/models/session-keys';
@@ -16,7 +15,7 @@ export class VenueListComponent implements OnInit {
     private readonly venueAllocationStorage: SessionStorage<HearingVenueResponse[]>;
     venues: HearingVenueResponse[];
     selectedVenues: HearingVenueResponse[];
-    dropdownSettings: IDropdownSettings;
+    venueListLoading: boolean;
 
     constructor(private videoWebService: VideoWebService, private router: Router) {
         this.selectedVenues = [];
@@ -24,49 +23,20 @@ export class VenueListComponent implements OnInit {
     }
 
     async ngOnInit() {
-        this.videoWebService.getHearingVenues().then((response) => {
+        this.venueListLoading = false;
+        this.videoWebService.getHearingVenues().then(response => {
             this.venues = response;
             this.selectedVenues = this.venueAllocationStorage.get();
+            this.venueListLoading = false;
         });
-        this.initDropDown();
-    }
-
-    private initDropDown() {
-        this.dropdownSettings = {
-            singleSelection: false,
-            idField: 'id',
-            textField: 'name',
-            selectAllText: 'Select All',
-            unSelectAllText: 'Unselect All',
-            itemsShowLimit: 1,
-            allowSearchFilter: true,
-            searchPlaceholderText: 'Search venue name'
-        };
     }
 
     get venuesSelected(): boolean {
-        return this.selectedVenues && this.selectedVenues.length > 0;
-    }
-
-    onItemSelect(venue: HearingVenueResponse) {
-        const existingVenue = this.selectedVenues.find((x) => x.id === venue.id);
-        if (!existingVenue) {
-            this.selectedVenues.push(this.venues.find((x) => x.id === venue.id));
-        }
-    }
-
-    onItemDeselect(venue: HearingVenueResponse) {
-        this.selectedVenues = this.selectedVenues.filter((x) => x.id !== venue.id);
-    }
-
-    onSelectAll() {
-        this.selectedVenues = this.venues;
+        return this.selectedVenues.length > 0;
     }
 
     updateSelection() {
-        if (this.venuesSelected) {
-            this.venueAllocationStorage.set(this.selectedVenues);
-        }
+        this.venueAllocationStorage.set(this.selectedVenues);
     }
 
     goToHearingList() {
