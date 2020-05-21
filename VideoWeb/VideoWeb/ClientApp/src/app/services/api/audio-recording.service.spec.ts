e9902995c34a75061917e1cbae46f0948dc63632
Valentina <Valentina.bennett@hmcts.net>
2020-05-21 10:50:12 +0100

    feature/VIH-5811 : amend unit test

--- VideoWeb/VideoWeb/ClientApp/src/app/services/api/audio-recording.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/api/audio-recording.service.spec.ts
@@ -3,7 +3,7 @@ import { ApiClient } from 'src/app/services/clients/api-client';
 import { of } from 'rxjs';
 
 describe('AudioRecordingService', () => {
-    const apiClient = jasmine.createSpyObj<ApiClient>('ApiClient', ['getAudioStreamInfo']);
+    const apiClient = jasmine.createSpyObj<ApiClient>('ApiClient', ['getAudioStreamInfo', 'stopAudioRecording']);
     const service = new AudioRecordingService(apiClient);
 
     it('should call api endpoint to define if audio recording is working', () => {
