4f064be6e41a909e21bc54ccdd0b349f7f028b3e
Valentina <40822624+valentinabennett@users.noreply.github.com>
2020-06-01 13:37:42 +0100

    VIH-6019 IM notification display (#611)
    * feature/VIH-6019 : IM notification display
    
    * feature/VIH-6019 : add IsJudge field to IM model
    
    * feature/VIH-6019 : fix merge issue
    
    * feature/VIH-6019 : Use participants list to identify the judge
    
    * feature/VIH-6019 : refactoring, adjust  code formatting
    
    * feature/VIH-6019 : removed commer

--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/unread-messages/unread-messages.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/unread-messages/unread-messages.component.ts
@@ -3,6 +3,8 @@ import { Subscription } from 'rxjs';
 import { VideoWebService } from 'src/app/services/api/video-web.service';
 import { EventsService } from 'src/app/services/events.service';
 import { Logger } from 'src/app/services/logging/logger-base';
+import { Hearing } from '../../shared/models/hearing';
+import { InstantMessage } from '../../services/models/instant-message';
 
 @Component({
     selector: 'app-unread-messages',
@@ -10,18 +12,19 @@ import { Logger } from 'src/app/services/logging/logger-base';
     styleUrls: ['./unread-messages.component.scss']
 })
 export class UnreadMessagesComponent implements OnInit, OnDestroy {
-    @Input() conferenceId: string;
+    @Input() hearing: Hearing;
+
     messagesSubscription$: Subscription = new Subscription();
     unreadCount: number;
-    constructor(private videoWebService: VideoWebService, private eventsService: EventsService, private logger: Logger) {}
+    constructor(private videoWebService: VideoWebService, private eventsService: EventsService, private logger: Logger) { }
 
     ngOnInit() {
         this.unreadCount = 0;
         this.setupSubscribers();
         this.videoWebService
-            .getUnreadAdminMessageCountForConference(this.conferenceId)
+            .getUnreadAdminMessageCountForConference(this.hearing.id)
             .then(response => (this.unreadCount = response.number_of_unread_messages))
-            .catch(err => this.logger.error(`Failed to get unread vho messages for ${this.conferenceId}`, err));
+            .catch(err => this.logger.error(`Failed to get unread vho messages for ${this.hearing.id}`, err));
     }
 
     setupSubscribers() {
@@ -31,11 +34,25 @@ export class UnreadMessagesComponent implements OnInit, OnDestroy {
                 this.resetConferenceUnreadCounter(message);
             })
         );
+
+        this.messagesSubscription$.add(
+            this.eventsService.getChatMessage().subscribe(message => {
+                this.logger.info(`an admin has message`);
+                if (this.hearing.id === message.conferenceId && this.messageFromParticipant(message)) {
+                    this.unreadCount++;
+                }
+            })
+        );
         this.eventsService.start();
     }
 
+    private messageFromParticipant(message: InstantMessage): boolean {
+        return this.hearing.participants.map(p => p.username.toUpperCase()).includes(message.from.toUpperCase());
+    }
+
+
     resetConferenceUnreadCounter(conferenceId: string) {
-        if (this.conferenceId === conferenceId) {
+        if (this.hearing.id === conferenceId) {
             this.unreadCount = 0;
         }
     }
