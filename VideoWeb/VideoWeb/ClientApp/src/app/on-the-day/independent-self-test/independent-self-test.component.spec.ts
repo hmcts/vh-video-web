ef1e066c962e1b39c61df64bedda7f5944331fab
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-06-02 12:12:43 +0100

    remove test bed for indepedent self test remove test bed for judge chat

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/independent-self-test/independent-self-test.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/independent-self-test/independent-self-test.component.spec.ts
@@ -1,58 +1,47 @@
-import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute, convertToParamMap, Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AdalService } from 'adal-angular4';
-import { configureTestSuite } from 'ng-bullet';
+import { convertToParamMap, Router } from '@angular/router';
 import { VideoWebService } from 'src/app/services/api/video-web.service';
+import { ErrorService } from 'src/app/services/error.service';
 import { Logger } from 'src/app/services/logging/logger-base';
 import { pageUrls } from 'src/app/shared/page-url.constants';
 import { SelfTestComponent } from 'src/app/shared/self-test/self-test.component';
 import { ConferenceTestData } from 'src/app/testing/mocks/data/conference-test-data';
 import { MockAdalService } from 'src/app/testing/mocks/MockAdalService';
 import { MockLogger } from 'src/app/testing/mocks/MockLogger';
-import { MockVideoWebService } from 'src/app/testing/mocks/MockVideoService';
-import { ContactUsFoldingStubComponent } from 'src/app/testing/stubs/contact-us-stub';
-import { SelfTestStubComponent } from 'src/app/testing/stubs/self-test-stub';
 import { IndependentSelfTestComponent } from './independent-self-test.component';
 
 describe('IndependentSelfTestComponent', () => {
     let component: IndependentSelfTestComponent;
-    let fixture: ComponentFixture<IndependentSelfTestComponent>;
+    let selfTestComponent: jasmine.SpyObj<SelfTestComponent>;
     const conference = new ConferenceTestData().getConferenceDetailFuture();
-    let router: Router;
 
-    configureTestSuite(() => {
-        TestBed.configureTestingModule({
-            imports: [RouterTestingModule, HttpClientModule],
-            declarations: [IndependentSelfTestComponent, SelfTestStubComponent, ContactUsFoldingStubComponent],
-            providers: [
-                {
-                    provide: ActivatedRoute,
-                    useValue: {
-                        snapshot: {
-                            paramMap: convertToParamMap({ conferenceId: conference.id })
-                        }
-                    }
-                },
-                { provide: Logger, useClass: MockLogger },
-                { provide: AdalService, useClass: MockAdalService },
-                { provide: Logger, useClass: MockLogger },
-                { provide: VideoWebService, useClass: MockVideoWebService }
-            ]
-        }).compileComponents();
+    let router: jasmine.SpyObj<Router>;
+    const activatedRoute: any = { snapshot: { paramMap: convertToParamMap({ conferenceId: conference.id }) } };
+    const logger: Logger = new MockLogger();
+    let videoWebService: jasmine.SpyObj<VideoWebService>;
+    const mockAdalService = new MockAdalService();
+    let adalService;
+    let errorService: jasmine.SpyObj<ErrorService>;
+
+    beforeAll(() => {
+        selfTestComponent = jasmine.createSpyObj<SelfTestComponent>('SelfTestComponent', ['replayVideo']);
+        adalService = mockAdalService;
+        videoWebService = jasmine.createSpyObj<VideoWebService>('VideoWebService', ['getConferenceById', 'getPexipConfig']);
+
+        router = jasmine.createSpyObj<Router>('Router', ['navigateByUrl']);
+
+        errorService = jasmine.createSpyObj<ErrorService>('ErrorService', [
+            'goToServiceError',
+            'handleApiError',
+            'returnHomeIfUnauthorised'
+        ]);
     });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(IndependentSelfTestComponent);
-        component = fixture.componentInstance;
-        component.selfTestComponent = TestBed.createComponent(SelfTestStubComponent).componentInstance as SelfTestComponent;
-        router = TestBed.get(Router);
-        fixture.detectChanges();
+        component = new IndependentSelfTestComponent(router, activatedRoute, videoWebService, errorService, adalService, logger);
+        component.selfTestComponent = selfTestComponent;
     });
 
     it('should navigate to hearing list when equipment works', () => {
-        spyOn(router, 'navigateByUrl').and.callFake(() => Promise.resolve(true));
         component.equipmentWorksHandler();
         expect(router.navigateByUrl).toHaveBeenCalledWith(pageUrls.ParticipantHearingList);
     });
--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-chat/judge-chat.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-chat/judge-chat.component.spec.ts
@@ -1,63 +1,68 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { AdalService } from 'adal-angular4';
+import { ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
 import { Guid } from 'guid-typescript';
-import { configureTestSuite } from 'ng-bullet';
 import { ProfileService } from 'src/app/services/api/profile.service';
 import { VideoWebService } from 'src/app/services/api/video-web.service';
+import { ConferenceResponse } from 'src/app/services/clients/api-client';
 import { EventsService } from 'src/app/services/events.service';
-import { Logger } from 'src/app/services/logging/logger-base';
 import { Hearing } from 'src/app/shared/models/hearing';
 import { ConferenceTestData } from 'src/app/testing/mocks/data/conference-test-data';
 import { MockAdalService } from 'src/app/testing/mocks/MockAdalService';
 import { MockEventsService } from 'src/app/testing/mocks/MockEventService';
 import { MockLogger } from 'src/app/testing/mocks/MockLogger';
 import { MockProfileService } from 'src/app/testing/mocks/MockProfileService';
-import { MockVideoWebService } from 'src/app/testing/mocks/MockVideoService';
-import { ChatInputBoxStubComponent } from 'src/app/testing/stubs/chat-input-box-stub.component';
 import { JudgeChatComponent } from './judge-chat.component';
 
 describe('JudgeChatComponent', () => {
     let component: JudgeChatComponent;
     let fixture: ComponentFixture<JudgeChatComponent>;
-    let eventService: MockEventsService;
-    let adalService: MockAdalService;
-    let profileService: MockProfileService;
-    const conference = new ConferenceTestData().getConferenceDetailFuture();
-    const hearing = new Hearing(conference);
-    const judgeUsername = 'judge.fudge@hearings.net';
-    const videoWebService = new MockVideoWebService();
-    videoWebService.username = judgeUsername;
+    let conference: ConferenceResponse;
+    let hearing: Hearing;
 
-    configureTestSuite(() => {
-        TestBed.configureTestingModule({
-            declarations: [JudgeChatComponent, ChatInputBoxStubComponent],
-            providers: [
-                { provide: AdalService, useClass: MockAdalService },
-                { provide: VideoWebService, useValue: videoWebService },
-                { provide: ProfileService, useClass: MockProfileService },
-                { provide: Logger, useClass: MockLogger },
-                { provide: EventsService, useClass: MockEventsService }
-            ]
-        }).compileComponents();
+    let videoWebService: jasmine.SpyObj<VideoWebService>;
+    const judgeUsername = 'judge.fudge@hearings.net';
+    let eventsService: jasmine.SpyObj<EventsService>;
+    let profileService: jasmine.SpyObj<ProfileService>;
+    const mockProfileService = new MockProfileService();
+    const mockAdalService = new MockAdalService();
+    const mockEventsService = new MockEventsService();
+    let adalService;
+
+    beforeAll(() => {
+        adalService = mockAdalService;
+        conference = new ConferenceTestData().getConferenceDetailFuture();
+        hearing = new Hearing(conference);
+        videoWebService = jasmine.createSpyObj<VideoWebService>('VideoWebService', ['getConferenceChatHistory']);
+        eventsService = jasmine.createSpyObj<EventsService>('EventsService', ['start', 'getChatMessage', 'sendMessage']);
+        profileService = jasmine.createSpyObj<ProfileService>('ProfileService', ['checkCacheForProfileByUsername', 'getProfileByUsername']);
     });
 
     beforeEach(() => {
-        eventService = TestBed.get(EventsService);
-        adalService = TestBed.get(AdalService);
-        profileService = TestBed.get(ProfileService);
-
         adalService.userInfo.userName = judgeUsername;
+        const chatHistory = new ConferenceTestData().getChatHistory(mockAdalService.userInfo.userName, conference.id);
 
-        fixture = TestBed.createComponent(JudgeChatComponent);
-        component = fixture.componentInstance;
+        profileService.checkCacheForProfileByUsername.and.returnValue(null);
+        profileService.getProfileByUsername.and.resolveTo(mockProfileService.mockProfile);
+        videoWebService.getConferenceChatHistory.and.resolveTo(chatHistory);
+
+        eventsService.getChatMessage.and.returnValue(mockEventsService.messageSubject.asObservable());
+
+        component = new JudgeChatComponent(videoWebService, profileService, eventsService, new MockLogger(), adalService);
+
         component.hearing = hearing;
-        component.messages = new ConferenceTestData().getChatHistory(judgeUsername, conference.id);
-        fixture.detectChanges();
+        component.messages = new ConferenceTestData().getChatHistory('vho.user@hearings.net', conference.id);
     });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
+    afterEach(() => {
+        component.ngOnDestroy();
     });
+
+    it('should get chat history and subscribe', fakeAsync(() => {
+        component.messages = undefined;
+        component.ngOnInit();
+        tick();
+        expect(component.loading).toBeFalsy();
+        expect(component.messages.length).toBeGreaterThan(0);
+    }));
 
     it('should toggle show chat state', () => {
         component.showChat = false;
@@ -95,18 +100,16 @@ describe('JudgeChatComponent', () => {
     });
 
     it('should get first name and flag isJudge when message from user not in conference', async () => {
-        await fixture.whenStable();
         const username = 'vhofficer.hearings.net';
-        const expectedFirstName = profileService.mockProfile.first_name;
+        const expectedFirstName = mockProfileService.mockProfile.first_name;
         const messageInfo = await component.assignMessageFrom(username);
         expect(messageInfo).toEqual(expectedFirstName);
     });
 
     it('should call api when local cache does not have user profile', async () => {
-        await fixture.whenStable();
         const username = 'vhofficer.hearings.net';
-        spyOn(profileService, 'checkCacheForProfileByUsername').and.returnValue(null);
-        const expectedFirstName = profileService.mockProfile.first_name;
+        profileService.checkCacheForProfileByUsername.and.returnValue(null);
+        const expectedFirstName = mockProfileService.mockProfile.first_name;
 
         const messageInfo = await component.assignMessageFrom(username);
         expect(messageInfo).toEqual(expectedFirstName);
@@ -152,9 +155,8 @@ describe('JudgeChatComponent', () => {
     });
 
     it('should send message to hub', () => {
-        spyOn(eventService, 'sendMessage');
         const message = 'test';
         component.sendMessage(message);
-        expect(eventService.sendMessage).toHaveBeenCalledWith(conference.id, message);
+        expect(eventsService.sendMessage).toHaveBeenCalledWith(conference.id, message);
     });
 });
