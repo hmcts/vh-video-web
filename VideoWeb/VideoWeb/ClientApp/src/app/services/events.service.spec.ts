9b2857454cbf288e238533278f29e864c021813a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-06-12 11:15:57 +0100

    verify event service setup

--- VideoWeb/VideoWeb/ClientApp/src/app/services/events.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/events.service.spec.ts
@@ -7,25 +7,62 @@ import { MockLogger } from '../testing/mocks/MockLogger';
 import { ConfigService } from './api/config.service';
 import { EventsService } from './events.service';
 import { Logger } from './logging/logger-base';
+import { Subject, Subscription } from 'rxjs';
+import { InstantMessage } from './models/instant-message';
+import { Guid } from 'guid-typescript';
 
 describe('EventsService', () => {
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [
-                EventsService,
-                { provide: AdalService, useClass: MockAdalService },
-                { provide: ConfigService, useClass: MockConfigService },
-                { provide: Logger, useClass: MockLogger }
-            ]
-        });
+    let service: EventsService;
+    const mockAdalService = new MockAdalService();
+    let adalService;
+    const logger: Logger = new MockLogger();
+    const subscription$ = new Subscription();
+
+    beforeAll(() => {
+        adalService = mockAdalService;
+        service = new EventsService(adalService, logger);
+
+        service.connection = new signalR.HubConnectionBuilder()
+            .configureLogging(signalR.LogLevel.Debug)
+            .withAutomaticReconnect([0])
+            .withUrl('/eventhub', {
+                accessTokenFactory: () => this.adalService.userInfo.token
+            })
+            .build();
     });
 
-    it('should start if not connected', inject([EventsService], async (service: EventsService) => {
+    afterEach(() => subscription$.unsubscribe());
+
+    it('should init observables', () => {
+        subscription$.add(service.getServiceReconnected().subscribe());
+        subscription$.add(service.getServiceDisconnected().subscribe());
+        subscription$.add(service.getParticipantStatusMessage().subscribe());
+        subscription$.add(service.getHearingStatusMessage().subscribe());
+        subscription$.add(service.getConsultationMessage().subscribe());
+        subscription$.add(service.getAdminConsultationMessage().subscribe());
+        subscription$.add(service.getChatMessage().subscribe());
+        subscription$.add(service.getAdminAnsweredChat().subscribe());
+        subscription$.add(service.getHeartbeat().subscribe());
+        expect(subscription$).toBeTruthy();
+    });
+
+    it('should start if not connected', async () => {
         spyOn(service.connection, 'start').and.callFake(() => Promise.resolve());
         await service.start();
         expect(service.reconnectionAttempt).toBe(0);
-    }));
-    it('should not start if connected', inject([EventsService], (service: EventsService) => {
+    });
+
+    it('should retry to connect on failure', async () => {
+        service.reconnectionAttempt = 0;
+        service.retryDelayTime = 1;
+        spyOn(service.connection, 'start').and.returnValues(Promise.reject('Unable to connect auto test'), Promise.resolve());
+        const sub$ = service.getServiceDisconnected();
+        await service.start();
+        expect(service.reconnectionAttempt).toBe(0);
+        expect(service.connection.start).toHaveBeenCalledTimes(2);
+    });
+
+    it('should not start if connected', () => {
         const spy = spyOnProperty(service.connection, 'state').and.returnValue(signalR.HubConnectionState.Disconnected);
         spyOn(service.connection, 'start').and.callFake(() => {
             spy.and.returnValue(signalR.HubConnectionState.Connected);
@@ -34,10 +71,28 @@ describe('EventsService', () => {
         service.start();
         service.start();
         expect(service.connection.start).toHaveBeenCalledTimes(1);
-    }));
-    it('should stop', inject([EventsService], (service: EventsService) => {
+    });
+
+    it('should stop', () => {
         spyOn(service.connection, 'stop').and.callFake(() => Promise.resolve());
         service.stop();
         expect(service.connection.stop).toHaveBeenCalled();
-    }));
+    });
+
+    it('should send im to "SendMessage" method', async () => {
+        const imTest = new InstantMessage({
+            conferenceId: Guid.create().toString(),
+            id: Guid.create().toString(),
+            to: 'test@to.com',
+            from: 'other@from.com',
+            from_display_name: 'You',
+            message: 'i have sent',
+            is_user: true,
+            timestamp: new Date(new Date().toUTCString())
+        });
+        spyOn(service.connection, 'send');
+        await service.sendMessage(imTest);
+
+        expect(service.connection.send).toHaveBeenCalledWith('SendMessage', imTest.conferenceId, imTest.message, imTest.to, imTest.id);
+    });
 });
