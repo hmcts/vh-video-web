8690f7703ae3dd1716beddfbd3e1a157d550426b
atif.rehman <atif.rehman@outlook.com>
2020-06-30 16:31:06 +0100

    feature/VIH-6136 : Clear session storage on logout

--- VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
@@ -1,6 +1,8 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { AdalService } from 'adal-angular4';
 import { ProfileService } from 'src/app/services/api/profile.service';
+import { SessionStorage } from 'src/app/services/session-storage';
+import { VhoStorageKeys } from '../../vh-officer/services/models/session-keys';
 
 @Component({
     selector: 'app-logout',
@@ -8,12 +10,16 @@ import { ProfileService } from 'src/app/services/api/profile.service';
 })
 @Injectable()
 export class LogoutComponent implements OnInit {
+    private readonly judgeAllocationStorage: SessionStorage<string[]>;
     readonly loginPath = '../login';
-    constructor(private adalSvc: AdalService, private profileService: ProfileService) {}
+    constructor(private adalSvc: AdalService, private profileService: ProfileService) {
+        this.judgeAllocationStorage = new SessionStorage<string[]>(VhoStorageKeys.VENUE_ALLOCATIONS_KEY);
+    }
 
     ngOnInit() {
         if (this.adalSvc.userInfo.authenticated) {
             this.profileService.clearUserProfile();
+            this.judgeAllocationStorage.clear();
             this.adalSvc.logOut();
         }
     }
