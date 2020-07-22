b07b443d2af398b9ad42fb6bf0046999a876c6e6
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-07-22 12:05:36 +0100

    VIH-5629 add app version to health check

--- VideoWeb/VideoWeb.Contract/Responses/HealthCheckResponse.cs
+++ VideoWeb/VideoWeb.Contract/Responses/HealthCheckResponse.cs
@@ -9,10 +9,12 @@ namespace VideoWeb.Contract.Responses
             BookingsApiHealth = new HealthCheck();
             UserApiHealth = new HealthCheck();
             VideoApiHealth = new HealthCheck();
+            AppVersion = new ApplicationVersion();
         }
         public HealthCheck BookingsApiHealth { get; set; }
         public HealthCheck UserApiHealth { get; set; }
         public HealthCheck VideoApiHealth { get; set; }
+        public ApplicationVersion AppVersion { get; set; }
 
     }
 
@@ -21,5 +23,11 @@ namespace VideoWeb.Contract.Responses
         public bool Successful { get; set; }
         public string ErrorMessage { get; set; }
         public IDictionary Data { get; set; }
+    }
+
+    public class ApplicationVersion
+    {
+        public string FileVersion { get; set; }
+        public string InformationVersion { get; set; }
     }
 }
