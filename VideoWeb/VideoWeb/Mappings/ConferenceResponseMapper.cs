12a6971ae935242ff896c00012f0afd7698b0502
Valentina <Valentina.bennett@hmcts.net>
2020-07-27 16:24:36 +0100

    feature/VIH-6271 : set T position for more then 5 paticipants

--- VideoWeb/VideoWeb/Mappings/ConferenceResponseMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ConferenceResponseMapper.cs
@@ -48,18 +48,30 @@ namespace VideoWeb.Mappings
             response.PexipSelfTestNodeUri = conference.Meeting_room.Pexip_self_test_node;
 
             var tiledParticipants = conference.Participants.Where(x =>
-                x.User_role == UserRole.Individual || x.User_role == UserRole.Representative).ToList();
-
-            var partyGroups = tiledParticipants.GroupBy(x => x.Case_type_group).ToList();
-            foreach (var group in partyGroups)
+             x.User_role == UserRole.Individual || x.User_role == UserRole.Representative).ToList();
+            if (tiledParticipants.Count > 4)
             {
-                var pats = group.ToList();
-                var position = partyGroups.IndexOf(group) + 1;
-                foreach (var p in pats)
+                // If the number of participants is more than 4, then simply increment the tile numbers
+                var position = 0;
+                foreach (var participant in response.Participants)
                 {
-                    var participant = response.Participants.Find(x => x.Id == p.Id);
-                    participant.TiledDisplayName = $"T{position};{participant.DisplayName};{participant.Id}";
-                    position += 2;
+                    participant.TiledDisplayName = $"T{position + 1 };{participant.DisplayName};{participant.Id}";
+                    position++;
+                }
+            }
+            else
+            {
+                var partyGroups = tiledParticipants.GroupBy(x => x.Case_type_group).ToList();
+                foreach (var group in partyGroups)
+                {
+                    var pats = group.ToList();
+                    var position = partyGroups.IndexOf(group) + 1;
+                    foreach (var p in pats)
+                    {
+                        var participant = response.Participants.Find(x => x.Id == p.Id);
+                        participant.TiledDisplayName = $"T{position};{participant.DisplayName};{participant.Id}";
+                        position += 2;
+                    }
                 }
             }
 
