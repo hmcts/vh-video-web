fa0942c94ccc1ccb2bcf525a4983019ae3f340b0
Valentina <40822624+valentinabennett@users.noreply.github.com>
2020-07-27 17:33:45 +0100

    VIH-6271 set T position for more then 5 participants (#780)
    * feature/VIH-6271 : set T position for more then 5 paticipants
    
    * feature/VIH-6271 : amended duplicated code

--- VideoWeb/VideoWeb.UnitTests/Mappings/ConferenceResponseMapperTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Mappings/ConferenceResponseMapperTests.cs
@@ -81,5 +81,71 @@ namespace VideoWeb.UnitTests.Mappings
             response.PexipNodeUri.Should().Be(meetingRoom.Pexip_node);
             response.PexipSelfTestNodeUri.Should().NotBeNullOrWhiteSpace();
         }
+
+        [Test]
+        public void Should_map_all_properties_for_more_then_4_participants()
+        {
+            var participants = new List<ParticipantDetailsResponse>
+            {
+                new ParticipantDetailsResponseBuilder(UserRole.Individual, "Claimant").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Individual, "Defendant").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Representative, "Defendant").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Judge, "None").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.CaseAdmin, "None").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Individual, "Observer").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Individual, "Panel Member").Build(),
+                new ParticipantDetailsResponseBuilder(UserRole.Individual, "Panel Member").Build()
+            };
+
+
+            var expectedConferenceStatus = ConferenceStatus.Suspended;
+
+            var meetingRoom = Builder<MeetingRoomResponse>.CreateNew().Build();
+
+            var conference = Builder<ConferenceDetailsResponse>.CreateNew()
+                .With(x => x.Current_status = ConferenceState.Suspended)
+                .With(x => x.Participants = participants)
+                .With(x => x.Meeting_room = meetingRoom)
+                .Build();
+
+            var response = ConferenceResponseMapper.MapConferenceDetailsToResponseModel(conference);
+
+            response.Id.Should().Be(conference.Id);
+            response.CaseName.Should().Be(conference.Case_name);
+            response.CaseType.Should().Be(conference.Case_type);
+            response.CaseNumber.Should().Be(conference.Case_number);
+            response.ScheduledDateTime.Should().Be(conference.Scheduled_date_time);
+            response.ScheduledDuration.Should().Be(conference.Scheduled_duration);
+            response.Status.Should().Be(expectedConferenceStatus);
+
+            var participantsResponse = response.Participants;
+            participantsResponse.Should().NotBeNullOrEmpty();
+
+            var tiledNames = participantsResponse.Select(x => x.TiledDisplayName).ToList();
+
+            foreach (var participantResponse in participantsResponse)
+            {
+                var position = participantResponse.TiledDisplayName.Split(';');
+                if (position[0].StartsWith("T"))
+                {
+                    tiledNames.Count(x => x.StartsWith(position[0])).Should().Be(1);
+
+                }
+             
+            }
+
+            var caseTypeGroups = participantsResponse.Select(p => p.CaseTypeGroup).Distinct().ToList();
+            caseTypeGroups.Count.Should().BeGreaterThan(2);
+            caseTypeGroups[0].Should().Be("Claimant");
+            caseTypeGroups[1].Should().Be("Defendant");
+            caseTypeGroups[2].Should().Be("None");
+            caseTypeGroups[3].Should().Be("Observer");
+            caseTypeGroups[4].Should().Be("Panel Member");
+
+            response.JudgeIFrameUri.Should().Be(meetingRoom.Judge_uri);
+            response.ParticipantUri.Should().Be(meetingRoom.Participant_uri);
+            response.PexipNodeUri.Should().Be(meetingRoom.Pexip_node);
+            response.PexipSelfTestNodeUri.Should().NotBeNullOrWhiteSpace();
+        }
     }
 }
