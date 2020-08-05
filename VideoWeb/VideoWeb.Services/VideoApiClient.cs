0ae2dda45e3debb90b6d29154c462dc53ca68f53
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-08-05 15:51:37 +0100

    VIH-6181 update video api client

--- VideoWeb/VideoWeb.Services/VideoApiClient.cs
+++ VideoWeb/VideoWeb.Services/VideoApiClient.cs
@@ -6697,6 +6697,12 @@ namespace VideoWeb.Services.Video
         [System.Runtime.Serialization.EnumMember(Value = @"ParticipantNotSignedIn")]
         ParticipantNotSignedIn = 14,
     
+        [System.Runtime.Serialization.EnumMember(Value = @"Start")]
+        Start = 15,
+    
+        [System.Runtime.Serialization.EnumMember(Value = @"CountdownFinished")]
+        CountdownFinished = 16,
+    
     }
     
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.1.4.0 (Newtonsoft.Json v12.0.0.0)")]
