bf47f5bb14892cdf36c72897c6f47eb2b16b9f75
Lionel Roche <lionel.roche@hmcts.net>
2020-08-10 17:37:23 +0100

    - removed subscription.

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-alert/participant-alert.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-alert/participant-alert.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Subscription } from 'rxjs';
 import { EventsService } from 'src/app/services/events.service';
-import { HeartbeatHealth, ParticipantHeartbeat } from 'src/app/services/models/participant-heartbeat';
 import { ParticipantNetworkHealthBaseComponent } from '../participant-network-health/participant-net-health-base.component';
 
 @Component({
@@ -10,19 +8,7 @@ import { ParticipantNetworkHealthBaseComponent } from '../participant-network-he
     styleUrls: ['../participants-panel/participants-panel.component.scss', './participant-alert.component.scss']
 })
 export class ParticipantAlertComponent extends ParticipantNetworkHealthBaseComponent {
-    eventSubscriptions$ = new Subscription();
-    networkHealth?: HeartbeatHealth;
     constructor(protected eventsService: EventsService) {
         super(eventsService);
-    }
-
-    setupSubscribers() {
-        this.eventSubscriptions$.add(this.eventsService.getHeartbeat().subscribe(heartbeat => this.handleHeartbeat(heartbeat)));
-    }
-
-    handleHeartbeat(heartbeat: ParticipantHeartbeat): void {
-        if (this.participant.id === heartbeat.participantId) {
-            this.networkHealth = heartbeat.heartbeatHealth;
-        }
     }
 }
--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-network-health/participant-net-health-base.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-network-health/participant-net-health-base.component.ts
@@ -11,6 +11,9 @@ export abstract class ParticipantNetworkHealthBaseComponent implements OnInit, O
     constructor(protected eventsService: EventsService) {}
 
     get isNetworkGood() {
+        console.log('ParticipantNetworkHealthBaseComponent -> PARTICIPANT ' + this.participant.display_name);
+        console.log('ParticipantNetworkHealthBaseComponent -> PARTICIPANT ID ' + this.participant.id);
+        console.log('ParticipantNetworkHealthBaseComponent -> NETWORK HEALTH ' + this.networkHealth);
         return this.networkHealth && this.networkHealth === HeartbeatHealth.Good;
     }
 
@@ -31,6 +34,7 @@ export abstract class ParticipantNetworkHealthBaseComponent implements OnInit, O
     }
 
     handleHeartbeat(heartbeat: ParticipantHeartbeat): void {
+        console.log('ParticipantNetworkHealthBaseComponent -> heartbeat ' + heartbeat);
         if (this.participant.id === heartbeat.participantId) {
             this.networkHealth = heartbeat.heartbeatHealth;
         }
