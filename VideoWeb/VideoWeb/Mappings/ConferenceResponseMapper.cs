e407ea82d3803acd25bc9f14d66a59eb73a82353
Valentina <Valentina.bennett@hmcts.net>
2020-08-27 19:28:52 +0100

    feature/VIH-6317 : refactoring conference response mapper

--- VideoWeb/VideoWeb/Mappings/ConferenceResponseMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ConferenceResponseMapper.cs
@@ -17,16 +17,6 @@ namespace VideoWeb.Mappings
                 status = ConferenceStatus.NotStarted;
             }
 
-            conference.Participants ??= new List<ParticipantDetailsResponse>();
-
-            var participants = conference.Participants
-                .OrderBy(x => x.Case_type_group)
-                .Select(x => ParticipantResponseMapper.MapParticipantToResponseModel(x))
-                .ToList();
-
-            conference.Endpoints ??= new List<EndpointResponse>();
-            var endpoints = conference.Endpoints.Select(x => EndpointsResponseMapper.Map(x)).ToList();
-
             var response = new ConferenceResponse
             {
                 Id = conference.Id,
@@ -36,51 +26,53 @@ namespace VideoWeb.Mappings
                 ScheduledDateTime = conference.Scheduled_date_time,
                 ScheduledDuration = conference.Scheduled_duration,
                 Status = status,
-                Participants = participants,
+                Participants = MapParticipants(conference),
                 ClosedDateTime = conference.Closed_date_time,
                 HearingVenueName = conference.Hearing_venue_name,
                 AudioRecordingRequired = conference.Audio_recording_required,
                 HearingRefId = conference.Hearing_id,
-                Endpoints = endpoints
+                Endpoints = MapEndpoints(conference)
             };
 
-            if (conference.Meeting_room == null) return response;
-
-            response.ParticipantUri = conference.Meeting_room.Participant_uri;
-            response.PexipNodeUri = conference.Meeting_room.Pexip_node;
-            response.PexipSelfTestNodeUri = conference.Meeting_room.Pexip_self_test_node;
-
-            var tiledParticipants = conference.Participants.Where(x =>
-             x.User_role == UserRole.Individual || x.User_role == UserRole.Representative).ToList();
-            if (tiledParticipants.Count > 4)
+            if (conference.Meeting_room != null)
             {
-                // If the number of participants is more than 4, then simply increment the tile numbers
-                var position = 1;
-                foreach (var participant in response.Participants)
+
+                response.ParticipantUri = conference.Meeting_room.Participant_uri;
+                response.PexipNodeUri = conference.Meeting_room.Pexip_node;
+                response.PexipSelfTestNodeUri = conference.Meeting_room.Pexip_self_test_node;
+
+                var tiledParticipants = GetNotJudgeParticipant(conference);
+
+                if (tiledParticipants.Count > 4)
                 {
-                    if (participant.Role == Role.Judge)
+                    // If the number of participants is more than 4, then simply increment the tile numbers
+                    var position = 1;
+                    foreach (var participant in response.Participants)
                     {
-                        participant.TiledDisplayName = GetTiledDisplayName(participant, 0);
-                    }
-                    else
-                    {
-                        participant.TiledDisplayName = GetTiledDisplayName(participant, position);
-                        position++;
+                        if (participant.Role == Role.Judge)
+                        {
+                            participant.TiledDisplayName = GetTiledDisplayName(participant, 0);
+                        }
+                        else
+                        {
+                            participant.TiledDisplayName = GetTiledDisplayName(participant, position);
+                            position++;
+                        }
                     }
                 }
-            }
-            else
-            {
-                var partyGroups = tiledParticipants.GroupBy(x => x.Case_type_group).ToList();
-                foreach (var group in partyGroups)
+                else
                 {
-                    var pats = group.ToList();
-                    var position = partyGroups.IndexOf(group) + 1;
-                    foreach (var p in pats)
+                    var partyGroups = tiledParticipants.GroupBy(x => x.Case_type_group).ToList();
+                    foreach (var group in partyGroups)
                     {
-                        var participant = response.Participants.Find(x => x.Id == p.Id);
-                        participant.TiledDisplayName = GetTiledDisplayName(participant, position);
-                        position += 2;
+                        var pats = group.ToList();
+                        var position = partyGroups.IndexOf(group) + 1;
+                        foreach (var p in pats)
+                        {
+                            var participant = response.Participants.Find(x => x.Id == p.Id);
+                            participant.TiledDisplayName = GetTiledDisplayName(participant, position);
+                            position += 2;
+                        }
                     }
                 }
             }
@@ -92,5 +84,27 @@ namespace VideoWeb.Mappings
         {
             return $"T{position};{participant.DisplayName};{participant.Id}";
         }
+
+        private static List<ParticipantResponse> MapParticipants(ConferenceDetailsResponse conference)
+        {
+            conference.Participants ??= new List<ParticipantDetailsResponse>();
+            return conference.Participants
+                .OrderBy(x => x.Case_type_group)
+                .Select(x => ParticipantResponseMapper.MapParticipantToResponseModel(x))
+                .ToList();
+        }
+
+        private static List<VideoEndpointResponse> MapEndpoints(ConferenceDetailsResponse conference)
+        {
+            conference.Endpoints ??= new List<EndpointResponse>();
+            return conference.Endpoints.Select(x => EndpointsResponseMapper.Map(x)).ToList();
+        }
+
+        private static List<ParticipantDetailsResponse> GetNotJudgeParticipant(ConferenceDetailsResponse conference)
+        {
+            return conference.Participants.Where(x =>
+             x.User_role == UserRole.Individual || x.User_role == UserRole.Representative).ToList();
+        }
+       
     }
 }
