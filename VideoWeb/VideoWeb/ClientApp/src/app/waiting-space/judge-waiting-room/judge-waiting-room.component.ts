c0215d52fb6b512a2072b657d31cb6cac61d1052
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-09-28 17:15:18 +0100

    VIH-6282 store layout for each conference

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
@@ -121,7 +121,7 @@ export class JudgeWaitingRoomComponent extends WaitingRoomBaseComponent implemen
     }
 
     startHearing() {
-        this.videoCallService.startHearing(this.hearing.id, this.videoCallService.getPreferredLayout());
+        this.videoCallService.startHearing(this.hearing.id, this.videoCallService.getPreferredLayout(this.conference.id));
     }
 
     goToJudgeHearingList(): void {
