2828eb7d491ed46595785ee8c991a2396e4d6097
Valentina <Valentina.bennett@hmcts.net>
2020-10-16 17:17:30 +0100

    feature/VIH-6315 : formatting issue

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/tests/judge-waiting-room.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/tests/judge-waiting-room.component.spec.ts
@@ -340,7 +340,7 @@ describe('JudgeWaitingRoomComponent when conference exists', () => {
         flag = component.getShowDialogChooseDevice();
         expect(flag).toBe(true);
     });
-    it('should on consultation accept stop streams for devices and close choose device popup', async() => {
+    it('should on consultation accept stop streams for devices and close choose device popup', async () => {
         component.displayDeviceChangeModal = true;
         await component.onConsultationAccepted();
 
