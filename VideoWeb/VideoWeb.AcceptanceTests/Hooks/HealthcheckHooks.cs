9b56e1a796543bf1d4d0434f4e61b8eb01e265cd
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2020-10-19 16:22:27 +0200

    testfix/VIH-5255 : Retry on Forbidden firewall issues

--- VideoWeb/VideoWeb.AcceptanceTests/Hooks/HealthcheckHooks.cs
+++ VideoWeb/VideoWeb.AcceptanceTests/Hooks/HealthcheckHooks.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Net;
 using FluentAssertions;
+using Polly;
+using RestSharp;
 using TechTalk.SpecFlow;
 using VideoWeb.AcceptanceTests.Helpers;
 
@@ -8,12 +11,22 @@ namespace VideoWeb.AcceptanceTests.Hooks
     [Binding]
     public class HealthcheckHooks
     {
+        private const int RETRIES = 4;
+
         [BeforeScenario(Order = (int) HooksSequence.HealthcheckHooks)]
         public void CheckApiHealth(TestContext context)
         {
-            var response = context.Apis.TestApi.HealthCheck();
-            response.StatusCode.Should().Be(HttpStatusCode.OK,
-                $"Healthcheck failed with '{response.StatusCode}' and error message '{response.ErrorMessage}'");
+            var retryOnForbiddenFirewallExceptions = Policy
+                .HandleResult<IRestResponse>(r => r.StatusCode == HttpStatusCode.Forbidden)
+                .WaitAndRetry(RETRIES, retryAttempt =>
+                        TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                    (exception, timeSpan) =>
+                    {
+                        NUnit.Framework.TestContext.WriteLine($"Encountered error '{exception.Result.StatusCode}' after {timeSpan.Seconds} seconds. Retrying...");
+                    });
+
+            var response = retryOnForbiddenFirewallExceptions.Execute(() => context.Apis.TestApi.HealthCheck());
+            response.StatusCode.Should().Be(HttpStatusCode.OK, $"Healthcheck failed with '{response.StatusCode}' and error message '{response.ErrorMessage}'");
         }
     }
 }
