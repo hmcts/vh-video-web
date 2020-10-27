7b6b9c1425bd9ef4085cf8cf4c2c151691772d78
Shaed Parkar <shaed.parkar@slimsolutions.company>
2020-10-27 17:11:32 +0000

    VIH-6681 introduce global error handler to capture uncaught errors

--- VideoWeb/VideoWeb/ClientApp/src/app/app.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { AdalGuard, AdalInterceptor, AdalService } from 'adal-angular4';
@@ -17,9 +17,10 @@ import { LoggerService, LOG_ADAPTER } from './services/logging/logger.service';
 import { AppInsightsLoggerService } from './services/logging/loggers/app-insights-logger.service';
 import { ConsoleLogger } from './services/logging/loggers/console-logger';
 import { PageTrackerService } from './services/page-tracker.service';
+import { ParticipantStatusUpdateService } from './services/participant-status-update.service';
+import { GlobalErrorHandler } from './shared/providers/global-error-handler';
 import { SharedModule } from './shared/shared.module';
 import { WaitingSpaceModule } from './waiting-space/waiting-space.module';
-import { ParticipantStatusUpdateService } from './services/participant-status-update.service';
 
 export function getSettings(configService: ConfigService) {
     return () => configService.loadConfig();
@@ -46,6 +47,7 @@ export function getSettings(configService: ConfigService) {
         AdalService,
         AdalGuard,
         { provide: HTTP_INTERCEPTORS, useClass: AdalInterceptor, multi: true },
+        { provide: ErrorHandler, useClass: GlobalErrorHandler },
         ConfigService,
         AuthGuard,
         Title,
---
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/providers/global-error-handler.ts
@@ -0,0 +1,13 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { Logger } from 'src/app/services/logging/logger-base';
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+    private logger: Logger;
+    constructor(injector: Injector) {
+        this.logger = injector.get(Logger);
+    }
+
+    handleError(error: Error) {
+        this.logger.error('Unexepected error', error);
+    }
+}
