114a467370f3185bf986fce327cf72df7c1fa593
Viren Sareen <viren.sareen@solirius.com>
2020-11-24 13:28:29 +0000

    fixed linting

--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/copy-telephone-id/copy-telephone-id.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/copy-telephone-id/copy-telephone-id.component.ts
@@ -2,22 +2,22 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
-  selector: 'app-copy-telephone-id',
-  templateUrl: './copy-telephone-id.component.html',
-  styleUrls: ['./copy-telephone-id.component.scss']
+    selector: 'app-copy-telephone-id',
+    templateUrl: './copy-telephone-id.component.html',
+    styleUrls: ['./copy-telephone-id.component.scss']
 })
 export class CopyTelephoneIdComponent implements OnInit {
     @Input() telephoneId: string;
     tooltip: string;
 
-    constructor(private clipboardService: ClipboardService) { }
+    constructor(private clipboardService: ClipboardService) {}
 
     ngOnInit(): void {
-      this.tooltip = 'Copy joining by phone details to clipboard';
+        this.tooltip = 'Copy joining by phone details to clipboard';
     }
 
     copyToClipboard() {
-      this.clipboardService.copyFromContent(this.telephoneId);
-      this.tooltip = 'Details copied to clipboard';
+        this.clipboardService.copyFromContent(this.telephoneId);
+        this.tooltip = 'Details copied to clipboard';
     }
 }
