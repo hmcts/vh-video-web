cce3b48ae7de48a6a27b5d8f77e353af2988d068
Lionel Roche <lionel.roche@hmcts.net>
2020-11-27 12:53:20 +0000

    - lint fix

--- VideoWeb/VideoWeb/ClientApp/src/app/services/error.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/error.service.spec.ts
@@ -114,7 +114,7 @@ describe('ErrorService', () => {
         const error = new CallError('Error connecting to conference');
         const conferenceId = Guid.create().toString();
         service.handlePexipError(error, conferenceId);
-        expect(service.goToServiceError).toHaveBeenCalledWith('There\'s a problem with your connection');
+        expect(service.goToServiceError).toHaveBeenCalledWith("There's a problem with your connection");
     }));
 
     it('should navigate to service error with connection lost message when pexip error message has firewall or browser extensions issue', inject(
