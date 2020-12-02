b967442b0253f956d65311840867a3832767387e
Sunil DSouza <sunil.dsouza111@hmcts.net>
2020-12-02 17:52:05 +0000

    Fixed unnecessary reference

--- VideoWeb/VideoWeb/ClientApp/src/app/services/error.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/error.service.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { ErrorMessage } from '../shared/models/error-message';
 import { pageUrls } from '../shared/page-url.constants';
 import { CallError } from '../waiting-space/models/video-call-models';
-import { HealthCheckService } from './api/healthcheck.service';
 import { ApiException } from './clients/api-client';
 import { Logger } from './logging/logger-base';
 import { SessionStorage } from './session-storage';
