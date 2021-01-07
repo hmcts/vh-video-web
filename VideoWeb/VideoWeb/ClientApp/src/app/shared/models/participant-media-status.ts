ac6e770d9f4c7ea7a0d738c3f8dd0762ca1cf380
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2021-01-07 15:53:32 +0000

    VIH-6829 include participant video muted in EventHub Client (#1114)

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/models/participant-media-status.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/models/participant-media-status.ts
@@ -1,4 +1,4 @@
 export class ParticipantMediaStatus {
     // tslint:disable-next-line: variable-name
-    constructor(public is_local_audio_muted: boolean) {}
+    constructor(public is_local_audio_muted: boolean, public is_local_video_muted: boolean) {}
 }
