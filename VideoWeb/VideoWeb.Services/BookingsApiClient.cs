e267dc6a76c5165ababb5faa403c65089217feae
Nicholas Poxon <NicholasPoxon1@hmcts.net>
2021-01-28 10:55:24 +0100

    feature/VIH-7058 : Added missing package for Windows User Secrets

--- VideoWeb/VideoWeb.Services/BookingsApiClient.cs
+++ VideoWeb/VideoWeb.Services/BookingsApiClient.cs
@@ -4148,13 +4148,14 @@ namespace VideoWeb.Services.Bookings
     
     }
     
-    /// <summary>Case role model</summary>
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.1.4.0 (Newtonsoft.Json v12.0.0.0)")]
     public partial class CaseRoleResponse 
     {
-        /// <summary>Name of a case role</summary>
         [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Name { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("user_role", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string User_role { get; set; }
     
         private System.Collections.Generic.IDictionary<string, object> _additionalProperties = new System.Collections.Generic.Dictionary<string, object>();
     
@@ -4201,13 +4202,14 @@ namespace VideoWeb.Services.Bookings
     
     }
     
-    /// <summary>Hearing role model</summary>
     [System.CodeDom.Compiler.GeneratedCode("NJsonSchema", "10.1.4.0 (Newtonsoft.Json v12.0.0.0)")]
     public partial class HearingRoleResponse 
     {
-        /// <summary>Name of a hearing role</summary>
         [Newtonsoft.Json.JsonProperty("name", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Name { get; set; }
+    
+        [Newtonsoft.Json.JsonProperty("user_role", Required = Newtonsoft.Json.Required.Default, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
+        public string User_role { get; set; }
     
         private System.Collections.Generic.IDictionary<string, object> _additionalProperties = new System.Collections.Generic.Dictionary<string, object>();
     
