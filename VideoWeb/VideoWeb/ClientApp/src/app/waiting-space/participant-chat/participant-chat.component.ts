10e76f09c1acff62647a514c3082d1c8d3167617
Valentina <Valentina.bennett@hmcts.net>
2021-01-30 16:44:17 +0000

    feature/VIH-7082 : linting issue

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-chat/participant-chat.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-chat/participant-chat.component.ts
@@ -54,7 +54,8 @@ export class ParticipantChatComponent extends ChatBaseComponent implements OnIni
         this.unreadMessageCount = 0;
         this.loading = true;
 
-        (async () => {this.loggedInUser = await this.videoWebService.getCurrentParticipant(this.hearing.id);
+        (async () => {
+            this.loggedInUser = await this.videoWebService.getCurrentParticipant(this.hearing.id);
             this.logger.debug(`[ChatHub Participant] get logged participant id: ${this.loggedInUser.participant_id}`);
             this.setupChatSubscription().then(sub => (this.chatHubSubscription = sub));
             this.retrieveChatForConference(this.loggedInUser.participant_id).then(messages => {
