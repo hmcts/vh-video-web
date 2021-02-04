9f817d0167498618078b58f8f5a534690e8810d7
Chris Tumolo <2236507+tremolo64@users.noreply.github.com>
2021-02-04 16:48:25 +0000

    VIH-7083 participants controller security (#1169)
    * Add middleware to check if participant belongs to a conference
    
    * Commit autogenerated files
    
    * Removed action-based uses of the new attribute, as it is a global filter
    
    * Removed unused usings
    
    * Remove SetCompatibilityVersion method as it is a no-op for ASP.NET Core 3.0 apps. Calling SetCompatibilityVersion with any value of CompatibilityVersion has no impact on the application.
    
    See: https://docs.microsoft.com/en-us/aspnet/core/mvc/compatibility-version?view=aspnetcore-3.1
    
    * Refactor CheckParticipantCanAccessConferenceAttribute to use conferenceId param on its own
    
    * Update code after PR review
    
    * Update autogenerated files
    
    * fix typo in test name
    
    * Specify numeric value as a decimal for heartbeat tests
    
    Co-authored-by: Chris Tumolo <chris.tumolo@HMCTS.net>

--- VideoWeb/VideoWeb.Services/BookingsApiClient.cs
+++ VideoWeb/VideoWeb.Services/BookingsApiClient.cs
@@ -598,6 +598,19 @@ namespace VideoWeb.Services.Bookings
         /// <exception cref="BookingsApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<System.Collections.Generic.List<PersonResponse>> PostPersonBySearchTermAsync(SearchTermRequest body, System.Threading.CancellationToken cancellationToken);
     
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<PersonResponse> SearchForNonJudicialPersonsByContactEmailAsync(string contactEmail);
+    
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        PersonResponse SearchForNonJudicialPersonsByContactEmail(string contactEmail);
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<PersonResponse> SearchForNonJudicialPersonsByContactEmailAsync(string contactEmail, System.Threading.CancellationToken cancellationToken);
+    
         /// <summary>Get a list of suitability answers for a given person</summary>
         /// <param name="username">The username of the person</param>
         /// <returns>Success</returns>
@@ -3636,6 +3649,103 @@ namespace VideoWeb.Services.Bookings
                         }
             
                         return default(System.Collections.Generic.List<PersonResponse>);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
+    
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<PersonResponse> SearchForNonJudicialPersonsByContactEmailAsync(string contactEmail)
+        {
+            return SearchForNonJudicialPersonsByContactEmailAsync(contactEmail, System.Threading.CancellationToken.None);
+        }
+    
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public PersonResponse SearchForNonJudicialPersonsByContactEmail(string contactEmail)
+        {
+            return System.Threading.Tasks.Task.Run(async () => await SearchForNonJudicialPersonsByContactEmailAsync(contactEmail, System.Threading.CancellationToken.None)).GetAwaiter().GetResult();
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Success</returns>
+        /// <exception cref="BookingsApiException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<PersonResponse> SearchForNonJudicialPersonsByContactEmailAsync(string contactEmail, System.Threading.CancellationToken cancellationToken)
+        {
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/persons?");
+            if (contactEmail != null) 
+            {
+                urlBuilder_.Append(System.Uri.EscapeDataString("contactEmail") + "=").Append(System.Uri.EscapeDataString(ConvertToString(contactEmail, System.Globalization.CultureInfo.InvariantCulture))).Append("&");
+            }
+            urlBuilder_.Length--;
+    
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Method = new System.Net.Http.HttpMethod("GET");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200") 
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<PersonResponse>(response_, headers_).ConfigureAwait(false);
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ == "404") 
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<string>(response_, headers_).ConfigureAwait(false);
+                            throw new BookingsApiException<string>("Not Found", (int)response_.StatusCode, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == "400") 
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<string>(response_, headers_).ConfigureAwait(false);
+                            throw new BookingsApiException<string>("Bad Request", (int)response_.StatusCode, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ == "401") 
+                        {
+                            var objectResponse_ = await ReadObjectResponseAsync<string>(response_, headers_).ConfigureAwait(false);
+                            throw new BookingsApiException<string>("Unauthorized", (int)response_.StatusCode, objectResponse_.Text, headers_, objectResponse_.Object, null);
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false); 
+                            throw new BookingsApiException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+            
+                        return default(PersonResponse);
                     }
                     finally
                     {
---
+++ VideoWeb/VideoWeb.UnitTests/Middleware/CheckParticipantCanAccessConferenceAttributeTests/when_action_has_participantId_and_conferenceId.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using VideoWeb.Common.Models;
+using VideoWeb.Services.Video;
+
+namespace VideoWeb.UnitTests.Middleware.CheckParticipantCanAccessConferenceAttributeTests
+{
+    public class when_action_has_participantId_and_conferenceId : CheckParticipantCanAccessConferenceAttributeTest
+    {
+        [TestCaseSource(nameof(AllNonVhoUsers))]
+        public async Task should_return_404_if_conference_does_not_exist(string appRole)
+        {
+            // arrange
+            var actionArguments = new Dictionary<string, object>
+            {
+                {"participantId", _participantId},
+                {"conferenceId", _conferenceId}
+            };
+
+            var user = _userBuilder.WithUsername(USER_NAME).WithRole(appRole).Build();
+
+            _conferenceCache.Setup(x => x.GetOrAddConferenceAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Func<Task<ConferenceDetailsResponse>>>()))
+                // conference doesn't exist (null)
+                .ReturnsAsync((Conference)null);
+
+            SetupActionExecutingContext(actionArguments, user);
+
+            // act
+            await _sut.OnActionExecutionAsync(_actionExecutingContext, async () => _actionExecutedContext);
+
+            // assert
+            _actionExecutingContext.Result.Should().BeOfType<NotFoundObjectResult>();
+            _actionExecutingContext.ModelState.ErrorCount.Should().Be(1);
+            var message404 = $"Conference with id:'{_conferenceId}' not found.";
+            _actionExecutingContext.ModelState["CheckParticipantCanAccessConference"]
+                .Errors.First().ErrorMessage
+                .Should().Be(message404);
+        }
+
+
+        [TestCaseSource(nameof(AllNonVhoUsers))]
+        public async Task should_return_401_if_conference_exists_but_user_does_not_belong_to_it(string appRole)
+        {
+            // arrange
+            var actionArguments = new Dictionary<string, object>
+            {
+                {"participantId", _participantId},
+                {"conferenceId", _conferenceId}
+            };
+
+            var user = _userBuilder.WithUsername(USER_NAME).WithRole(appRole).Build();
+
+            var conference = new Conference
+            {
+                // conference exists...
+                Id = _conferenceId,
+                Participants = new List<Participant>
+                {
+                    new Participant
+                    {
+                        // ...but user does not belong to it
+                        Username = "Username",
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+            _conferenceCache.Setup(x => x.GetOrAddConferenceAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Func<Task<ConferenceDetailsResponse>>>()))
+                .ReturnsAsync(conference);
+
+            SetupActionExecutingContext(actionArguments, user);
+
+            // act
+            await _sut.OnActionExecutionAsync(_actionExecutingContext, async () => _actionExecutedContext);
+
+            // assert
+            _actionExecutingContext.Result.Should().BeOfType<UnauthorizedObjectResult>();
+            _actionExecutingContext.ModelState.ErrorCount.Should().Be(1);
+            var message401 = "User does not belong to this conference.";
+            _actionExecutingContext.ModelState["CheckParticipantCanAccessConference"]
+                .Errors.First().ErrorMessage
+                .Should().Be(message401);
+        }
+
+        [TestCaseSource(nameof(AllNonVhoUsers))]
+        public async Task
+            should_continue_with_other_middleware_if_conference_exists_and_conference_contains_participantId(
+                string appRole)
+        {
+            // arrange
+            var actionArguments = new Dictionary<string, object>
+            {
+                {"participantId", _participantId},
+                {"conferenceId", _conferenceId}
+            };
+
+            var user = _userBuilder.WithUsername(USER_NAME).WithRole(appRole).Build();
+
+            var conference = new Conference
+            {
+                // conference exists...
+                Id = _conferenceId,
+                Participants = new List<Participant>
+                {
+                    new Participant
+                    {
+                        // ...and user is a part of it
+                        Username = USER_NAME,
+                        Id = _participantId
+                    }
+                }
+            };
+
+            _conferenceCache.Setup(x => x.GetOrAddConferenceAsync(
+                    It.IsAny<Guid>(),
+                    It.IsAny<Func<Task<ConferenceDetailsResponse>>>()))
+                .ReturnsAsync(conference);
+
+            SetupActionExecutingContext(actionArguments, user);
+
+            // act
+            await _sut.OnActionExecutionAsync(_actionExecutingContext, async () => _actionExecutedContext);
+
+            // assert
+            _actionExecutingContext.Result.Should().BeOfType<OkResult>();
+        }
+    }
+}
