000893fc5134ed478b47c97d1af8afa0eef9ca73
Valentina <Valentina.bennett@hmcts.net>
2021-02-09 13:13:57 +0000

    feature/VIH-7090 : modify unit test for kinlyConfiguration

--- VideoWeb/VideoWeb.UnitTests/Controllers/ConfigSettingController/GetClientConfigurationSettingsTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/ConfigSettingController/GetClientConfigurationSettingsTests.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using NUnit.Framework;
 using VideoWeb.Common.Configuration;
+using VideoWeb.Common.Security.HashGen;
 using VideoWeb.Contract.Responses;
 using VideoWeb.Controllers;
 using VideoWeb.Mappings;
@@ -19,7 +20,8 @@ namespace VideoWeb.UnitTests.Controllers.ConfigSettingController
         public void Setup()
         {
             _mocker = AutoMock.GetLoose();
-            _mocker.Mock<IMapperFactory>().Setup(x => x.Get<AzureAdConfiguration, HearingServicesConfiguration, ClientSettingsResponse>()).Returns(_mocker.Create<ClientSettingsResponseMapper>());
+            _mocker.Mock<IMapperFactory>().Setup(x => x.Get<AzureAdConfiguration, HearingServicesConfiguration, KinlyConfiguration, ClientSettingsResponse>())
+                .Returns(_mocker.Create<ClientSettingsResponseMapper>());
         }
 
         [Test]
@@ -31,7 +33,7 @@ namespace VideoWeb.UnitTests.Controllers.ConfigSettingController
                 TenantId = "TenantId",
                 ClientSecret = "ClientSecret",
                 Authority = "Authority",
-                ApplicationInsights = new ApplicationInsightsConfiguration {InstrumentationKey = "AiKey"}
+                ApplicationInsights = new ApplicationInsightsConfiguration { InstrumentationKey = "AiKey" }
             };
 
             var servicesConfiguration = new HearingServicesConfiguration
@@ -39,15 +41,24 @@ namespace VideoWeb.UnitTests.Controllers.ConfigSettingController
                 VideoApiUrl = "https://vh-video-api/"
             };
 
-            var parameters = new ParameterBuilder(_mocker).AddObject(Options.Create(securitySettings)).AddObject(Options.Create(servicesConfiguration)).Build();
+            var kinlyConfiguration = new KinlyConfiguration
+            {
+                JoinByPhoneFromDate = "2021-02-09"
+            };
+
+            var parameters = new ParameterBuilder(_mocker).AddObject(Options.Create(securitySettings))
+                .AddObject(Options.Create(servicesConfiguration))
+                .AddObject(kinlyConfiguration).Build();
+
             var configSettingsController = _mocker.Create<ConfigSettingsController>(parameters);
 
             var actionResult = (OkObjectResult)configSettingsController.GetClientConfigurationSettings().Result;
             var clientSettings = (ClientSettingsResponse)actionResult.Value;
-            
+
             clientSettings.ClientId.Should().Be(securitySettings.ClientId);
             clientSettings.TenantId.Should().Be(securitySettings.TenantId);
             clientSettings.VideoApiUrl.Should().Be(servicesConfiguration.VideoApiUrl);
+            clientSettings.JoinByPhoneFromDate.Should().Be(kinlyConfiguration.JoinByPhoneFromDate);
         }
     }
 }
