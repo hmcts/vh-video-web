1842a22861e79f8e0450cea9bb0064fdb56e0b6b
Valentina <40822624+valentinabennett@users.noreply.github.com>
2021-02-10 10:07:12 +0000

    VIH-7090 hide or show option join by phone (#1180)
    * feature/VIH-7090 : hide or show join by phone
    
    * feature/VIH-7090 : Option join by phone on or off
    
    * feature/VIH-7090 : lint issues
    
    * feature/VIH-7090 : lint issues and added looger msg
    
    * feature/VIH-7090 : modify unit test for kinlyConfiguration
    
    * feature/VIH-7090 : more tests
    
    * feature/VIH-7090 : edit test
    
    * feature/VIH-7090 : add test
    
    * feature/VIH-7090 : fix test
    
    * feature/VIH-7090 : fix test
    
    * feature/VIH-7090 : fix check date in test
    
    * feature/VIH-7090 : lint issue

--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/vh-officer-routing.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/vh-officer-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { pageUrls } from '../shared/page-url.constants';
 import { CommandCentreComponent } from './command-centre/command-centre.component';
 import { VenueListComponent } from './venue-list/venue-list.component';
+import { ConfigSettingsResolveService } from 'src/app/services/config-settings-resolve.service';
 
 const routes: Routes = [
     { path: '', redirectTo: pageUrls.AdminVenueList },
     { path: 'venue-list', component: VenueListComponent, data: { title: 'VHO Admin dashboard' } },
-    { path: 'hearing-list', component: CommandCentreComponent, data: { title: 'VHO Admin dashboard' } }
+    {
+        path: 'hearing-list',
+        component: CommandCentreComponent,
+        data: { title: 'VHO Admin dashboard' },
+        resolve: { configSettings: ConfigSettingsResolveService }
+    }
 ];
 
 @NgModule({
