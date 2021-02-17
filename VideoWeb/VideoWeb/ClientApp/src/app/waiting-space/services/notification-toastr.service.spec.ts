bb9d000081333a7de08ab902b46fd7039681e9e9
David Decent <david.decent@solirius.com>
2021-02-17 17:46:30 +0000

    VIH-7299 toast test progress

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/notification-toastr.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/notification-toastr.service.spec.ts
@@ -1,18 +1,38 @@
+import { Component } from '@angular/core';
+import { ToastRef } from 'ngx-toastr';
+import { ConferenceResponse, ParticipantResponse } from 'src/app/services/clients/api-client';
 import { Logger } from 'src/app/services/logging/logger-base';
+import { Participant } from 'src/app/shared/models/participant';
 import { MockLogger } from 'src/app/testing/mocks/MockLogger';
-import { consultationService, notificationSoundsService, toastrService } from '../waiting-room-shared/tests/waiting-room-base-setup';
+import { component, consultationService, globalConference, globalParticipant, mockToast, mockToastRef, notificationSoundsService, toastrService } from '../waiting-room-shared/tests/waiting-room-base-setup';
 import { NotificationToastrService } from './notification-toastr.service';
 
 describe('NotificationToastrService', () => {
     let service: NotificationToastrService;
     const logger: Logger = new MockLogger();
-    beforeAll(() => {});
+    let roomLabel: string;
+    let conference: ConferenceResponse;
+    let participant: ParticipantResponse;
+
+    beforeAll(() => { });
 
     beforeEach(() => {
         service = new NotificationToastrService(logger, toastrService, consultationService, notificationSoundsService);
+        roomLabel = 'Meeting room 1'
+        conference = new ConferenceResponse(Object.assign({}, globalConference));
+        participant = new ParticipantResponse(Object.assign({}, globalParticipant));
     });
 
     it('should create', async () => {
         expect(service).toBeTruthy();
+    });
+
+    it('should play notification ringtone', async () => {
+        //spyOnProperty(mockToast, 'toastRef').and.returnValue(mockToastRef);
+        (Object.getOwnPropertyDescriptor(mockToast, 'toastRef')?.get as jasmine.Spy<() => ToastRef<Component>>).and.returnValue(mockToastRef);
+        toastrService.show.and.returnValue(mockToast);
+        const p = new Participant(participant);
+        service.showConsultationInvite(roomLabel, conference.id, p, p, [p], false);
+        expect(notificationSoundsService.playConsultationRequestRingtone).toHaveBeenCalledTimes(1);
     });
 });
