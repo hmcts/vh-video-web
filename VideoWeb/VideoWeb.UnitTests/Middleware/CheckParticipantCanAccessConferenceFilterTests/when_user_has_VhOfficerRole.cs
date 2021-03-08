a2d60a47a62f00a98ddc6749222920e3e0bc11f8
Levi <levi.butler@hmcts.net>
2021-03-08 12:17:45 +0000

    changed to filter and registered

--- VideoWeb/VideoWeb.UnitTests/Middleware/CheckParticipantCanAccessConferenceFilterTests/when_user_has_VhOfficerRole.cs
+++ VideoWeb/VideoWeb.UnitTests/Middleware/CheckParticipantCanAccessConferenceFilterTests/when_user_has_VhOfficerRole.cs
@@ -5,9 +5,9 @@ using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using VideoWeb.Common.Models;
 
-namespace VideoWeb.UnitTests.Middleware.CheckParticipantCanAccessConferenceAttributeTests
+namespace VideoWeb.UnitTests.Middleware.CheckParticipantCanAccessConferenceFilterTests
 {
-    public class when_user_has_VhOfficerRole : CheckParticipantCanAccessConferenceAttributeTest
+    public class when_user_has_VhOfficerRole : CheckParticipantCanAccessConferenceFilterTest
     {
         [Test]
         public async Task should_continue_with_other_middleware()
