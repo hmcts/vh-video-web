7c925876beaf7aa1c08a87cf9c7d38aabab3c31c
Shaed Parkar <shaed.parkar@slimsolutions.company>
2021-03-11 23:36:49 +0000

    VIH-7192 connect to vmr with participant display name

--- VideoWeb/VideoWeb.UnitTests/Controllers/VirtualRoomController/GetInterpreterRoomForParticipantTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/VirtualRoomController/GetInterpreterRoomForParticipantTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Autofac.Extras.Moq;
+using FizzWare.NBuilder;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +15,8 @@ using VideoWeb.Controllers;
 using VideoWeb.Mappings;
 using VideoApi.Client;
 using VideoApi.Contract.Responses;
+using VideoWeb.Common.Caching;
+using VideoWeb.Common.Models;
 using VideoWeb.UnitTests.Builders;
 
 namespace VideoWeb.UnitTests.Controllers.VirtualRoomController
@@ -20,17 +25,23 @@ namespace VideoWeb.UnitTests.Controllers.VirtualRoomController
     {
         private AutoMock _mocker;
         private VirtualRoomsController _controller;
+        private Conference _testConference;
 
         [SetUp]
         public void Setup()
         {
+            BuildConferenceForTest();
             _mocker = AutoMock.GetLoose();
+            
+            _mocker.Mock<IConferenceCache>().Setup(cache => cache.GetOrAddConferenceAsync(_testConference.Id, It.IsAny<Func<Task<ConferenceDetailsResponse>>>()))
+                .Callback(async (Guid anyGuid, Func<Task<ConferenceDetailsResponse>> factory) => await factory())
+                .ReturnsAsync(_testConference);
             var parameters = new ParameterBuilder(_mocker)
                 .AddTypedParameters<SharedParticipantRoomMapper>()
                 .Build();
 
             _mocker.Mock<IMapperFactory>()
-                .Setup(x => x.Get<SharedParticipantRoomResponse, Guid, bool, SharedParticipantRoom>())
+                .Setup(x => x.Get<SharedParticipantRoomResponse, Participant, bool, SharedParticipantRoom>())
                 .Returns(_mocker.Create<SharedParticipantRoomMapper>(parameters));
             
             var claimsPrincipal = new ClaimsPrincipalBuilder().Build();
@@ -55,8 +66,8 @@ namespace VideoWeb.UnitTests.Controllers.VirtualRoomController
                 ParticipantJoinUri = "pat_join__interpreter",
                 PexipNode = "sip.unit.test.com"
             };
-            var participantId = Guid.NewGuid();
-            var conferenceId = Guid.NewGuid();
+            var participantId = _testConference.Participants.First(x=> !x.IsWitness() && !x.IsJudge()).Id;
+            var conferenceId = _testConference.Id;
             
             _mocker.Mock<IVideoApiClient>()
                 .Setup(x => x.GetInterpreterRoomForParticipantAsync(conferenceId, participantId))
@@ -75,8 +86,8 @@ namespace VideoWeb.UnitTests.Controllers.VirtualRoomController
                 ParticipantJoinUri = "pat_join__interpreter",
                 PexipNode = "sip.unit.test.com"
             };
-            var participantId = Guid.NewGuid();
-            var conferenceId = Guid.NewGuid();
+            var participantId = _testConference.Participants.First(x=> x.IsWitness()).Id;
+            var conferenceId = _testConference.Id;
             var participantType = "Witness";
             
             _mocker.Mock<IVideoApiClient>()
@@ -102,6 +113,31 @@ namespace VideoWeb.UnitTests.Controllers.VirtualRoomController
             
             var result = await _controller.GetInterpreterRoomForParticipant(conferenceId, participantId);
             result.Should().BeAssignableTo<ObjectResult>().Which.StatusCode.Should().Be(apiException.StatusCode);
+        }
+        
+        private void BuildConferenceForTest()
+        {
+            var conference = new Conference
+            {
+                Id = Guid.NewGuid(),
+                HearingId = Guid.NewGuid(),
+                Participants = new List<Participant>()
+                {
+                    Builder<Participant>.CreateNew()
+                        .With(x => x.Role = Role.Judge).With(x => x.Id = Guid.NewGuid())
+                        .Build(),
+                    Builder<Participant>.CreateNew().With(x => x.Role = Role.Individual)
+                        .With(x => x.Id = Guid.NewGuid())
+                        .With(x=> x.HearingRole = "Witness").Build(),
+                    Builder<Participant>.CreateNew().With(x => x.Role = Role.Individual)
+                        .With(x => x.Id = Guid.NewGuid()).Build(),
+                    Builder<Participant>.CreateNew().With(x => x.Role = Role.Representative)
+                        .With(x => x.Id = Guid.NewGuid()).Build()
+                },
+                HearingVenueName = "Hearing Venue Test",
+            };
+            
+            _testConference = conference;
         }
     }
 }
