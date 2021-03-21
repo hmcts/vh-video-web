61a05090d942245401992432cab6ad42eb4af0ac
Levi <levi.butler@hmcts.net>
2021-03-21 22:46:16 +0000

    added missing translations

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/select-hearing-layout/select-hearing-layout.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/select-hearing-layout/select-hearing-layout.component.ts
@@ -10,6 +10,7 @@ import { VideoCallService } from '../services/video-call.service';
 export class SelectHearingLayoutComponent implements OnInit {
     availableLayouts = HearingLayout;
     selectedLayout: HearingLayout;
+    accordionOpenAllElement: HTMLButtonElement;
     @Input() conference: ConferenceResponse;
     constructor(private videoCallService: VideoCallService, protected translateService: TranslateService) {}
 
@@ -22,7 +23,27 @@ export class SelectHearingLayoutComponent implements OnInit {
             this.selectedLayout = this.recommendedLayout();
             this.updateSelectedLayout(this.selectedLayout);
         }
+
         (<any>window).GOVUKFrontend.initAll();
+        document.getElementById('accordion-choose-layout-heading').onclick = (e) => this.setAccordionText(e);
+        this.accordionOpenAllElement = document.getElementsByClassName('govuk-accordion__open-all').item(0) as HTMLButtonElement;
+        this.accordionOpenAllElement.onclick = (e) => this.setAccordionText(e);
+        this.setAccordionText({} as MouseEvent);
+    }
+    
+    setAccordionText(event: MouseEvent) {
+        const element = event.target as HTMLButtonElement
+        if (element?.id == 'accordion-choose-layout-heading') {
+            setTimeout(() => this.setAccordionText({} as MouseEvent), 1);
+        }
+
+        const text = this.accordionOpenAllElement.innerHTML;
+        if (!text.startsWith('<')) {
+            const originalText = text.split('<')[0];
+            const currentText = originalText.toLowerCase().split(' ').join('-').trim();
+            const translated = `<span>${this.translateService.instant(`select-hearing-layout.${currentText}`)}</span>`;
+            this.accordionOpenAllElement.innerHTML = this.accordionOpenAllElement.innerHTML.replace(originalText, translated);
+        }
     }
 
     get recommendDynamic(): boolean {
