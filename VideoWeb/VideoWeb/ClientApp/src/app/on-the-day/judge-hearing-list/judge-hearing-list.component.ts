2bb7466e110b2641d29f9dbf37ed592806a30a3a
Valentina <Valentina.bennett@hmcts.net>
2021-03-26 13:54:23 +0000

    feature/VIH-7367 : formatting issue

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-list/judge-hearing-list.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/judge-hearing-list/judge-hearing-list.component.ts
@@ -98,9 +98,7 @@ export class JudgeHearingListComponent implements OnInit, OnDestroy {
     onConferenceSelected(conference: ConferenceForJudgeResponse) {
         this.logger.debug('[JudgeHearingList] - Signing into judge waiting room', { conference: conference.id });
         this.videoWebService.getCurrentParticipant(conference.id).then(x => {
-            const result = conference.participants.find(
-                p => p.id === x.participant_id && p.hearing_role === 'Judge'
-            );
+            const result = conference.participants.find(p => p.id === x.participant_id && p.hearing_role === 'Judge');
             if (result) {
                 this.router.navigate([pageUrls.JudgeWaitingRoom, conference.id]);
             } else {
