304cb5563f0cd7a7e5ee5a6e168c7d395a381b91
ZJianWang <zijian.wang@hmcts.net>
2021-04-19 16:56:19 +0100

    create a new component navigator

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/home/navigator/navigator.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavigatorComponent } from './navigator.component';
+
+describe('NavigatorComponent', () => {
+  let component: NavigatorComponent;
+  let fixture: ComponentFixture<NavigatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavigatorComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavigatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
