008ec6a72780927415a93d8ec4f5868eeea5873c
Jordan Mulvaney <Jordan.Mulvaney@version1.com>
2021-04-28 10:27:05 +0100

    Fixing Unit Tests

--- VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.spec.ts
@@ -1,4 +1,7 @@
-import { Router } from '@angular/router';
+import { Type } from '@angular/core';
+import { Router, Event } from '@angular/router';
+import { EventTypes, OidcClientNotification, PublicEventsService } from 'angular-auth-oidc-client';
+import { of, Subject } from 'rxjs';
 import { ProfileService } from '../services/api/profile.service';
 import { Role, UserProfileResponse } from '../services/clients/api-client';
 import { DeviceTypeService } from '../services/device-type.service';
@@ -8,19 +11,34 @@ import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
     let component: HomeComponent;
-    let router: jasmine.SpyObj<Router>;
+    let routerSpy: jasmine.SpyObj<Router>;
+    let eventServiceSpy: jasmine.SpyObj<PublicEventsService>;
+    let oidcClientNotificationSpy: jasmine.SpyObj<OidcClientNotification<any>>;
+
 
     beforeAll(() => {
-        router = jasmine.createSpyObj<Router>('Router', ['navigate']);
+        routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+        eventServiceSpy = jasmine.createSpyObj('PublicEventsService', ['registerForEvents']);
     });
 
     beforeEach(() => {
-        component = new HomeComponent(router);
-        router.navigate.and.callFake(() => Promise.resolve(true));
+        component = new HomeComponent(routerSpy, eventServiceSpy);
+        routerSpy.navigate.and.callFake(() => Promise.resolve(true));
     });
 
     it('should go to navigator if user log in', async () => {
+        debugger;
+        oidcClientNotificationSpy = jasmine.createSpyObj('OidcClientNotification', {}, { type: EventTypes.UserDataChanged });
+        eventServiceSpy.registerForEvents.and.returnValue(of(oidcClientNotificationSpy));
         component.ngOnInit();
-        expect(router.navigate).toHaveBeenCalledWith([`/${pageUrls.Navigator}`]);
+        expect(routerSpy.navigate).toHaveBeenCalledWith([`/${pageUrls.Navigator}`]);
     });
 });
+
+/*
+        subscribe(async (value: OidcClientNotification<AuthorizationResult>) => {
+        eventService.registerForEvents.and.returnValue(of(oidcClientNotificationSpy));
+        
+        .subscribe(() => this.router.navigate([`/${pageUrls.Navigator}`]));
+        eventService.registerForEvents(router)));
+*/
