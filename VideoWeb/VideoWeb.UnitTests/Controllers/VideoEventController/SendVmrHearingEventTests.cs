dce3fa28b6425faf7c4c16bdb935afc96dc96f6b
Robert Bennett <robertbennett1998@outlook.com>
2021-07-09 14:29:49 +0100

    Finished off unit test to check that the correct events are raised.

--- VideoWeb/VideoWeb.UnitTests/Controllers/VideoEventController/SendVmrHearingEventTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/VideoEventController/SendVmrHearingEventTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -7,6 +9,8 @@ using NUnit.Framework;
 using VideoApi.Client;
 using VideoApi.Contract.Enums;
 using VideoApi.Contract.Requests;
+using VideoApi.Contract.Responses;
+using VideoWeb.Common.Models;
 using VideoWeb.EventHub.Handlers.Core;
 using VideoWeb.EventHub.Models;
 
@@ -66,14 +70,51 @@ namespace VideoWeb.UnitTests.Controllers.VideoEventController
         public async Task should_raise_transfer_event_when_participant_joins_and_other_vmr_participants_are_in_consultation()
         {
             // Arrange
-            var room = TestConference.CivilianRooms.First(x => x.Participants.Any());
-            var roomId = room.Id;
+            var vmr = TestConference.CivilianRooms.First(x => x.Participants.Any());
+            vmr.Participants.ForEach(x => TestConference.Participants.First(y => y.Id == x).ParticipantStatus = ParticipantStatus.InConsultation);
+            
+            var finalParticipantCount = vmr.Participants.Count + 1;
+            var roomId = vmr.Id;
             var participantId = TestConference.Participants.Last().Id;
             var request = CreateRequest();
+            var consultationRoomId = 1234;
+            var consultationRoomLabel = "ConsultationRoom1";
             request.EventType = EventType.Joined;
             request.ParticipantRoomId = roomId.ToString();
             request.ParticipantId = participantId.ToString();
 
+            var linkedParticipantInConsultation = vmr?.Participants.Where(x => x != participantId)
+                .Select(x => TestConference.Participants.FirstOrDefault(y => x == y.Id))
+                .FirstOrDefault(z => z?.ParticipantStatus == ParticipantStatus.InConsultation);
+
+            var videoApiParticipantResponse = TestConference.Participants.Select(x => new ParticipantSummaryResponse()
+            {
+                CaseGroup = x.CaseTypeGroup,
+                ContactEmail = x.ContactEmail,
+                ContactTelephone = x.ContactTelephone,
+                CurrentInterpreterRoom = new RoomResponse()
+                {
+                    Id = vmr.Id,
+                    Label = vmr.RoomLabel,
+                    Locked = false
+                },
+                CurrentRoom = new RoomResponse()
+                {
+                    Id = consultationRoomId,
+                    Label = consultationRoomLabel,
+                    Locked = false
+                },
+                DisplayName = x.DisplayName,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                HearingRole = x.HearingRole,
+                Id = x.Id
+            }).ToList();
+
+            Mocker.Mock<IVideoApiClient>()
+                .Setup(x => x.GetParticipantsByConferenceIdAsync(It.Is<Guid>(y => y == TestConference.Id)))
+                .ReturnsAsync(videoApiParticipantResponse);
+            
             // Act
             var result = await Sut.SendHearingEventAsync(request);
 
@@ -82,10 +123,15 @@ namespace VideoWeb.UnitTests.Controllers.VideoEventController
             var typedResult = (NoContentResult)result;
             typedResult.Should().NotBeNull();
 
-            //Mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.IsAny<CallbackEvent>()),
-                //Times.Exactly(participantCount));
-            Mocker.Mock<IVideoApiClient>().Verify(x => x.RaiseVideoEventAsync(It.IsAny<ConferenceEventRequest>()),
-                Times.Exactly(participantCount));
+            Mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.Is<CallbackEvent>(y => y.EventType == EventHub.Enums.EventType.Joined)),
+                Times.Once);
+            Mocker.Mock<IEventHandler>().Verify(x => x.HandleAsync(It.Is<CallbackEvent>(y => y.EventType == EventHub.Enums.EventType.Transfer && y.TransferFrom == "WaitingRoom" && y.TransferTo == consultationRoomLabel)),
+                Times.Exactly(finalParticipantCount));
+            
+            Mocker.Mock<IVideoApiClient>().Verify(x => x.RaiseVideoEventAsync(It.Is<ConferenceEventRequest>(y => y.EventType == EventType.RoomParticipantJoined)),
+                Times.Once);
+            Mocker.Mock<IVideoApiClient>().Verify(x => x.RaiseVideoEventAsync(It.Is<ConferenceEventRequest>(y => y.EventType == EventType.RoomParticipantTransfer && y.TransferFrom == "WaitingRoom" && y.TransferTo == consultationRoomLabel)),
+                Times.Exactly(finalParticipantCount));
         }
 
         [Test]
