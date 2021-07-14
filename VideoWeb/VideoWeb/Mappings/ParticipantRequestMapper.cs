f25ab6feab050111708590532d86bb26e86e85cb
FarronF <farron.flaherty@hmcts.net>
2021-07-14 15:35:57 +0100

    Mapping linked participants

--- VideoWeb/VideoWeb/Mappings/ParticipantRequestMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ParticipantRequestMapper.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using VideoApi.Contract.Requests;
 using VideoWeb.Common.Models;
 using VideoWeb.Mappings.Interfaces;
@@ -6,6 +7,11 @@ namespace VideoWeb.Mappings
 {
     public class ParticipantRequestMapper : IMapTo<ParticipantRequest, Participant>
     {
+        private readonly IMapTo<LinkedParticipantRequest, LinkedParticipant> linkedParticipantMapper;
+        public ParticipantRequestMapper(IMapperFactory mapperFactory)
+        {
+            linkedParticipantMapper = mapperFactory.Get<LinkedParticipantRequest, LinkedParticipant>();
+        }
         public Participant Map(ParticipantRequest request)
         {
             return new Participant
@@ -14,7 +20,7 @@ namespace VideoWeb.Mappings
                 Name = request.Name,
                 Role = (Role)request.UserRole,
                 HearingRole = request.HearingRole,
-                
+
                 CaseTypeGroup = request.CaseTypeGroup,
                 ContactEmail = request.ContactEmail,
                 ContactTelephone = request.ContactTelephone,
@@ -25,6 +31,7 @@ namespace VideoWeb.Mappings
                 RefId = request.ParticipantRefId,
                 Representee = request.Representee,
                 Username = request.Username,
+                LinkedParticipants = request.LinkedParticipants.Select(linkedParticipant => linkedParticipantMapper.Map(linkedParticipant)).ToList()
             };
         }
     }
