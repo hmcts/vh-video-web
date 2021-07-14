9221544cc495c673421c8416e55f3c5f884597b9
FarronF <farron.flaherty@hmcts.net>
2021-07-14 15:46:18 +0100

    Additional mappers

---
+++ VideoWeb/VideoWeb/Mappings/LinkedParticipantRequestToLinkedParticipantMapper.cs
@@ -0,0 +1,18 @@
+using VideoApi.Contract.Requests;
+using VideoWeb.Common.Models;
+using VideoWeb.Mappings.Interfaces;
+
+namespace VideoWeb.Mappings
+{
+    public class LinkedParticipantRequestToLinkedParticipantResponse : IMapTo<LinkedParticipantRequest, LinkedParticipant>
+    {
+        public LinkedParticipant Map(LinkedParticipantRequest input)
+        {
+            return new LinkedParticipant()
+            {
+                LinkedId = input.LinkedRefId,
+                LinkType = (LinkType)input.Type
+            };
+        }
+    }
+}
