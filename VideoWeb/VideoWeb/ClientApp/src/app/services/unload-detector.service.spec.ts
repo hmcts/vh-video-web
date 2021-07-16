f5476757dc97cb9f8495fbb0586279de44f6356f
Robert Bennett <robertbennett1998@outlook.com>
2021-07-16 16:44:02 +0100

    Added logging and ran prettier/lint

--- VideoWeb/VideoWeb/ClientApp/src/app/services/unload-detector.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/unload-detector.service.spec.ts
@@ -1,97 +1,99 @@
-import { Renderer2, RendererFactory2 } from "@angular/core";
-import { fakeAsync } from "@angular/core/testing";
-import { DeviceDetectorService } from "ngx-device-detector";
-import { UnloadDetectorService } from "./unload-detector.service";
+import { Renderer2, RendererFactory2 } from '@angular/core';
+import { fakeAsync } from '@angular/core/testing';
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { Logger } from './logging/logger-base';
+import { UnloadDetectorService } from './unload-detector.service';
 
 class Renderer2Mock {
-  beforeUnloadCallback: (event : any) => boolean | void;
-  visibiltyChangeCallback: (event : any) => boolean | void;
+    beforeUnloadCallback: (event: any) => boolean | void;
+    visibiltyChangeCallback: (event: any) => boolean | void;
 
-  listen(target: 'window' | 'document' | 'body' | any, eventName: string, callback: (event: any) => boolean | void) {
-    if (target === 'window' && eventName === 'beforeunload')
-    {
-      this.beforeUnloadCallback = callback;
-    } else if (target === 'document' && eventName === 'visibilitychange') {
-      this.visibiltyChangeCallback = callback;
+    listen(target: 'window' | 'document' | 'body' | any, eventName: string, callback: (event: any) => boolean | void) {
+        if (target === 'window' && eventName === 'beforeunload') {
+            this.beforeUnloadCallback = callback;
+        } else if (target === 'document' && eventName === 'visibilitychange') {
+            this.visibiltyChangeCallback = callback;
+        }
     }
-  }
 }
 
 describe('UnloadDetectorService', () => {
-  let service: UnloadDetectorService;
-  let deviceDetectorServiceSpy : jasmine.SpyObj<DeviceDetectorService>;
-  let renderer2FactorySpy : jasmine.SpyObj<RendererFactory2>;
-  let renderer2Mock : Renderer2Mock;
+    let service: UnloadDetectorService;
+    let deviceDetectorServiceSpy: jasmine.SpyObj<DeviceDetectorService>;
+    let renderer2FactorySpy: jasmine.SpyObj<RendererFactory2>;
+    let renderer2Mock: Renderer2Mock;
+    let loggerSpy: jasmine.SpyObj<Logger>;
 
-  beforeEach(() => {
-    deviceDetectorServiceSpy = jasmine.createSpyObj<DeviceDetectorService>('DeviceDetectorService', ['isDesktop']);
-    renderer2FactorySpy = jasmine.createSpyObj<RendererFactory2>('RendererFactory2', ['createRenderer']);
-
-    renderer2Mock = new Renderer2Mock();
-    spyOn(renderer2Mock, 'listen').and.callThrough();
-
-    renderer2FactorySpy.createRenderer.withArgs(null, null).and.returnValue(renderer2Mock as unknown as Renderer2);
-  });
-
-  describe('when on desktop', () => {
     beforeEach(() => {
-      deviceDetectorServiceSpy.isDesktop.and.returnValue(true);
-      service = new UnloadDetectorService(deviceDetectorServiceSpy, renderer2FactorySpy);
+        deviceDetectorServiceSpy = jasmine.createSpyObj<DeviceDetectorService>('DeviceDetectorService', ['isDesktop']);
+        renderer2FactorySpy = jasmine.createSpyObj<RendererFactory2>('RendererFactory2', ['createRenderer']);
+        loggerSpy = jasmine.createSpyObj<Logger>('Logger', ['info']);
+
+        renderer2Mock = new Renderer2Mock();
+        spyOn(renderer2Mock, 'listen').and.callThrough();
+
+        renderer2FactorySpy.createRenderer.withArgs(null, null).and.returnValue((renderer2Mock as unknown) as Renderer2);
     });
 
-    it('should create', () => {
-      expect(service).toBeTruthy();
+    describe('when on desktop', () => {
+        beforeEach(() => {
+            deviceDetectorServiceSpy.isDesktop.and.returnValue(true);
+            service = new UnloadDetectorService(deviceDetectorServiceSpy, renderer2FactorySpy, loggerSpy);
+        });
+
+        it('should create', () => {
+            expect(service).toBeTruthy();
+        });
+
+        it('should initialise the isDesktop value', () => {
+            expect(service.isDesktop).toBeTrue();
+        });
+
+        it('should listen to the before unload event', () => {
+            expect(renderer2Mock.listen).toHaveBeenCalledOnceWith('window', 'beforeunload', jasmine.anything());
+        });
+
+        it('should emit an event when the visibilitychange event is recieved with the isHidden as true', fakeAsync(() => {
+            // Arrange
+            let wasCalled = false;
+            service.shouldUnload.subscribe(() => (wasCalled = true));
+
+            // Act
+            renderer2Mock.beforeUnloadCallback(undefined);
+
+            // Assert
+            expect(wasCalled).toBeTrue();
+        }));
     });
 
-    it('should initialise the isDesktop value', () => {
-      expect(service.isDesktop).toBeTrue();
+    describe('when NOT on desktop', () => {
+        beforeEach(() => {
+            deviceDetectorServiceSpy.isDesktop.and.returnValue(false);
+            service = new UnloadDetectorService(deviceDetectorServiceSpy, renderer2FactorySpy, loggerSpy);
+        });
+
+        it('should create', () => {
+            expect(service).toBeTruthy();
+        });
+
+        it('should initialise the isDesktop value', () => {
+            expect(service.isDesktop).toBeFalse();
+        });
+
+        it('should listen to the visibility change event', () => {
+            expect(renderer2Mock.listen).toHaveBeenCalledOnceWith('document', 'visibilitychange', jasmine.anything());
+        });
+
+        it('should emit an event when the visibilitychange event is recieved with the isHidden as true', fakeAsync(() => {
+            // Arrange
+            let wasCalled = false;
+            service.shouldUnload.subscribe(() => (wasCalled = true));
+
+            // Act
+            renderer2Mock.visibiltyChangeCallback(undefined);
+
+            // Assert
+            expect(wasCalled).toBeTrue();
+        }));
     });
-
-    it('should listen to the before unload event', () => {
-      expect(renderer2Mock.listen).toHaveBeenCalledOnceWith('window', 'beforeunload', jasmine.anything());
-    });
-
-    it('should emit an event when the visibilitychange event is recieved with the isHidden as true', fakeAsync(() => {
-      // Arrange
-      let wasCalled = false;
-      service.shouldUnload.subscribe(() => wasCalled = true);
-
-      // Act
-      renderer2Mock.beforeUnloadCallback(undefined);
-
-      // Assert
-      expect(wasCalled).toBeTrue();
-    }));
-  });
-
-  describe('when NOT on desktop', () => {
-    beforeEach(() => {
-      deviceDetectorServiceSpy.isDesktop.and.returnValue(false);
-      service = new UnloadDetectorService(deviceDetectorServiceSpy, renderer2FactorySpy);
-    });
-
-    it('should create', () => {
-      expect(service).toBeTruthy();
-    });
-
-    it('should initialise the isDesktop value', () => {
-      expect(service.isDesktop).toBeFalse();
-    });
-
-    it('should listen to the visibility change event', () => {
-      expect(renderer2Mock.listen).toHaveBeenCalledOnceWith('document', 'visibilitychange', jasmine.anything());
-    });
-
-    it('should emit an event when the visibilitychange event is recieved with the isHidden as true', fakeAsync(() => {
-      // Arrange
-      let wasCalled = false;
-      service.shouldUnload.subscribe(() => wasCalled = true);
-
-      // Act
-      renderer2Mock.visibiltyChangeCallback(undefined);
-
-      // Assert
-      expect(wasCalled).toBeTrue();
-    }));
-  });
 });
--- VideoWeb/VideoWeb/ClientApp/src/app/services/unload-detector.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/unload-detector.service.ts
@@ -2,12 +2,14 @@ import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, tap } from 'rxjs/operators';
+import { Logger } from './logging/logger-base';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UnloadDetectorService {
+    private loggerPrefix = '[UnloadDetectorService] -';
     private visibilityChangeSubject = new Subject<boolean>();
     private shouldUnloadSubject = new Subject<void>();
     private beforeUnloadSubject = new Subject<void>();
@@ -15,7 +17,7 @@ export class UnloadDetectorService {
     private renderer: Renderer2;
     isDesktop: boolean;
 
-    constructor(private deviceDetectorService: DeviceDetectorService, renderer2Factor: RendererFactory2) {
+    constructor(private deviceDetectorService: DeviceDetectorService, renderer2Factor: RendererFactory2, private logger: Logger) {
         this.renderer = renderer2Factor.createRenderer(null, null);
         this.initialise();
     }
@@ -24,11 +26,27 @@ export class UnloadDetectorService {
         this.isDesktop = this.deviceDetectorService.isDesktop();
 
         if (this.isDesktop) {
+            this.logger.info(`${this.loggerPrefix} Desktop device detected. Will raise unload event when window:beforeunload is raised!`);
             this.renderer.listen('window', 'beforeunload', () => this.beforeUnloadSubject.next());
-            this.beforeUnload.subscribe(() => this.shouldUnloadSubject.next());
+            this.beforeUnload
+                .pipe(
+                    tap(() => {
+                        this.logger.info(`${this.loggerPrefix} window:beforeunload recieved. Emitting the should unload event!`);
+                    })
+                )
+                .subscribe(() => this.shouldUnloadSubject.next());
         } else {
+            this.logger.info(
+                `${this.loggerPrefix} Mobile device detected. Will raise unload event when document:visibilitychange is raised!`
+            );
             this.renderer.listen('document', 'visibilitychange', () => this.visibilityChangeSubject.next(document.hidden));
-            this.visibilityChangedToHidden.subscribe(() => this.shouldUnloadSubject.next());
+            this.visibilityChangedToHidden
+                .pipe(
+                    tap(() => {
+                        this.logger.info(`${this.loggerPrefix} Visibility changed to hidden. Emitting the should unload event!`);
+                    })
+                )
+                .subscribe(() => this.shouldUnloadSubject.next());
         }
     }
 
