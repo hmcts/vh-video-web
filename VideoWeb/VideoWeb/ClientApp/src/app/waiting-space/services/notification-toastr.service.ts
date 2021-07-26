bf4b70d5fffde79467adb25448a0bbcecab86129
FarronF <farron.flaherty@hmcts.net>
2021-07-26 09:05:59 +0100

    VIH-7657 toastr style issues (#1488)

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/notification-toastr.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/notification-toastr.service.ts
@@ -294,16 +294,19 @@ export class NotificationToastrService {
     }
 
     showParticipantAdded(participant: ParticipantResponse, inHearing: boolean = false): VhToastComponent {
-        let message = `<span class="govuk-!-font-weight-bold">${this.translateService.instant(
+        let message = `<span class="govuk-!-font-weight-bold toast-content toast-header">${this.translateService.instant(
             'notification-toastr.participant-added.title',
             {
                 name: participant.name
             }
         )}</span>`;
-        message += `<br/>${this.translateService.instant('notification-toastr.participant-added.message', {
-            role: this.translateHearingRole(participant.hearing_role),
-            party: this.translateCaseRole(participant.case_type_group)
-        })}<br/>`;
+        message += `<span class="toast-content toast-body">${this.translateService.instant(
+            'notification-toastr.participant-added.message',
+            {
+                role: this.translateHearingRole(participant.hearing_role),
+                party: this.translateCaseRole(participant.case_type_group)
+            }
+        )}</span>`;
 
         const toast = this.toastr.show('', '', {
             timeOut: 0,
