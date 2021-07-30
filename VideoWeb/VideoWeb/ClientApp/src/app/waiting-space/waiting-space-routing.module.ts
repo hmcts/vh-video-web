d67c0f81b8e525b626b2895c476b32e99dfd09cf
FarronF <farron.flaherty@hmcts.net>
2021-07-30 10:44:16 +0100

    Changing translation path

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-space-routing.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-space-routing.module.ts
@@ -9,27 +9,27 @@ import { JudgeWaitingRoomComponent } from './judge-waiting-room/judge-waiting-ro
 import { ParticipantWaitingRoomComponent } from './participant-waiting-room/participant-waiting-room.component';
 import { LoggedUserResolveService } from './services/logged-user-resolve.service';
 
-const returnText = 'judge-waiting-room.return-to-video-hearing-list';
+const returnToVideoHearingListText = 'back-navigation.return-to-video-hearing-list';
 const routes: Routes = [
     {
         path: `${pageUrls.ParticipantWaitingRoom}/:conferenceId`,
         component: ParticipantWaitingRoomComponent,
         canActivate: [ParticipantWaitingRoomGuard],
         resolve: { loggedUser: LoggedUserResolveService },
-        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnText, pageUrls.ParticipantHearingList) }
+        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnToVideoHearingListText, pageUrls.ParticipantHearingList) }
     },
     {
         path: `${pageUrls.JudgeWaitingRoom}/:conferenceId`,
         component: JudgeWaitingRoomComponent,
         canActivate: [ConferenceGuard],
         resolve: { loggedUser: LoggedUserResolveService },
-        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnText, pageUrls.JudgeHearingList) }
+        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnToVideoHearingListText, pageUrls.JudgeHearingList) }
     },
     {
         path: `${pageUrls.JOHWaitingRoom}/:conferenceId`,
         component: JohWaitingRoomComponent,
         resolve: { loggedUser: LoggedUserResolveService },
-        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnText, pageUrls.JudgeHearingList) }
+        data: { title: 'Waiting room', backLink: new BackLinkDetails(returnToVideoHearingListText, pageUrls.JudgeHearingList) }
     }
 ];
 
