302b8c40e8e746a3ee92d84eb7c0984dae7231b6
muktadiru <muktadiru.ahmed@google.com>
2021-08-10 17:10:39 +0100

    removing unused imports

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/header/header.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { pageUrls } from '../page-url.constants';
 import { topMenuItems } from './topMenuItems';
 
 @Component({
