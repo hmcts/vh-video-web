443d1bba88b9ce09e676ad0989b31ce4d2beca26
krishnapottekkat-hmcts <krishna.pottekkat@hmcts.net>
2021-08-11 09:45:50 +0100

    VIH-7903 - Correcting the Lint failures

--- VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
@@ -27,7 +27,6 @@ export class LogoutComponent implements OnInit {
                 this.profileService.clearUserProfile();
                 this.judgeAllocationStorage.clear();
                 this.securityService.logoffAndRevokeTokens();
-                console.log('success');
             }
         });
     }
