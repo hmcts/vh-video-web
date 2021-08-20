c9e659271ecb1832150c871a06ce5ff754657e25
Zijian Wang <Zijian.Wang@hmcts.net>
2021-08-20 09:58:56 +0100

    Retry 10

--- VideoWeb/VideoWeb/ClientApp/src/app/services/user-media.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/user-media.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { from, Observable, of, ReplaySubject, zip } from 'rxjs';
+import { from, Observable, ReplaySubject, zip } from 'rxjs';
 import 'webrtc-adapter';
 import { UserMediaDevice } from '../shared/models/user-media-device';
 import { Logger } from './logging/logger-base';
@@ -170,12 +170,11 @@ export class UserMediaService {
 
     hasValidCameraAndMicAvailable(): Observable<boolean> {
         // TODO: Have a look at switching to audio only when the video camera is unavailable?
-        return of(true);
-        // from(this.navigator.mediaDevices.getUserMedia(this.defaultStreamConstraints)).pipe(
-        //     retry(3),
-        //     take(1),
-        //     map(stream => !!stream && stream.getVideoTracks().length > 0 && stream.getAudioTracks().length > 0)
-        // );
+        return from(this.navigator.mediaDevices.getUserMedia(this.defaultStreamConstraints)).pipe(
+            retry(10),
+            take(1),
+            map(stream => !!stream && stream.getVideoTracks().length > 0 && stream.getAudioTracks().length > 0)
+        );
     }
 
     updateActiveMicrophone(microhoneDevice: UserMediaDevice) {
