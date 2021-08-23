2bcfc2fe7597aaa40ad83249da7d967b5b63d1c0
FarronF <farron.flaherty@hmcts.net>
2021-08-23 10:29:48 +0100

    Correcting test cases

--- VideoWeb/VideoWeb/ClientApp/src/app/security/participant-status.guard.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/participant-status.guard.spec.ts
@@ -14,8 +14,8 @@ const testCases = [
     { role: Role.Individual, shouldFireJoiningEvent: true },
     { role: Role.Representative, shouldFireJoiningEvent: true },
     { role: Role.JudicialOfficeHolder, shouldFireJoiningEvent: true },
-    { role: Role.QuickLinkParticipant, shouldFireJoiningEvent: false },
-    { role: Role.QuickLinkObserver, shouldFireJoiningEvent: false }
+    { role: Role.QuickLinkParticipant, shouldFireJoiningEvent: true },
+    { role: Role.QuickLinkObserver, shouldFireJoiningEvent: true }
 ];
 
 describe('ParticipantStatusGuard', () => {
