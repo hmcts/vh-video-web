e295b66bb0b967f9963f65eb324536d10012f2bd
omio-ma <84961314+omio-ma@users.noreply.github.com>
2021-08-24 09:27:37 +0100

    VIH-8141 added constants for browser names, added samsung as supported browser (#1518)
    * added constants for browser names, added samsung as supported browser
    
    * removed duplicated assertions
    
    * updated key name for internet explorer
    
    Co-authored-by: muktadiru <muktadiru.ahmed@google.com>

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/browser.constants.ts
@@ -0,0 +1,11 @@
+export const browsers = {
+    Firefox: 'Firefox',
+    Safari: 'Safari',
+    Chrome: 'Chrome',
+    MSEdge: 'MS-Edge',
+    MSEdgeChromium: 'MS-Edge-Chromium',
+    Samsung: 'Samsung',
+    Opera: 'Opera',
+    Brave: 'Brave',
+    MSInternetExplorer: 'MSIE'
+};
