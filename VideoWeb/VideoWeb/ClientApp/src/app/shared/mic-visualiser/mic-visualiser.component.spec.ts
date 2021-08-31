56537ad3bec2fbd745c78633ca119da04e2282bc
FarronF <farron.flaherty@hmcts.net>
2021-08-31 16:42:45 +0100

    VIH-8100 Making microphone bar responsive (#1520)
    * Making responsive
    
    * One reference to canvas
    
    * Changing to use viewchild
    
    * Matching older implementation
    
    * Correcting variables used in html
    
    * Removing redundent function call
    
    * Linting
    
    * Adding unit test file
    
    * Unit tests
    
    * Removing console log
    
    * More comprehensive tests
    
    * Removing console log.

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/mic-visualiser/mic-visualiser.component.spec.ts
@@ -0,0 +1,122 @@
+import { ChangeDetectorRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { MicVisualiserComponent } from './mic-visualiser.component';
+
+describe('MicVisualiserComponent', () => {
+    let component: MicVisualiserComponent;
+    let fixture: ComponentFixture<MicVisualiserComponent>;
+    let changeDetectorSpy: jasmine.SpyObj<ChangeDetectorRef>;
+    let canvasElement;
+    let canvasContext;
+
+    beforeEach(async () => {
+        changeDetectorSpy = jasmine.createSpyObj<ChangeDetectorRef>('ChangeDetectorRef', ['detectChanges']);
+
+        await TestBed.configureTestingModule({
+            declarations: [MicVisualiserComponent],
+            providers: [{ provide: ChangeDetectorRef, useValue: changeDetectorSpy }]
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MicVisualiserComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        canvasElement = fixture.debugElement.query(By.css('#meter')).nativeElement;
+        canvasContext = canvasElement.getContext('2d');
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should call setupStream when there is a change', () => {
+        spyOn(component, 'setupStream');
+        component.ngOnChanges();
+        expect(component.setupStream).toHaveBeenCalledTimes(1);
+    });
+
+    it('should setup canvas afterViewInit', () => {
+        component.ngAfterViewInit();
+        fixture.detectChanges();
+        expect(canvasContext.fillStyle).toBe('#008000');
+    });
+
+    describe('fillMeter', () => {
+        let clearRectSpy: jasmine.SpyObj<any>;
+        let fillRectSpy: jasmine.SpyObj<any>;
+        const originalElementWidth = 100;
+        const elementHeight = 30;
+        const feedback = 1;
+
+        let fillWidth: number;
+
+        beforeEach(() => {
+            canvasElement.style.width = `${originalElementWidth}px`;
+            canvasElement.style.height = `${elementHeight}px`;
+            clearRectSpy = spyOn(canvasContext, 'clearRect');
+            fillRectSpy = spyOn(canvasContext, 'fillRect').and.callFake((x: number, y: number, width: number, height: number) => {
+                fillWidth = width;
+            });
+        });
+
+        it('should clear and fill canvas with 0 width when feedback is 0', () => {
+            component.fillMeter(0);
+
+            expect(canvasContext.clearRect).toHaveBeenCalledTimes(1);
+            expect(canvasContext.clearRect).toHaveBeenCalledWith(0, 0, originalElementWidth, elementHeight);
+            expect(canvasContext.fillRect).toHaveBeenCalledTimes(1);
+            expect(canvasContext.fillRect).toHaveBeenCalledWith(0, 0, 0, elementHeight);
+        });
+
+        it('should clear and fill canvas', () => {
+            component.fillMeter(feedback);
+
+            expect(canvasContext.clearRect).toHaveBeenCalledTimes(1);
+            expect(canvasContext.clearRect).toHaveBeenCalledWith(0, 0, originalElementWidth, elementHeight);
+            expect(canvasContext.fillRect).toHaveBeenCalledTimes(1);
+            expect(canvasContext.fillRect).toHaveBeenCalledWith(0, 0, jasmine.any(Number), elementHeight);
+            expect(fillWidth).toBeGreaterThan(0);
+        });
+
+        describe('different widths', () => {
+            const difference = 10;
+            let originalFillWidth: number;
+            let newElementWidth: number;
+            beforeEach(() => {
+                component.fillMeter(feedback);
+                expect(fillWidth).toBeGreaterThan(0);
+                originalFillWidth = fillWidth;
+                clearRectSpy.calls.reset();
+                fillRectSpy.calls.reset();
+            });
+
+            it('should fill a greater value when width is greater', () => {
+                newElementWidth = originalElementWidth + difference;
+                canvasElement.style.width = `${newElementWidth}px`;
+                component.fillMeter(feedback);
+
+                expect(canvasContext.clearRect).toHaveBeenCalledTimes(1);
+                expect(canvasContext.clearRect).toHaveBeenCalledWith(0, 0, newElementWidth, elementHeight);
+                expect(canvasContext.fillRect).toHaveBeenCalledTimes(1);
+                expect(canvasContext.fillRect).toHaveBeenCalledWith(0, 0, jasmine.any(Number), elementHeight);
+                expect(fillWidth).toBeGreaterThan(originalFillWidth);
+            });
+
+            it('should fill a lower value when width is lower', () => {
+                newElementWidth = originalElementWidth - difference;
+                canvasElement.style.width = `${newElementWidth}px`;
+                component.fillMeter(feedback);
+
+                expect(canvasContext.clearRect).toHaveBeenCalledTimes(1);
+                expect(canvasContext.clearRect).toHaveBeenCalledWith(0, 0, newElementWidth, elementHeight);
+                expect(canvasContext.fillRect).toHaveBeenCalledTimes(1);
+                expect(canvasContext.fillRect).toHaveBeenCalledWith(0, 0, jasmine.any(Number), elementHeight);
+                expect(fillWidth).toBeLessThan(originalFillWidth);
+            });
+        });
+    });
+});
