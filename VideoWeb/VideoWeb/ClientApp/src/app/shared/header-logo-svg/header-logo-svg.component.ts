886235b90ea909764ad427f39a4aba94c6a8017d
muktadiru <muktadiru.ahmed@google.com>
2021-08-31 16:48:55 +0100

    removed reference to css file

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/header-logo-svg/header-logo-svg.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/header-logo-svg/header-logo-svg.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header-logo-svg',
-    templateUrl: './header-logo-svg.component.html',
-    styleUrls: ['./header-logo-svg.component.css']
+    templateUrl: './header-logo-svg.component.html'
 })
 export class HeaderLogoSvgComponent {}
