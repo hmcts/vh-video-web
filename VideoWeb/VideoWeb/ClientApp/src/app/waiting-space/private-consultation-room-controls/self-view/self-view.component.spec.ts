d19eb03c41520e52b33f04408c3b38ce66f93f1e
Robert Bennett <robertbennett1998@outlook.com>
2021-09-08 10:34:09 +0100

    Fixed broken test

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/self-view/self-view.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/self-view/self-view.component.spec.ts
@@ -1,25 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipeMock } from 'src/app/testing/mocks/mock-translation-pipe';
 
 import { SelfViewComponent } from './self-view.component';
 
 describe('SelfViewComponent', () => {
-  let component: SelfViewComponent;
-  let fixture: ComponentFixture<SelfViewComponent>;
+    let component: SelfViewComponent;
+    let fixture: ComponentFixture<SelfViewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SelfViewComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [SelfViewComponent],
+            providers: [{ provide: TranslatePipe, useValue: TranslatePipeMock }],
+            imports: [TranslateModule.forRoot()]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SelfViewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SelfViewComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
