66f99793831962fa38a2a985a6cdf46d8178a3dd
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2021-09-13 10:46:58 +0100

    VIH-8041 switch between original and filtered streams (#1556)

--- VideoWeb/VideoWeb/ClientApp/src/app/services/media-stream.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/media-stream.service.spec.ts
@@ -46,11 +46,10 @@ describe('MediaStreamService', () => {
         navigatorSpy = jasmine.createSpyObj<Navigator>([], ['mediaDevices']);
         getSpiedPropertyGetter(navigatorSpy, 'mediaDevices').and.returnValue(mediaDevicesSpy);
 
-        videoFilterStreamServiceSpy = jasmine.createSpyObj<VideoFilterService>([
-            'initFilterFromMediaStream',
-            'startFilteredStream',
-            'doesSupportVideoFiltering'
-        ]);
+        videoFilterStreamServiceSpy = jasmine.createSpyObj<VideoFilterService>(
+            ['initFilterFromMediaStream', 'startFilteredStream', 'doesSupportVideoFiltering'],
+            ['filterOn']
+        );
         const filterStreamTracks = [];
         filterStreamTracks.push(
             jasmine.createSpyObj<MediaStreamTrack>(['stop'])
@@ -145,9 +144,26 @@ describe('MediaStreamService', () => {
         it('should return a promise from getUserMedia', fakeAsync(() => {
             // Arrange
             const expectedStream = new MediaStream();
-            const expectedStreamClone = spyOn(expectedStream, 'clone');
-            expectedStreamClone.and.returnValue(expectedStream);
             mediaDevicesSpy.getUserMedia.and.resolveTo(expectedStream);
+            getSpiedPropertyGetter(videoFilterStreamServiceSpy, 'filterOn').and.returnValue(false);
+
+            // Act
+            let resultantStream = null;
+            sut.getStreamForCam(cameraDevice).subscribe(stream => (resultantStream = stream));
+            flush();
+
+            // Assert
+            expect(mediaDevicesSpy.getUserMedia).toHaveBeenCalledWith(cameraConstraintBuilder(cameraDevice));
+
+            expect(resultantStream).toBe(expectedStream);
+            expect(errorServiceSpy.handlePexipError).not.toHaveBeenCalled();
+        }));
+
+        it('should return a filtered stream when filter is on', fakeAsync(() => {
+            // Arrange
+            const expectedStream = new MediaStream();
+            mediaDevicesSpy.getUserMedia.and.resolveTo(expectedStream);
+            getSpiedPropertyGetter(videoFilterStreamServiceSpy, 'filterOn').and.returnValue(true);
 
             // Act
             let resultantStream = null;
@@ -158,7 +174,6 @@ describe('MediaStreamService', () => {
             expect(mediaDevicesSpy.getUserMedia).toHaveBeenCalledWith(cameraConstraintBuilder(cameraDevice));
 
             expect(resultantStream).toBe(filterStream);
-            expect(expectedStreamClone).toHaveBeenCalledTimes(1);
             expect(errorServiceSpy.handlePexipError).not.toHaveBeenCalled();
         }));
 
