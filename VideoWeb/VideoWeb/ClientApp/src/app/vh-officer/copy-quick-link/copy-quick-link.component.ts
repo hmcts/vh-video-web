508fcec4d6c46ec7570a08e73ac478205ea9ee71
alberto-hortiguela <alberto.hortiguela@version1.com>
2021-09-15 11:06:17 +0100

    VIH-8207-remove unncessary onChange

--- VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/copy-quick-link/copy-quick-link.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/vh-officer/copy-quick-link/copy-quick-link.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ClipboardService } from 'ngx-clipboard';
 import { VhoQueryService } from '../services/vho-query-service.service';
