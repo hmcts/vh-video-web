f26f51cb35e32929a0b8cd4e66255c481e2e73fd
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-09-23 15:58:44 +0100

    VIH-8215 EJUD Feature flag for idp-section and login page (#1589)
    * VIH-8215 ejud feature flag enabled
    
    * VIH-8125 added missing file
    
    * remove tests
    
    * VIH-8215 - feature flag acceptance tests
    
    * lint
    
    * lint
    
    * extracted to a method

--- VideoWeb/VideoWeb/ClientApp/src/app/security/auth.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/auth.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
+import { FeatureFlagService } from '../services/feature-flag.service';
 import { Logger } from '../services/logging/logger-base';
 import { pageUrls } from '../shared/page-url.constants';
 import { SecurityServiceProvider } from './authentication/security-provider.service';
@@ -10,7 +11,12 @@ import { ISecurityService } from './authentication/security-service.interface';
 @Injectable()
 export class AuthGuard implements CanActivate {
     private securityService: ISecurityService;
-    constructor(securityServiceProviderService: SecurityServiceProvider, private router: Router, private logger: Logger) {
+    constructor(
+        securityServiceProviderService: SecurityServiceProvider,
+        private router: Router,
+        private logger: Logger,
+        private featureFlagService: FeatureFlagService
+    ) {
         securityServiceProviderService.currentSecurityService$.subscribe(securityService => {
             this.securityService = securityService;
         });
@@ -21,7 +27,13 @@ export class AuthGuard implements CanActivate {
             map((isAuthorized: boolean) => {
                 this.logger.debug('AuthorizationGuard, canActivate isAuthorized: ' + isAuthorized);
                 if (!isAuthorized) {
-                    this.router.navigate([`/${pageUrls.IdpSelection}`]);
+                    this.featureFlagService
+                        .getFeatureFlagByName('EJudFeature')
+                        .pipe(first())
+                        .subscribe(result => {
+                            const routePath = result ? `/${pageUrls.IdpSelection}` : `/${pageUrls.Login}`;
+                            this.router.navigate([routePath]);
+                        });
                     return false;
                 }
                 return true;
