d0daed31925b69b63e7f086a626d9ca51fd76e3a
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-10-04 15:10:58 +0100

    VIH-8291 Remove unused code

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/joh-waiting-room/joh-waiting-room.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/joh-waiting-room/joh-waiting-room.component.spec.ts
@@ -92,26 +92,6 @@ describe('JohWaitingRoomComponent', () => {
         videoWebService.getConferenceById.calls.reset();
     });
 
-    describe('toggleParticipantsPanel', () => {
-        it('should switch isParticipantsPanelHidden from false to true to false to true to false when starting from false', () => {
-            // Arrange
-            component.isParticipantsPanelHidden = false;
-
-            // Act & Assert
-            component.toggleParticipantsPanel();
-            expect(component.isParticipantsPanelHidden).toBe(true);
-
-            component.toggleParticipantsPanel();
-            expect(component.isParticipantsPanelHidden).toBe(false);
-
-            component.toggleParticipantsPanel();
-            expect(component.isParticipantsPanelHidden).toBe(true);
-
-            component.toggleParticipantsPanel();
-            expect(component.isParticipantsPanelHidden).toBe(false);
-        });
-    });
-
     describe('get allowAudioOnlyToggle', () => {
         it('should return false if the conference is null', () => {
             // Arrange
