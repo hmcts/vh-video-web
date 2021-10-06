c45d164973e0777dd46d433380190a0194c13dc4
muktadiru <muktadiru.ahmed@google.com>
2021-10-06 09:57:09 +0100

    moved style to shared file

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/context-menu-header/context-menu-header.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/context-menu-header/context-menu-header.component.ts
@@ -2,8 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-context-menu-header',
-    templateUrl: './context-menu-header.component.html',
-    styleUrls: ['./context-menu-header.component.scss']
+    templateUrl: './context-menu-header.component.html'
 })
 export class ContextMenuHeaderComponent {
     @Input() public isPrivateConsultation: boolean;
