067825cdaf183562f696346e108459d2e9ca4a29
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2021-10-12 14:43:43 +0100

    VIH-8131 increase test coverage

--- VideoWeb/VideoWeb.UnitTests/Controllers/ConferenceManagement/StartVideoHearingTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/ConferenceManagement/StartVideoHearingTests.cs
@@ -1,16 +1,15 @@
-using System;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
-using VideoWeb.Common.Models;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using VideoApi.Client;
 using VideoApi.Contract.Requests;
-using VideoApi.Contract.Responses;
+using VideoWeb.Common.Models;
 using VideoWeb.UnitTests.Builders;
 using ProblemDetails = Microsoft.AspNetCore.Mvc.ProblemDetails;
 
@@ -25,7 +24,7 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
         }
 
         [Test]
-        public async Task should_return_unauthorised_if_user_not_judge()
+        public async Task Should_return_unauthorised_if_user_not_judge()
         {
             var participant = TestConference.Participants.First(x => x.Role == Role.Individual);
             var user = new ClaimsPrincipalBuilder()
@@ -46,7 +45,7 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
         }
 
         [Test]
-        public async Task should_return_unauthorised_if_judge_not_assigned_to_conference()
+        public async Task Should_return_unauthorised_if_judge_not_assigned_to_conference()
         {
             var user = new ClaimsPrincipalBuilder()
                 .WithUsername("notforconference@hmcts.net")
@@ -65,7 +64,7 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
         }
 
         [Test]
-        public async Task should_return_video_api_error()
+        public async Task Should_return_video_api_error()
         {
             var participant = TestConference.GetJudge();
             var user = new ClaimsPrincipalBuilder()
@@ -91,7 +90,7 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
         }
 
         [Test]
-        public async Task should_return_accepted_when_user_is_judge_in_conference()
+        public async Task Should_return_accepted_when_user_is_judge_in_conference()
         {
             var participant = TestConference.GetJudge();
             var user = new ClaimsPrincipalBuilder()
@@ -109,17 +108,21 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
                 It.Is<StartHearingRequest>(r => r.Layout == HearingLayout.Dynamic)), Times.Once);
         }
         
-        [Test]
-        public async Task should_send_all_judges_and_staff_members_in_conference_as_participants_to_transfer()
+        [TestCase(AppRoles.JudgeRole)]
+        [TestCase(AppRoles.StaffMember)]
+        public async Task Should_send_all_Hosts_in_conference_as_participants_to_transfer(string role)
         {
             var participant = TestConference.GetJudge();
            
             var user = new ClaimsPrincipalBuilder()
                 .WithUsername(participant.Username)
-                .WithRole(AppRoles.JudgeRole).Build();
+                .WithRole(role).Build();
 
             var expectedParticipantsToForceTransfer = TestConference.Participants
                .Where(x => x.Username.Equals(user.Identity?.Name)).Select(x => x.Id.ToString());
+
+            var expectedParticipantsToNotForceTransfer = TestConference.Participants
+                .Where(x => x.Role.ToString() != role).Select(x => x.Id.ToString());
 
             // ConferenceCache is mocked in the base class for these tests...
 
@@ -132,6 +135,8 @@ namespace VideoWeb.UnitTests.Controllers.ConferenceManagement
 
             VideoApiClientMock.Verify(x => x.StartOrResumeVideoHearingAsync(TestConference.Id,
                 It.Is<StartHearingRequest>(r => r.Layout == HearingLayout.Dynamic && r.ParticipantsToForceTransfer.SequenceEqual(expectedParticipantsToForceTransfer) && r.MuteGuests == true)), Times.Once);
+            VideoApiClientMock.Verify(x => x.StartOrResumeVideoHearingAsync(TestConference.Id,
+                It.Is<StartHearingRequest>(r => r.Layout == HearingLayout.Dynamic && r.ParticipantsToForceTransfer.SequenceEqual(expectedParticipantsToNotForceTransfer) && r.MuteGuests == true)), Times.Never);
         }
     }
 }
