8059aa2edc944919cbdc4425cc096860384058ce
Robert Bennett <robertbennett1998@outlook.com>
2021-10-18 12:30:16 +0100

    Added additional logging

--- VideoWeb/VideoWeb/Helpers/HearingLayoutService.cs
+++ VideoWeb/VideoWeb/Helpers/HearingLayoutService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,13 +14,15 @@ namespace VideoWeb.Helpers
 {
     public class HearingLayoutService : IHearingLayoutService
     {
+        private readonly ILogger<HearingLayoutService> _logger;
         private readonly IVideoApiClient _videoApiClient;
         private readonly IConferenceCache _conferenceCache;
         private readonly IHearingLayoutCache _hearingLayoutCache;
         private readonly IHubContext<EventHub.Hub.EventHub, IEventHubClient> _hubContext;
 
-        public HearingLayoutService(IVideoApiClient videoApiClient, IConferenceCache conferenceCache, IHearingLayoutCache hearingLayoutCache, IHubContext<EventHub.Hub.EventHub, IEventHubClient> hubContext)
+        public HearingLayoutService(ILogger<HearingLayoutService> logger, IVideoApiClient videoApiClient, IConferenceCache conferenceCache, IHearingLayoutCache hearingLayoutCache, IHubContext<EventHub.Hub.EventHub, IEventHubClient> hubContext)
         {
+            _logger = logger;
             _videoApiClient = videoApiClient;
             _conferenceCache = conferenceCache;
             _hearingLayoutCache = hearingLayoutCache;
@@ -64,19 +67,30 @@ namespace VideoWeb.Helpers
             {
                 conference = await GetConferenceFromCache(conferenceId);
             }
-            catch (Exception)
+            catch (Exception exception)
             {
+                _logger.LogError($"Failed to update layout for conferece {conferenceId} to {newLayout} change requested by {changedById}", exception);
                 return;
             }
 
             var hearingLayout = await GetHearingLayoutFromCache(conferenceId);
             var oldLayout = hearingLayout ?? conference.GetRecommendedLayout();
+
+            _logger.LogWarning($"Hearing layout changed for {conferenceId} from {oldLayout} to {newLayout} by participant with the ID {changedById}.");
+
             await SetHearingLayoutInCache(conferenceId, newLayout);
 
-            await _hubContext.Clients
-                            .Groups(conference.Participants
+            _logger.LogWarning($"Set hearing layout in the cache for {conferenceId}.");
+
+
+            var hosts = conference.Participants
                             .Where(participant => participant.IsHost())
-                            .Select(participant => participant.Username.ToLowerInvariant()).ToList())
+                            .Select(participant => participant.Username.ToLowerInvariant());
+
+            _logger.LogWarning($"Sending message to {hosts} for layout change in {conferenceId}.");
+
+            await _hubContext.Clients
+                            .Groups(hosts.ToList())
                             .HearingLayoutChanged(conferenceId, changedById, newLayout, oldLayout);
         }
     }
