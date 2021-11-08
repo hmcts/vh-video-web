caba1a521dc3d5e83f81f5b33bcfb25252802060
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2021-11-08 16:30:25 +0000

    Fixed missing reference

--- VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/staff-member-hearing-selection/staff-member-hearing-selection.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/on-the-day/staff-member-hearing-selection/staff-member-hearing-selection.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-staff-member-hearing-selection',
-    templateUrl: './staff-member-hearing-selection.component.html',
-    styleUrls: ['./staff-member-hearing-selection.component.css']
+    templateUrl: './staff-member-hearing-selection.component.html'
 })
 export class StaffMemberHearingSelectionComponent {
     constructor() {}
