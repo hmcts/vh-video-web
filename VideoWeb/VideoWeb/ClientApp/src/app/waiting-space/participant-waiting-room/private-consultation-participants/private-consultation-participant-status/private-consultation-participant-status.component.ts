344bf3210f4c962d660eee6a4adea63f2942ff6a
Farron Flaherty <farron.flaherty@hmcts.net>
2021-12-06 14:43:21 +0000

    VIH-8451 Moving html to templates. (#1727)
    * Reapplying changes moving html to templates
    
    * Adding directive and component to reduce code duplication
    
    * Adding on destroy
    
    * Adding and using pipe for room name
    
    * removing directive
    
    * Adding mocks to test beds
    
    * prettify
    
    * Removing empty methods
    
    * Removing empty constructor
    
    * Toggle participant icon for JOH and participant in private consultation
    
    * Adding unit tests
    
    * Removing console log
    
    * Adding unit tests for areParticipantsVisible
    
    * Renaming component to avoid confusion with other with same name.
    
    * Updating test describe
    
    Co-authored-by: omio-ma <84961314+omio-ma@users.noreply.github.com>

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-waiting-room/private-consultation-participants/private-consultation-participant-status/private-consultation-participant-status.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { ConsultationAnswer, EndpointResponse, ParticipantResponse } from 'src/app/services/clients/api-client';
+
+@Component({
+    selector: 'app-private-consultation-participant-status',
+    templateUrl: './private-consultation-participant-status.component.html',
+    styleUrls: ['./private-consultation-participant-status.component.scss']
+})
+export class PrivateConsultationParticipantStatusComponent {
+    ConsultationAnswer = ConsultationAnswer;
+    @Input() entity: ParticipantResponse | EndpointResponse;
+    @Input() status: string;
+    @Input() roomLabel: string;
+    private availableStatuses = ['Available', 'Connected', 'InConsultation'];
+
+    isAvailable(): boolean {
+        return this.availableStatuses.indexOf(this.entity.status) >= 0;
+    }
+
+    isInCurrentRoom(): boolean {
+        return this.entity?.current_room?.label === this.roomLabel;
+    }
+}
