15bb8cdec1c973add613a081069747d164c3ea24
Zijian Wang <Zijian.Wang@hmcts.net>
2021-12-07 17:36:00 +0000

    Revert "Dual Host stuck when trying to leave hearing"
    This reverts commit d1eecfd3fdffd82f4084c100265e6c75d79461cc.

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/private-consultation-room-controls.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/private-consultation-room-controls/private-consultation-room-controls.component.spec.ts
@@ -743,7 +743,6 @@ describe('PrivateConsultationRoomControlsComponent', () => {
     describe('leave', () => {
         it('should call super leave method with participants', () => {
             const spy = spyOn(HearingControlsBaseComponent.prototype, 'leave');
-            spyOn(component, 'isAnotherHostInHearing').and.returnValue(true);
             getSpiedPropertyGetter(participantServiceSpy, 'participants').and.returnValue([
                 new ParticipantModel(
                     '7879c48a-f513-4d3b-bb1b-151831427507',
