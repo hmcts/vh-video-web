fe4927311157b149c4cd03620a1998bc210afa71
Zijian Wang <47365487+ZJianWang@users.noreply.github.com>
2021-12-13 09:36:27 +0000

    VIH-8525 Investigate and fix get User Profile failed (#1742)
    * VIH-8525 Investigate and fix get User Profile failed
    
    * Apply auth base guard for judge guard
    
    * fix sonar cloud

--- VideoWeb/VideoWeb/ClientApp/src/app/security/auth.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/auth.guard.ts
@@ -1,43 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { first, map } from 'rxjs/operators';
 import { FeatureFlagService } from '../services/feature-flag.service';
 import { Logger } from '../services/logging/logger-base';
-import { pageUrls } from '../shared/page-url.constants';
+import { AuthBaseGuard } from './auth-base.guard';
 import { SecurityServiceProvider } from './authentication/security-provider.service';
-import { ISecurityService } from './authentication/security-service.interface';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
-    private securityService: ISecurityService;
+export class AuthGuard extends AuthBaseGuard implements CanActivate {
     constructor(
         securityServiceProviderService: SecurityServiceProvider,
-        private router: Router,
-        private logger: Logger,
-        private featureFlagService: FeatureFlagService
+        protected router: Router,
+        protected logger: Logger,
+        protected featureFlagService: FeatureFlagService
     ) {
-        securityServiceProviderService.currentSecurityService$.subscribe(securityService => {
-            this.securityService = securityService;
-        });
+        super(securityServiceProviderService, router, logger, featureFlagService);
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-        return this.securityService.isAuthenticated$.pipe(
-            map((isAuthorized: boolean) => {
-                this.logger.debug('AuthorizationGuard, canActivate isAuthorized: ' + isAuthorized);
-                if (!isAuthorized) {
-                    this.featureFlagService
-                        .getFeatureFlagByName('EJudFeature')
-                        .pipe(first())
-                        .subscribe(result => {
-                            const routePath = result ? `/${pageUrls.IdpSelection}` : `/${pageUrls.Login}`;
-                            this.router.navigate([routePath]);
-                        });
-                    return false;
-                }
-                return true;
-            })
-        );
+        return this.isUserAuthorized(route, state);
     }
 }
--- VideoWeb/VideoWeb/ClientApp/src/app/security/participant.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/participant.guard.ts
@@ -1,36 +1,56 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { take } from 'rxjs/operators';
 import { ProfileService } from '../services/api/profile.service';
 import { Role } from '../services/clients/api-client';
+import { FeatureFlagService } from '../services/feature-flag.service';
 import { Logger } from '../services/logging/logger-base';
+import { AuthBaseGuard } from './auth-base.guard';
+import { SecurityServiceProvider } from './authentication/security-provider.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ParticipantGuard implements CanActivate {
-    constructor(private userProfileService: ProfileService, private router: Router, private logger: Logger) {}
+export class ParticipantGuard extends AuthBaseGuard implements CanActivate {
+    constructor(
+        protected featureFlagService: FeatureFlagService,
+        securityServiceProviderService: SecurityServiceProvider,
+        protected userProfileService: ProfileService,
+        protected router: Router,
+        protected logger: Logger
+    ) {
+        super(securityServiceProviderService, router, logger, featureFlagService);
+    }
 
     async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-        this.logger.debug(`[ParticipantGuard] Checking if user is a representative or individual.`);
-        try {
-            const profile = await this.userProfileService.getUserProfile();
-            if (
-                profile.role === Role.Representative ||
-                profile.role === Role.Individual ||
-                profile.role === Role.QuickLinkParticipant ||
-                profile.role === Role.QuickLinkObserver
-            ) {
-                this.logger.debug(`[ParticipantGuard] User is a representative or individual.`);
-                return true;
-            } else {
-                this.logger.debug(`[ParticipantGuard] User is not a representative or individual. Going home.`);
-                this.router.navigate(['/home']);
-                return false;
-            }
-        } catch (err) {
-            this.logger.error(`[ParticipantGuard] Failed to get user profile. Logging out.`, err);
-            this.router.navigate(['/logout']);
-            return false;
-        }
+        return this.isUserAuthorized(next, state)
+            .pipe(take(1))
+            .toPromise()
+            .then(async (auth: boolean) => {
+                if (!auth) {
+                    this.router.navigate(['/login']);
+                    return false;
+                }
+                try {
+                    const profile = await this.userProfileService.getUserProfile();
+                    if (
+                        profile.role === Role.Representative ||
+                        profile.role === Role.Individual ||
+                        profile.role === Role.QuickLinkParticipant ||
+                        profile.role === Role.QuickLinkObserver
+                    ) {
+                        this.logger.debug(`[ParticipantGuard] User is a representative or individual.`);
+                        return true;
+                    } else {
+                        this.logger.debug(`[ParticipantGuard] User is not a representative or individual. Going home.`);
+                        this.router.navigate(['/home']);
+                        return false;
+                    }
+                } catch (err) {
+                    this.logger.error(`[ParticipantGuard] Failed to get user profile. Logging out.`, err);
+                    this.router.navigate(['/logout']);
+                    return false;
+                }
+            });
     }
 }
