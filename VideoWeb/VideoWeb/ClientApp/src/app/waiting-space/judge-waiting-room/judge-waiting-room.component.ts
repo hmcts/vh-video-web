0f19fdb1474f6e4642059dee0c7ce6db399981aa
Prasanna Dommalapati <prasanna.dommalapati@gmail.com>
2022-01-05 16:24:25 +0000

    lint

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/judge-waiting-room.component.ts
@@ -216,10 +216,7 @@ export class JudgeWaitingRoomComponent extends WaitingRoomBaseDirective implemen
     assignPexipIdToRemoteStore(participant: ParticipantUpdated): void {
         const participantDisplayName = PexipDisplayNameModel.fromString(participant.pexipDisplayName);
         if (participant.uuid && participantDisplayName?.participantOrVmrId) {
-            this.participantRemoteMuteStoreService.assignPexipId(
-                participantDisplayName.participantOrVmrId,
-                participant.uuid
-            );
+            this.participantRemoteMuteStoreService.assignPexipId(participantDisplayName.participantOrVmrId, participant.uuid);
         }
         this.logger.debug(`${this.loggerPrefixJudge} stored pexip ID updated`, {
             pexipId: participant.uuid,
