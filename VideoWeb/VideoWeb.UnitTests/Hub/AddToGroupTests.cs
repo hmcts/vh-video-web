eadf1a35b2c78032267c8907c7df38c577b99019
muktadiru <muktadiru.ahmed@google.com>
2022-01-14 17:26:00 +0000

    updated unit tests

--- VideoWeb/VideoWeb.UnitTests/Hub/AddToGroupTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Hub/AddToGroupTests.cs
@@ -3,6 +3,8 @@ using NUnit.Framework;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using VideoWeb.Common.Models;
+using VideoWeb.UnitTests.Builders;
 
 namespace VideoWeb.UnitTests.Hub
 {
@@ -12,12 +14,28 @@ namespace VideoWeb.UnitTests.Hub
         [Test]
         public async Task Add_To_Group_Updates_Group_With_New_Conference()
         {
+            Claims = new ClaimsPrincipalBuilder().WithRole(AppRoles.VhOfficerRole).Build();
+            HubCallerContextMock.Setup(x => x.User).Returns(Claims);
+
             var newConferenceId = Guid.NewGuid().ToString();
             await Hub.AddToGroup(newConferenceId);
 
             GroupManagerMock.Verify(
                 x => x.AddToGroupAsync(HubCallerContextMock.Object.ConnectionId, newConferenceId,
                     CancellationToken.None), Times.Once);
+        }
+
+        [Test]
+        public async Task Add_To_Group_Does_Not_Update_Group_With_New_Conference_For_Non_Vho_Sender()
+        {
+            Claims = new ClaimsPrincipalBuilder().WithRole(AppRoles.CitizenRole).Build();
+            HubCallerContextMock.Setup(x => x.User).Returns(Claims);
+
+            await Hub.AddToGroup(Guid.NewGuid().ToString());
+
+            GroupManagerMock.Verify(
+                x => x.AddToGroupAsync(HubCallerContextMock.Object.ConnectionId, It.IsAny<string>(),
+                    CancellationToken.None), Times.Never);
         }
     }
 }
