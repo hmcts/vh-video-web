47dc85335d1b038e52a5fe692fbf5de54d2adb70
anaia <anai.agu@hmcts.net>
2022-01-28 16:11:17 +0000

    Updated contact details object structure

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/contact-information.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/contact-information.ts
@@ -1,4 +1,10 @@
 export const vhContactDetails = {
-    phone: '0300 303 0655',
-    adminEmail: 'video-hearings@justice.gov.uk'
+    uk: {
+        phoneNumber: '0300 303 0655',
+        email: 'video-hearings@justice.gov.uk'
+    },
+    scotland: {
+        phoneNumber: '0300 790 6234',
+        email: 'SSCSA-Glasgow@Justice.gov.uk'
+    }
 };
