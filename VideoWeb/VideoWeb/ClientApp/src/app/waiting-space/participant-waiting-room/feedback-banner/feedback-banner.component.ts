5f0f242434c87a68983910302b439f1491247870
FarronF <farron.flaherty@hmcts.net>
2022-02-10 10:01:47 +0000

    Removing empty methods

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-waiting-room/feedback-banner/feedback-banner.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-waiting-room/feedback-banner/feedback-banner.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-feedback-banner',
     templateUrl: './feedback-banner.component.html',
     styleUrls: ['./feedback-banner.component.scss']
 })
-export class FeedbackBannerComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit(): void {}
-}
+export class FeedbackBannerComponent {}
