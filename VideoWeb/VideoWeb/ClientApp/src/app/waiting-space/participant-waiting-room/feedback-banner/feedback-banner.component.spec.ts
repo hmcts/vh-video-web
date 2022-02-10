1678ed02a54f70d5e817b930aef1e90af30e94ef
FarronF <farron.flaherty@hmcts.net>
2022-02-10 09:56:30 +0000

    linting and pretiier

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-waiting-room/feedback-banner/feedback-banner.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/participant-waiting-room/feedback-banner/feedback-banner.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FeedbackBannerComponent } from './feedback-banner.component';
 
 describe('FeedbackBannerComponent', () => {
-  let component: FeedbackBannerComponent;
-  let fixture: ComponentFixture<FeedbackBannerComponent>;
+    let component: FeedbackBannerComponent;
+    let fixture: ComponentFixture<FeedbackBannerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FeedbackBannerComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [FeedbackBannerComponent]
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FeedbackBannerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(FeedbackBannerComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
