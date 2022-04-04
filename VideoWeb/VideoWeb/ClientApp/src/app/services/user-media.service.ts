150ea34655150c1dac10751d148935873bbeca52
Arif Ahmed <arifahmed2009@hotmail.co.uk>
2022-04-04 15:07:44 +0100

    Removed console.log

--- VideoWeb/VideoWeb/ClientApp/src/app/services/user-media.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/user-media.service.ts
@@ -200,7 +200,6 @@ export class UserMediaService {
             take(1),
             map(stream => !!stream && stream.getVideoTracks().length > 0 && stream.getAudioTracks().length > 0),
             catchError(error => {
-                console.log(error);
                 this.logger.error(`${this.loggerPrefix} couldn't get a valid camera and microphone`, error);
                 if (error.message.includes('Permission denied') || error.message.includes('Permission dismissed')) {
                     this.errorService.goToServiceError(
