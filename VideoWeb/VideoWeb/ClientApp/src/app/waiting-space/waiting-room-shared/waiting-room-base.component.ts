1025e66f597a2cda232273bf9b1d86e6c9d02397
Oliver Scott <oliver.scott@justice.gov.uk>
2022-06-27 11:39:25 +0100

    Fix issue - WIP

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-room-shared/waiting-room-base.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-room-shared/waiting-room-base.component.ts
@@ -898,6 +898,13 @@ export abstract class WaitingRoomBaseDirective {
             conference: this.conferenceId,
             participant: this.participant.id
         });
+
+        if (error.reason.includes('IP address')) {
+            // TODO implement retry loop
+            // TODO move to errorService
+            return;
+        }
+
         this.errorService.handlePexipError(error, this.conferenceId);
     }
 
