be10490697e5e0620312b397e2d02350d48ac4ac
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-07-27 08:23:29 +0100

    VIH-9189 - added trap ficus and first element focus - added tests

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-leave-hearing-popup.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-leave-hearing-popup.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { YesNoPopupBaseDirective } from './yes-no-popup-base.component';
+import { ModalTrapFocus } from '../../shared/modal/modal-trap-focus';
 
 @Component({
     selector: 'app-confirm-leave-hearing-popup',
     templateUrl: './confirm-leave-hearing-popup.component.html',
     styleUrls: ['./yes-no-popup-base.component.scss']
 })
-export class ConfirmLeaveHearingPopupComponent extends YesNoPopupBaseDirective {}
+
+export class ConfirmLeaveHearingPopupComponent extends YesNoPopupBaseDirective implements AfterViewInit {
+    private readonly LEAVE_MODAL = 'confirm-leave-hearing-modal';
+
+    @ViewChild('btnConfirmStartRef') btnLeave: ElementRef;
+
+    ngAfterViewInit(): void {
+        ModalTrapFocus.trap(this.LEAVE_MODAL);
+        this.btnLeave.nativeElement.focus();
+    }
+}
