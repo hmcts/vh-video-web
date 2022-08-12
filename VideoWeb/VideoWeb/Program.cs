fd45e8d63012d975afdc9e7336a63a8a29765eff
Your Name <james.ferrari@version1.com>
2022-08-12 11:44:16 +0100

    add System.Collections.Generic

--- VideoWeb/VideoWeb/Program.cs
+++ VideoWeb/VideoWeb/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using VH.Core.Configuration;
+using System.Collections.Generic;
 
 namespace VideoWeb
 {
