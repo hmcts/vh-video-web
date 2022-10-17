f2a0b54bb6799e4bdeadadb6235013def8478a19
marcogagliardi <marco.gagliardi@justice.gov.uk>
2022-10-17 11:37:07 +0100

    Revert "VIH-9051 - duplicate code fix"
    This reverts commit e499d608b3f55decdd74b1c4475ca5a43f1f5bd2.

--- VideoWeb/VideoWeb/ClientApp/src/app/security/participant-status.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/participant-status.guard.ts
@@ -33,7 +33,12 @@ export class ParticipantStatusGuard extends AuthBaseGuard implements CanActivate
             .pipe(take(1))
             .toPromise()
             .then(async (auth: boolean) => {
-                this.logger.debug(`[ParticipantStatusGuard] Checking if user is a individual`);
+                if (!auth) {
+                    this.router.navigate([pageUrls.Login]);
+                    return false;
+                }
+
+                this.logger.debug(`[ParticipantStatusGuard] Checking if user is a admin`);
                 try {
                     const profile = await this.userProfileService.getUserProfile();
 
--- VideoWeb/VideoWeb/ClientApp/src/app/security/staff-member.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/staff-member.guard.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { ProfileService } from '../services/api/profile.service';
+import { Role } from '../services/clients/api-client';
 import { Logger } from '../services/logging/logger-base';
 import { AuthBaseGuard } from './auth-base.guard';
 import { SecurityServiceProvider } from './authentication/security-provider.service';
 import { FeatureFlagService } from '../services/feature-flag.service';
 import { take } from 'rxjs/operators';
+import { pageUrls } from '../shared/page-url.constants';
 
 @Injectable({
     providedIn: 'root'
@@ -26,15 +28,27 @@ export class StaffMemberGuard extends AuthBaseGuard implements CanActivate {
             .pipe(take(1))
             .toPromise()
             .then(async (auth: boolean) => {
-                const result = await this.checkUserProfileAuthorisation(auth, next, this.userProfileService, '[StaffMemberGuard]');
-                let canAuth = false;
-                if (result != '') {
-                    canAuth = true;
+                if (!auth) {
+                    this.router.navigate([pageUrls.Login]);
+                    return false;
                 }
-                else {
-                    this.router.navigate([result]);
+
+                this.logger.debug(`[StaffMemberGuard] Checking if user is a Staff Member`);
+                try {
+                    const profile = await this.userProfileService.getUserProfile();
+                    if (profile.role === Role.StaffMember) {
+                        this.logger.debug(`[StaffMemberGuard] User is a Staff Member.`);
+                        return true;
+                    } else {
+                        this.logger.debug(`[StaffMemberGuard] User is not a Staff Member. Going back home`);
+                        this.router.navigate([pageUrls.Home]);
+                        return false;
+                    }
+                } catch (err) {
+                    this.logger.error(`[StaffMemberGuard] Failed to get user profile. Logging out.`, err);
+                    this.router.navigate([pageUrls.Logout]);
+                    return false;
                 }
-                return canAuth;
             });
     }
 }
