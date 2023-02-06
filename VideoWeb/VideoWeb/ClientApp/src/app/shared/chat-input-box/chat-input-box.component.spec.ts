6032aa80942f33f1caa9f1c79c8c41b6cde4b7c9
Oliver Scott <oliver.scott@justice.gov.uk>
2023-02-06 08:49:37 +0000

    VIH-8608 Announce chat box character limit to screen readers (#1959)
    * Announce chat box limits to screen readers
    
    * Rename
    
    * Implement translations
    
    * Fix tests
    
    * Use ViewChild
    
    * Lint and prettier
    
    * More test coverage
    
    * More test coverage

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/chat-input-box/chat-input-box.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/chat-input-box/chat-input-box.component.spec.ts
@@ -1,13 +1,21 @@
 import { ChatInputBoxComponent } from './chat-input-box.component';
+import { translateServiceSpy } from 'src/app/testing/mocks/mock-translation.service';
+import { ElementRef } from '@angular/core';
 
 describe('ChatInputBoxComponent', () => {
     let component: ChatInputBoxComponent;
     let emitSpy;
+    const translateService = translateServiceSpy;
+    const SCREEN_READER_INPUT_ALERT_MESSAGE = 'chat-input-box.maximum-characters-entered';
 
     beforeEach(() => {
-        component = new ChatInputBoxComponent();
+        translateService.instant.calls.reset();
+
+        component = new ChatInputBoxComponent(translateService);
+        component.screenReaderInputLimitAlert = new ElementRef(document.createElement('div'));
         emitSpy = spyOn(component.submittedMessage, 'emit');
         component.ngOnInit();
+        component.ngAfterViewInit();
     });
 
     it('should create', () => {
@@ -91,8 +99,79 @@ describe('ChatInputBoxComponent', () => {
         expect(component.isSendingBlocked).toBeFalsy();
     });
 
+    describe('onKeyup', () => {
+        it('should hide input alert for screen readers when current input length is less than max length', () => {
+            const body = 'Lorem';
+            setTextInput(body);
+            const event = new KeyboardEvent('keyup', {
+                key: 'a'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe('');
+        });
+
+        it('should hide existing input alert for screen readers when current input length is less than max length', () => {
+            const body = 'Lorem';
+            setTextInput(body);
+            showInputAlertForScreenReaders();
+            const event = new KeyboardEvent('keyup', {
+                key: 'a'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe('');
+        });
+
+        it('should hide input alert for screen readers when current input length is greater than max length and Delete key pressed', () => {
+            const body =
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            setTextInput(body);
+            const event = new KeyboardEvent('keyup', {
+                key: 'Delete'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe('');
+        });
+
+        it('should hide input alert for screen readers when current input length is greater than max length and Backspace key pressed', () => {
+            const body =
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            setTextInput(body);
+            const event = new KeyboardEvent('keyup', {
+                key: 'Backspace'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe('');
+        });
+
+        it('should show input alert for screen readers when current input length is greater than max length and key pressed', () => {
+            const body =
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+            setTextInput(body);
+            const event = new KeyboardEvent('keyup', {
+                key: 'a'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe(SCREEN_READER_INPUT_ALERT_MESSAGE);
+        });
+
+        it('should show input alert for screen readers when current input length is equal to max length and key pressed', () => {
+            const body =
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in';
+            setTextInput(body);
+            const event = new KeyboardEvent('keyup', {
+                key: 'a'
+            });
+            component.onKeyup(event);
+            expect(component.screenReaderInputLimitAlert.nativeElement.textContent).toBe(SCREEN_READER_INPUT_ALERT_MESSAGE);
+        });
+    });
+
     function setTextInput(value: string) {
         component.newMessageBody.setValue(value);
         component.newMessageBody.markAsDirty();
+    }
+
+    function showInputAlertForScreenReaders() {
+        component.screenReaderInputLimitAlert.nativeElement.textContent = SCREEN_READER_INPUT_ALERT_MESSAGE;
     }
 });
