add2e7ac0cdfd5cff5194e2c8be0a5b68bd7dfb8
Oliver Scott <oliver.scott@justice.gov.uk>
2023-02-20 11:34:07 +0000

    Update auth package, revert subscribe changes

--- VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/logout/logout.component.ts
@@ -40,7 +40,7 @@ export class LogoutComponent implements OnInit {
                 console.log('LogoutComponent - clearing judge allocation storage');
                 this.judgeAllocationStorage.clear();
                 console.log('LogoutComponent - logging off and revoking tokens');
-                this.securityService.logoffAndRevokeTokens().subscribe();
+                this.securityService.logoffAndRevokeTokens();
             } else {
                 console.log('LogoutComponent - not authenticated');
             }
