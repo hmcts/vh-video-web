ddbd9a989380767b7c165f7d96229f1711a8c727
marcogagliardi <marco.gagliardi@justice.gov.uk>
2023-03-01 13:16:07 +0000

    Update AllocationHearingsTests.cs

--- VideoWeb/VideoWeb.UnitTests/Controllers/InternalEventController/AllocationHearingsTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/InternalEventController/AllocationHearingsTests.cs
@@ -51,7 +51,6 @@ namespace VideoWeb.UnitTests.Controllers.InternalEventControllerTests
             _controller.ControllerContext = context;
 
             _mocker.Mock<IAllocationHearingsEventNotifier>();
-
         }
 
 
@@ -72,6 +71,30 @@ namespace VideoWeb.UnitTests.Controllers.InternalEventControllerTests
 
             // Assert
             result.Should().BeOfType<NoContentResult>();
+
+            _mocker.Mock<IAllocationHearingsEventNotifier>().Verify(x => x.PushAllocationHearingsEvent("csousername@email.com", allocatedHearingsDetails), Times.Once);
+        }
+        
+        [Test]
+        public async Task Should_not_send_event_if_exception_throwed()
+        {
+            _mocker.Mock<IAllocationHearingsEventNotifier>().Setup(x =>
+                    x.PushAllocationHearingsEvent("csousername@email.com", new List<HearingDetailRequest>()))
+                .Throws(new VideoApiException("error", StatusCodes.Status500InternalServerError, "", null, null));
+            // Arrange
+            var allocatedHearingsDetails = new List<HearingDetailRequest>();
+            var allocationHearingsToCsoRequest = new AllocationHearingsToCsoRequest()
+            {
+                Hearings = allocatedHearingsDetails,
+                AllocatedCsoUserName = "csousername@email.com"
+            };
+            
+
+            // Act
+            var result = await _controller.AllocationHearings(allocationHearingsToCsoRequest);
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
 
             _mocker.Mock<IAllocationHearingsEventNotifier>().Verify(x => x.PushAllocationHearingsEvent("csousername@email.com", allocatedHearingsDetails), Times.Once);
         }
