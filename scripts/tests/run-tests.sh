1dfe63de42ac4f7a7143adc09238953af945f437
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-22 17:52:30 +0000

    update sonar settings name

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -9,7 +9,7 @@ rm -d -r ${PWD}/VideoWeb/VideoWeb/ClientApp/node_modules
 configuration=Release
 exclusions="[VideoWeb]VideoWeb.ConfigureServicesExtensions,[VideoWeb]VideoWeb.Program,[*]VideoWeb.Extensions.*[VideoWeb]VideoWeb.Startup,[Testing.Common]*,[VideoWeb.Common]VideoWeb.Common.*,[VideoWeb]VideoWeb.Security.*,[VideoWeb]VideoWeb.Configuration.*,[VideoWeb]VideoWeb.Pages.*,[VideoWeb.Testing.Common]*,[*]VideoWeb.Swagger.*"
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /s:"${PWD}/vh-video-web-sonar-settings.xml"
+dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /s:"${PWD}/vh-sonar-settings.xml"
 
 dotnet build VideoWeb/VideoWeb.sln -c $configuration
 
