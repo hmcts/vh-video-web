1aeaded2209b58ed8aec47651fe7098a433e00e4
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 13:22:38 +0000

    use new template

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -9,8 +9,6 @@ rm -d -r ${PWD}/VideoWeb/VideoWeb/ClientApp/node_modules
 configuration=Release
 exclusions="[VideoWeb]VideoWeb.ConfigureServicesExtensions,[VideoWeb]VideoWeb.Program,[*]VideoWeb.Extensions.*[VideoWeb]VideoWeb.Startup,[Testing.Common]*,[VideoWeb.Common]VideoWeb.Common.*,[VideoWeb]VideoWeb.Security.*,[VideoWeb]VideoWeb.Configuration.*,[VideoWeb]VideoWeb.Pages.*,[VideoWeb.Testing.Common]*,[*]VideoWeb.Swagger.*"
 
-dotnet sonarscanner begin /k:"${SONAR_PROJECT_KEY}" /o:"${SONAR_ORG}" /version:"${SONAR_PROJECT_VERSION}" /name:"${SONAR_PROJECT_NAME}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.pullrequest.key="${SONAR_PR_KEY}" /d:sonar.pullrequest.branch="${SONAR_PR_BRANCH}" /d:sonar.pullrequest.base="${SONAR_PR_BASE}" /s:"${PWD}/vh-sonar-settings.xml"
-
 dotnet build VideoWeb/VideoWeb.sln -c $configuration
 
 # Script is for docker compose tests where the script is at the root level
@@ -23,10 +21,6 @@ dotnet test VideoWeb/VideoWeb.UnitTests/VideoWeb.UnitTests.csproj -c $configurat
 
 # Run the Jasmine tests
 cd VideoWeb/VideoWeb/ClientApp
-npm install
-npm run lint
-npm run test-once-ci
-
-# Return to the root directory to finish the SonarQube analysis
-cd $OLDPWD
-dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
+npm install --prefix VideoWeb/VideoWeb/ClientApp
+npm run --prefix VideoWeb/VideoWeb/ClientApp lint
+npm run --prefix VideoWeb/VideoWeb/ClientApp test-once-ci
