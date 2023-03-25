707b3d741914d0c9ff4567f7a778d866a84958c6
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-25 15:13:12 +0000

    update karma and jamine reporter in hope to fix report bug

--- scripts/tests/run-tests.sh
+++ scripts/tests/run-tests.sh
@@ -4,7 +4,6 @@ set -x
 rm -d -r ${PWD}/Coverage
 rm -d -r ${PWD}/TestResults
 rm -d -r ${PWD}/VideoWeb/VideoWeb/ClientApp/coverage
-rm -d -r ${PWD}/VideoWeb/VideoWeb/ClientApp/node_modules
 
 configuration=Release
 exclusions="[VideoWeb]VideoWeb.ConfigureServicesExtensions,[VideoWeb]VideoWeb.Program,[*]VideoWeb.Extensions.*[VideoWeb]VideoWeb.Startup,[Testing.Common]*,[VideoWeb.Common]VideoWeb.Common.*,[VideoWeb]VideoWeb.Security.*,[VideoWeb]VideoWeb.Configuration.*,[VideoWeb]VideoWeb.Pages.*,[VideoWeb.Testing.Common]*,[*]VideoWeb.Swagger.*"
