e43c0b89ec43bec41af7e7ef3614a65f20b00d8c
Robert Twyford <robert.twyford@version1.com>
2023-03-31 13:09:31 +0100

    Adding InternalEventController tests for EndpointsUpdated

--- VideoWeb/VideoWeb.UnitTests/Controllers/InternalEventController/EndpointsUpdatedTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/InternalEventController/EndpointsUpdatedTests.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Autofac.Extras.Moq;
+using BookingsApi.Contract.Requests;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
+using VideoApi.Client;
+using VideoApi.Contract.Requests;
 using VideoApi.Contract.Responses;
 using VideoWeb.Common.Caching;
 using VideoWeb.Common.Models;
@@ -84,6 +87,40 @@ namespace VideoWeb.UnitTests.Controllers.InternalEventControllerTests
             result.Should().BeOfType<NoContentResult>();
 
             _mocker.Mock<IEndpointsUpdatedEventNotifier>().Verify(x => x.PushEndpointsUpdatedEvent(mockConference.Object, updateEndpointsRequest), Times.Once);
+        }
+
+        [Test]
+        public async Task Should_not_send_event_if_exception_thrown()
+        {
+            // Arrange
+            _mocker.Mock<IEndpointsUpdatedEventNotifier>().Setup(x =>
+                    x.PushEndpointsUpdatedEvent(mockConference.Object, mockUpdateConferenceEndpointsRequest.Object))
+                .Throws(new VideoApiException("error", StatusCodes.Status500InternalServerError, "", null, null));
+
+            // Act
+            var result = await _controller.EndpointsUpdated(testConferenceId, mockUpdateConferenceEndpointsRequest.Object);
+
+            // Assert
+            result.Should().BeOfType<ObjectResult>();
+            var typedResult = (ObjectResult)result;
+            typedResult.Should().NotBeNull();
+            typedResult.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+
+            _mocker.Mock<IEndpointsUpdatedEventNotifier>().Verify(x => x.PushEndpointsUpdatedEvent(mockConference.Object, mockUpdateConferenceEndpointsRequest.Object), Times.Once);
+        }
+
+        [Test]
+        public async Task Should_call_api_when_cache_is_empty()
+        {
+            _mocker.Mock<IConferenceCache>().Setup(cache => cache.GetOrAddConferenceAsync(mockConference.Object.Id, It.IsAny<Func<Task<ConferenceDetailsResponse>>>()))
+                .Callback(async (Guid anyGuid, Func<Task<ConferenceDetailsResponse>> factory) => await factory())
+                .ReturnsAsync(mockConference.Object);
+
+            var result = await _controller.EndpointsUpdated(testConferenceId, mockUpdateConferenceEndpointsRequest.Object);
+            
+            result.Should().BeOfType<NoContentResult>();
+            _mocker.Mock<IConferenceCache>().Verify(x => x.GetOrAddConferenceAsync(testConferenceId, It.IsAny<Func<Task<ConferenceDetailsResponse>>>()), Times.Once);
+            _mocker.Mock<IVideoApiClient>().Verify(x => x.GetConferenceDetailsByIdAsync(testConferenceId), Times.Once);
         }
     }    
 }
