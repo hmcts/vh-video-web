36163d02fdb1e7e4ce792851ad1f23ce1fafd401
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-06 17:45:10 +0100

    Add more tests

--- VideoWeb/VideoWeb/Services/AppRoleService.cs
+++ VideoWeb/VideoWeb/Services/AppRoleService.cs
@@ -69,11 +69,11 @@ namespace VideoWeb.Services
         {
             CaseAdmin = 1,
             Vho = 2,
-            // Clerk = 3,
+            Clerk = 3,
             Judge = 4,
-            // Individual = 5,
-            // Representative = 6,
-            // JudicialOfficeHolder = 7,
+            Individual = 5,
+            Representative = 6,
+            JudicialOfficeHolder = 7,
             StaffMember = 8,
             VhTeamLead = 9
         }
