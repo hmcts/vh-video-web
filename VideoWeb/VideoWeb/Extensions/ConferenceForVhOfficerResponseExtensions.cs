8dfb35ce9ebf2f6b1886c6552a052114ad6c6293
william.craig <william.craig2@justice.gov.uk>
2023-04-12 16:01:38 +0100

    Consuming excluded venue list from booking api
    Signed-off-by: william.craig <william.craig2@justice.gov.uk>

--- VideoWeb/VideoWeb/Extensions/ConferenceForVhOfficerResponseExtensions.cs
+++ VideoWeb/VideoWeb/Extensions/ConferenceForVhOfficerResponseExtensions.cs
@@ -2,18 +2,12 @@ using System.Collections.Generic;
 using System.Linq;
 using VideoWeb.Contract.Request;
 using VideoWeb.Contract.Responses;
-using VideoWeb.Helpers;
+using BookingsApi.Contract.Helper;
 
 namespace VideoWeb.Extensions;
 
 public static class ConferenceForVhOfficerResponseExtensions
 {
-    private static readonly IReadOnlyList<string> ExcludedHearingVenueNames = new List<string> (HearingScottishVenueNames.ScottishHearingVenuesList)
-    {
-        "Teesside Combined Court Centre",
-        "Teesside Magistrates Court",
-        "Middlesbrough County Court"
-    };
     public static IEnumerable<ConferenceForVhOfficerResponse> ApplyCsoFilter(this IEnumerable<ConferenceForVhOfficerResponse> conferences, VhoConferenceFilterQuery query)
     {
         var isQueryingByCso = query.AllocatedCsoIds.Any() || query.IncludeUnallocated;
@@ -28,7 +22,7 @@ public static class ConferenceForVhOfficerResponseExtensions
         {
             filteredConferences = conferences
                 .Where(r => r.AllocatedCsoId == null &&
-                            ExcludedHearingVenueNames.All(venueName => venueName != r.HearingVenueName));
+                           HearingAllocationExcludedVenueList.ExcludedHearingVenueNames.All(venueName => venueName != r.HearingVenueName));
         }
         else
         {
