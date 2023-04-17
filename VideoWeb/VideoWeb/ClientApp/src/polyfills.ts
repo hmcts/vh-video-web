a2041f1edbde8bb5d7e02495b190b911564dc324
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-17 13:49:26 +0100

    upgrade to ng 13

--- VideoWeb/VideoWeb/ClientApp/src/polyfills.ts
+++ VideoWeb/VideoWeb/ClientApp/src/polyfills.ts
@@ -32,10 +32,7 @@ import 'core-js/es/array';
 import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/weak-map';
-import 'core-js/es/set';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js'; // Run `npm install --save classlist.js`.
+import 'core-js/es/set'; // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
 import 'core-js/es/reflect';
@@ -43,13 +40,6 @@ import 'core-js/es/reflect';
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 // import 'core-js/es7/reflect';
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
