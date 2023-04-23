389b45dd49c079f2276bf75ca90ab8f8823fe76e
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-23 12:49:35 +0100

    update auth in home component to navigate only after isAuthenticated

--- VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/home/home.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { UserDataResult } from 'angular-auth-oidc-client';
-import { switchMap } from 'rxjs/operators';
 import { SecurityServiceProvider } from '../security/authentication/security-provider.service';
-import { Observable, combineLatest } from 'rxjs';
+import { combineLatest } from 'rxjs';
 import { ISecurityService } from '../security/authentication/security-service.interface';
 import { IdpProviders } from '../security/idp-providers';
+import { Router } from '@angular/router';
+import { pageUrls } from '../shared/page-url.constants';
 
 @Component({
     selector: 'app-home',
@@ -14,10 +14,7 @@ export class HomeComponent implements OnInit {
     securityService: ISecurityService;
     currentIdp: IdpProviders;
 
-    userData$: Observable<UserDataResult>;
-    isAuthenticated = false;
-
-    constructor(private securityServiceProviderService: SecurityServiceProvider) {
+    constructor(private securityServiceProviderService: SecurityServiceProvider, private router: Router) {
         combineLatest([
             this.securityServiceProviderService.currentSecurityService$,
             this.securityServiceProviderService.currentIdp$
@@ -28,30 +25,10 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.securityService.checkAuth(undefined, this.currentIdp).subscribe();
-        this.userData$ = this.securityService.checkAuth(undefined, this.currentIdp).pipe(
-            switchMap(({ isAuthenticated }) => {
-                this.isAuthenticated = isAuthenticated;
-                return this.securityService.getUserData(this.currentIdp);
-            })
-        );
-
-        // this.eventService
-        //     .registerForEvents()
-        //     .pipe(
-        //         filter(
-        //             notification =>
-        //                 notification.type === EventTypes.UserDataChanged || notification.type === EventTypes.NewAuthenticationResult
-        //         )
-        //     )
-        //     .subscribe(value => {
-        //         if (!value.value?.isRenewProcess) {
-        //             this.logger.info('[HomeComponent] - First time logging', value);
-        //             this.router.navigate([`/${pageUrls.Navigator}`]);
-        //         }
-        //     });
-        // if (this.route.snapshot.queryParamMap.get('code') === null) {
-        //     this.router.navigate([`/${pageUrls.Login}`]);
-        // }
+        this.securityService.isAuthenticated(this.currentIdp).subscribe(isAuthenticated => {
+            if (isAuthenticated) {
+                this.router.navigate([`/${pageUrls.Navigator}`]);
+            }
+        });
     }
 }
