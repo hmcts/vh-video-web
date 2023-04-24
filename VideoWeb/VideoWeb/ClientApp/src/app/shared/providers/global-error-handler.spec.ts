5ba07e87386b270421b8d61d888eb997fc22ee0f
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-04-24 23:22:44 +0100

    update dockerfile to use node 18.x

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/providers/global-error-handler.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/providers/global-error-handler.spec.ts
@@ -1,26 +1,48 @@
-import { Injector } from '@angular/core';
-import { Logger } from 'src/app/services/logging/logger-base';
+import { Injector, NgZone } from '@angular/core';
 import { GlobalErrorHandler } from './global-error-handler';
+import { Router } from '@angular/router';
+import { LoggerService } from 'src/app/services/logging/logger.service';
+import { pageUrls } from '../page-url.constants';
 
 describe('GlobalErrorHandler', () => {
-    let logger: jasmine.SpyObj<Logger>;
-    let injector: jasmine.SpyObj<Injector>;
+    let ngZoneSpy: jasmine.SpyObj<NgZone>;
+    let routerSpy: jasmine.SpyObj<Router>;
+    let loggerSpy: jasmine.SpyObj<LoggerService>;
+    let injector: Injector;
+    let redirectToSpy: jasmine.Spy;
     let errorHandler: GlobalErrorHandler;
 
-    beforeAll(() => {
-        logger = jasmine.createSpyObj<Logger>('Logger', ['debug', 'info', 'warn', 'event', 'error']);
-        injector = jasmine.createSpyObj<Injector>('Injector', ['get']);
-    });
-
     beforeEach(() => {
-        // tslint:disable-next-line
-        injector.get.and.returnValue(logger);
-        errorHandler = new GlobalErrorHandler(injector);
+        routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+        ngZoneSpy = jasmine.createSpyObj<NgZone>('NgZone', ['run']);
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['error']);
+        ngZoneSpy.run.and.callThrough();
+
+        injector = Injector.create({ providers: [] });
+        spyOn(injector, 'get').and.callFake(token => {
+            if (token === Router) {
+                return routerSpy;
+            } else {
+                return loggerSpy;
+            }
+        });
+
+        errorHandler = new GlobalErrorHandler(injector, ngZoneSpy);
+        redirectToSpy = spyOn(errorHandler, 'redirectTo');
     });
 
-    it('should init the logger from the injector and write error', () => {
-        const error = new Error('failed to find data');
-        errorHandler.handleError(error);
-        expect(logger.error).toHaveBeenCalledWith('Unexpected error', error);
+    it('navigates to unauthorised page when 401 status code is returned', () => {
+        errorHandler.handleError({ status: 401 });
+        expect(redirectToSpy).toHaveBeenCalledWith(routerSpy, pageUrls.Unauthorised);
+    });
+
+    it('navigates to error page when status code is not 401 or 403', () => {
+        errorHandler.handleError({ status: 500 });
+        expect(redirectToSpy).toHaveBeenCalledWith(routerSpy, pageUrls.ServiceError);
+    });
+
+    it('navigates to unauthorised page when 403 status code is returned', () => {
+        errorHandler.handleError({ status: 403 });
+        expect(redirectToSpy).toHaveBeenCalledWith(routerSpy, pageUrls.Unauthorised);
     });
 });
