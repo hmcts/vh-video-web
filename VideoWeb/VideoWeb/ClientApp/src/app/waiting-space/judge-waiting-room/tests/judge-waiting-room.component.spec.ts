0eceb0b095709213bbadea96b4305e6cb9c6a9fb
William Craig <mr.craig1991@gmail.com>
2023-05-12 16:38:36 +0100

    mapping
    Signed-off-by: William Craig <mr.craig1991@gmail.com>

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/tests/judge-waiting-room.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/judge-waiting-room/tests/judge-waiting-room.component.spec.ts
@@ -53,9 +53,7 @@ import { HearingRole } from '../../models/hearing-role-model';
 import { UnloadDetectorService } from 'src/app/services/unload-detector.service';
 import { HearingLayoutService } from 'src/app/services/hearing-layout.service';
 import { createParticipantRemoteMuteStoreServiceSpy } from '../../services/mock-participant-remote-mute-store.service';
-import { HearingVenueFlagsService } from 'src/app/services/hearing-venue-flags.service';
-import { ParticipantUpdated } from '../../models/video-call-models';
-import { ParticipantRemoteMuteStoreService } from '../../services/participant-remote-mute-store.service';
+import {ParticipantDeleted, ParticipantUpdated} from '../../models/video-call-models';
 import { PexipDisplayNameModel } from '../../../services/conference/models/pexip-display-name.model';
 import { WaitingRoomBaseDirective } from '../../waiting-room-shared/waiting-room-base.component';
 
@@ -520,7 +518,7 @@ describe('JudgeWaitingRoomComponent when conference exists', () => {
         });
 
         it('Should display audio alert if wowza listener is deleted', () => {
-            videoCallService.onParticipantDeleted.and.returnValue(of({ uuid: wowzaParticipant.uuid }));
+            videoCallService.onParticipantDeleted.and.returnValue(of(new ParticipantDeleted(wowzaParticipant.uuid)));
             component.conference.audio_recording_required = true;
 
             component.ngOnInit();
