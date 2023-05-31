84775d011384f6b3fef65acbb8949b4180d88d0e
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-06-01 00:35:48 +0100

    Update to latest bookings api client

--- VideoWeb/VideoWeb/Services/AppRoleService.cs
+++ VideoWeb/VideoWeb/Services/AppRoleService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using BookingsApi.Client;
+using BookingsApi.Contract.Requests.Enums;
 using BookingsApi.Contract.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
@@ -57,7 +58,7 @@ namespace VideoWeb.Services
             }
             else
             {
-                claims = MapUserRoleToAppRole(user.UserRoleId);
+                claims = MapUserRoleToAppRole(user.UserRoles);
                 claims.Add(new Claim(ClaimTypes.GivenName, user.FirstName));
                 claims.Add(new Claim(ClaimTypes.Surname, user.Lastname));
                 claims.Add(new Claim(ClaimTypes.Name, user.FullName));
@@ -72,38 +73,28 @@ namespace VideoWeb.Services
             return claims;
         }
         
-        private static List<Claim> MapUserRoleToAppRole(int userUserRoleId)
+        private static List<Claim> MapUserRoleToAppRole(List<JusticeUserRole> userRoles)
         {
-            var appRole = userUserRoleId switch
-            {
-                (int) JusticeUserRole.CaseAdmin => AppRoles.CaseAdminRole,
-                (int) JusticeUserRole.Vho => AppRoles.VhOfficerRole,
-                (int) JusticeUserRole.Judge => AppRoles.JudgeRole,
-                (int) JusticeUserRole.StaffMember => AppRoles.StaffMember,
-                (int) JusticeUserRole.VhTeamLead => AppRoles.VhOfficerRole,
-                _ => null
-            };
-
             var claims = new List<Claim>();
-            if (appRole != null)
+            
+            foreach (JusticeUserRole role in userRoles)
             {
-                claims.Add(new Claim(ClaimTypes.Role, appRole));
+                var appRole = role switch
+                {
+                    JusticeUserRole.CaseAdmin => AppRoles.CaseAdminRole,
+                    JusticeUserRole.Vho => AppRoles.VhOfficerRole,
+                    JusticeUserRole.Judge => AppRoles.JudgeRole,
+                    JusticeUserRole.StaffMember => AppRoles.StaffMember,
+                    JusticeUserRole.VhTeamLead => AppRoles.VhOfficerRole,
+                    _ => null
+                };
+                if (appRole != null)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, appRole));
+                }
             }
             
             return claims;
-        }
-
-        public enum JusticeUserRole
-        {
-            CaseAdmin = 1,
-            Vho = 2,
-            Clerk = 3,
-            Judge = 4,
-            Individual = 5,
-            Representative = 6,
-            JudicialOfficeHolder = 7,
-            StaffMember = 8,
-            VhTeamLead = 9
         }
     }
 }
