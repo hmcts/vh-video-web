6ca857a8cff89a43483ddbf42fac440474e3d198
Oliver Scott <oliver.scott@justice.gov.uk>
2023-06-28 17:10:08 +0100

    Remove error redirects

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/providers/global-error-handler.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/providers/global-error-handler.ts
@@ -1,26 +1,26 @@
 import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { Logger } from 'src/app/services/logging/logger-base';
-import { pageUrls } from '../page-url.constants';
+//import { pageUrls } from '../page-url.constants';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
     constructor(private injector: Injector, private zone: NgZone) {}
 
     handleError(err: any) {
-        const router: Router = this.injector.get(Router);
+        //const router: Router = this.injector.get(Router);
         const logger: Logger = this.injector.get(Logger);
 
         err = this.unboxRejection(err);
         logger.error('Unexpected error', err);
 
-        if (this.isUnauthorized(err)) {
-            logger.error('User is not authorized - 401', err, { url: router.url });
-            this.redirectTo(router, pageUrls.Unauthorised);
-        } else {
-            logger.error('Unhandled error occured', err, { url: router.url });
-            this.redirectTo(router, pageUrls.ServiceError);
-        }
+        // if (this.isUnauthorized(err)) {
+        //     logger.error('User is not authorized - 401', err, { url: router.url });
+        //     this.redirectTo(router, pageUrls.Unauthorised);
+        // } else {
+        //     logger.error('Unhandled error occured', err, { url: router.url });
+        //     this.redirectTo(router, pageUrls.ServiceError);
+        // }
     }
 
     public redirectTo(router: Router, page: string): any {
@@ -33,7 +33,7 @@ export class GlobalErrorHandler implements ErrorHandler {
         return err.rejection || err;
     }
 
-    private isUnauthorized(err) {
-        return err.status && (err.status === 401 || err.status === 403);
-    }
+    // private isUnauthorized(err) {
+    //     return err.status && (err.status === 401 || err.status === 403);
+    // }
 }
