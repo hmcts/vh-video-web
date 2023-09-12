d7dd5b6da7ff9157d3bc6c59a0176e5a6378ae72
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-09-12 12:07:33 +0100

    Add RequestBodyLoggingMiddleware.cs

---
+++ VideoWeb/VideoWeb/Middleware/RequestBodyLoggingMiddleware.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Http;
+
+namespace VideoWeb.Middleware;
+
+public class RequestBodyLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestBodyLoggingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var method = context.Request.Method;
+
+        // Ensure the request body can be read multiple times
+        context.Request.EnableBuffering();
+
+        // Only if we are dealing with POST or PUT, GET and others shouldn't have a body
+        if (context.Request.Body.CanRead && (method == HttpMethods.Post || method == HttpMethods.Put || method == HttpMethods.Patch))
+        {
+            // Leave stream open so next middleware can read it
+            using var reader = new StreamReader(
+                context.Request.Body,
+                Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                bufferSize: 512, leaveOpen: true);
+
+            var requestBody = await reader.ReadToEndAsync();
+
+            // Reset stream position, so next middleware can read it
+            context.Request.Body.Position = 0;
+
+            // Write request body to App Insights
+            var requestTelemetry = context.Features.Get<RequestTelemetry>();                              
+            requestTelemetry?.Properties.Add("RequestBody", requestBody);
+        }
+
+        // Call next middleware in the pipeline
+        await _next(context);
+    }
+}
