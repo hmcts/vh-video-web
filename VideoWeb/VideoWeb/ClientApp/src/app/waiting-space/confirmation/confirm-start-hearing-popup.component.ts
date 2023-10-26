05f24870983956a79796769d51e361442b79e79d
Oliver Scott <oliver.scott@justice.gov.uk>
2023-10-26 17:52:59 +0100

    Update popup

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-start-hearing-popup.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-start-hearing-popup.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { YesNoPopupBaseDirective } from './yes-no-popup-base.component';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
     selector: 'app-confirm-start-hearing-popup',
@@ -10,13 +11,37 @@ import { YesNoPopupBaseDirective } from './yes-no-popup-base.component';
 export class ConfirmStartHearingPopupComponent extends YesNoPopupBaseDirective {
     @Input() hearingStarted = false;
 
-    constructor(protected translateService: TranslateService) {
+    readonly START_AUDIO_MUTED_KEY = 'vh.start-audio-muted';
+
+    form = this.formBuilder.group({
+        muteMicrophone: new FormControl(false)
+    });
+
+    constructor(protected translateService: TranslateService, private formBuilder: FormBuilder) {
         super();
+
+        this.form.reset({
+            muteMicrophone: this.startAudioMuted
+        });
     }
 
     get action(): string {
         return this.hearingStarted
             ? this.translateService.instant('confirm-start-hearing-popup.resume')
             : this.translateService.instant('confirm-start-hearing-popup.start');
+    }
+
+    private get startAudioMuted(): boolean {
+        return localStorage.getItem(this.START_AUDIO_MUTED_KEY) === 'true';
+    }
+
+    private set startAudioMuted(value: boolean) {
+        localStorage.setItem(this.START_AUDIO_MUTED_KEY, value.toString());
+    }
+
+    respondWithYes(): void {
+        this.startAudioMuted = this.form.value.muteMicrophone;
+
+        super.respondWithYes();
     }
 }
