9cd8c678b92015a03059464841d073eaa57ce263
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-11-03 09:51:20 +0000

    update test fixture setup

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-close-hearing-popup.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/confirmation/confirm-close-hearing-popup.component.spec.ts
@@ -1,24 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ConfirmCloseHearingPopupComponent } from './confirm-close-hearing-popup.component';
 
 describe('ConfirmCloseHearingPopupComponent', () => {
     let component: ConfirmCloseHearingPopupComponent;
-    let fixture: ComponentFixture<ConfirmCloseHearingPopupComponent>;
-
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [ConfirmCloseHearingPopupComponent]
-        }).compileComponents();
-    });
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(ConfirmCloseHearingPopupComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
-    it('should create', () => {
-        expect(component).toBeTruthy();
+        component = new ConfirmCloseHearingPopupComponent();
     });
 
     it('should set modalDivId to confirmationDialog', () => {
