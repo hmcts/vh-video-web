ab7eb5b01c48be0fd417fe24b5cdf92879fd2038
Oliver Scott <oliver.scott@justice.gov.uk>
2023-11-10 08:50:55 +0000

    Change cutoff to 3 days

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/helpers/conference-setting-helper.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/helpers/conference-setting-helper.ts
@@ -3,11 +3,11 @@ import { ConferenceSetting } from '../models/conference-setting';
 export class ConferenceSettingHelper {
     static isExpired(conferenceSetting: ConferenceSetting): boolean {
         const today = new Date();
-        const yesterday = new Date(today);
-        yesterday.setDate(today.getDate() - 1);
-        yesterday.setHours(0, 0, 0, 0);
+        const cutoff = new Date(today);
+        cutoff.setDate(today.getDate() - 3);
+        cutoff.setHours(0, 0, 0, 0);
         const createdDate = new Date(conferenceSetting.createdDate);
 
-        return createdDate <= yesterday;
+        return createdDate <= cutoff;
     }
 }
