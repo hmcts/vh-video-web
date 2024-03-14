f65719d95218e38c6375879ad323c874bb9c1bda
William Craig <mr.craig1991@gmail.com>
2024-03-14 13:42:27 +0000

    initial changes to bring inline with gds
    Signed-off-by: William Craig <mr.craig1991@gmail.com>

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/header/header.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/header/header.component.ts
@@ -8,7 +8,7 @@ import { topMenuItems } from './topMenuItems';
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
     @Input() loggedIn: boolean;
