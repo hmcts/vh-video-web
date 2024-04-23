b1b3e5786ba400f5ba6843f298bea83995e72570
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-04-23 21:32:19 +0100

    create idp specific guards

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/guards/already-authenticated-quick-link.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, tap, map, timeout } from 'rxjs/operators';
+import { pageUrls } from 'src/app/shared/page-url.constants';
+import { SecurityServiceProvider } from '../authentication/security-provider.service';
+import { IdpProviders } from '../idp-providers';
+
+@Injectable({ providedIn: 'root' })
+export class AlreadyAuthenticatedQuickLinkGuard {
+    currentIdp: IdpProviders = IdpProviders.quickLink;
+    constructor(private securityServiceProvider: SecurityServiceProvider, private router: Router) {}
+
+    canActivate(): Observable<boolean> {
+        return this.securityServiceProvider
+            .getSecurityService(this.currentIdp)
+            .isAuthenticated(this.currentIdp)
+            .pipe(
+                timeout(30000),
+                take(1),
+                tap(authenticated => {
+                    if (authenticated) {
+                        this.router.navigate([pageUrls.Home]);
+                    }
+                }),
+                map(authenticated => !authenticated)
+            );
+    }
+}
