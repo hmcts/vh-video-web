8377c5f14d15c02f27e740df6e152a0631057186
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2024-04-25 14:02:05 +0100

    VIH-10636 create idp specific guards (#2134)

--- VideoWeb/VideoWeb/ClientApp/src/app/security/security-routing.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/security-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { pageUrls } from '../shared/page-url.constants';
-import { AlreadyAuthenticatedGuard } from './guards/already-authenticated.guard';
 import { EjudSignInComponent } from './idp-selection/ejud-sign-in.component';
 import { IdpSelectionComponent } from './idp-selection/idp-selection.component';
 import { VhSignInComponent } from './idp-selection/vh-sign-in.component';
@@ -9,13 +8,16 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { UnauthorisedComponent } from './unauthorised/unauthorised.component';
 import { Dom1SignInComponent } from './idp-selection/dom1-sign-in.component';
+import { AlreadyAuthenticatedDom1Guard } from './guards/already-authenticated-dom1.guard';
+import { AlreadyAuthenticatedEjudGuard } from './guards/already-authenticated-ejud.guard';
+import { AlreadyAuthenticatedVhGuard } from './guards/already-authenticated-vh.guard';
 
 export const routes: Routes = [
     { path: `${pageUrls.Login}`, component: LoginComponent },
     { path: `${pageUrls.Logout}`, component: LogoutComponent },
-    { path: `${pageUrls.EJudSignIn}`, component: EjudSignInComponent, canActivate: [AlreadyAuthenticatedGuard] },
-    { path: `${pageUrls.Dom1SignIn}`, component: Dom1SignInComponent, canActivate: [AlreadyAuthenticatedGuard] },
-    { path: `${pageUrls.VHSignIn}`, component: VhSignInComponent, canActivate: [AlreadyAuthenticatedGuard] },
+    { path: `${pageUrls.EJudSignIn}`, component: EjudSignInComponent, canActivate: [AlreadyAuthenticatedEjudGuard] },
+    { path: `${pageUrls.Dom1SignIn}`, component: Dom1SignInComponent, canActivate: [AlreadyAuthenticatedDom1Guard] },
+    { path: `${pageUrls.VHSignIn}`, component: VhSignInComponent, canActivate: [AlreadyAuthenticatedVhGuard] },
     { path: `${pageUrls.IdpSelection}`, component: IdpSelectionComponent },
     { path: `${pageUrls.Unauthorised}`, component: UnauthorisedComponent }
 ];
