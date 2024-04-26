6fd1a8bf5200f9d7a6fa46bb91bbbadcca053391
William Craig <mr.craig1991@gmail.com>
2024-04-26 14:48:32 +0100

    Updated template vars

--- VideoWeb/VideoWeb.Common/AppRoleService.cs
+++ VideoWeb/VideoWeb.Common/AppRoleService.cs
@@ -1,11 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using BookingsApi.Client;
 using BookingsApi.Contract.V1.Requests.Enums;
-using BookingsApi.Contract.V2.Responses;
+using BookingsApi.Contract.V1.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using VideoWeb.Common.Caching;
@@ -39,47 +38,11 @@ namespace VideoWeb.Common
             {
                 return claims;
             }
-            claims = new List<Claim>();
+
+            JusticeUserResponse user = null;
             try
             {
-                var hearings = await _bookingsApiClient!.GetConfirmedHearingsByUsernameForToday2Async(username);
-                   
-                var participant = hearings.SelectMany(x => x.Participants)
-                    .FirstOrDefault(x => x.Username.Equals(username, StringComparison.InvariantCultureIgnoreCase));
-                
-                var judiaryParticipant = hearings.SelectMany(x => x.JudiciaryParticipants)
-                    .FirstOrDefault(x => x.Email.Equals(username, StringComparison.InvariantCultureIgnoreCase));
-
-                if (judiaryParticipant != null)
-                {
-                    var appRole = judiaryParticipant.HearingRoleCode == JudiciaryParticipantHearingRoleCode.Judge
-                        ? AppRoles.JudgeRole 
-                        : AppRoles.JudicialOfficeHolderRole;
-                    claims.Add(new Claim(ClaimTypes.Role, appRole));
-                    claims.Add(new Claim(ClaimTypes.GivenName, judiaryParticipant.FirstName));
-                    claims.Add(new Claim(ClaimTypes.Surname, judiaryParticipant.LastName));
-                    await _userClaimscache.SetAsync(username, claims);
-                    return claims;
-                }
-                
-                if (participant != null)
-                {
-                    var appRole = GetRoleFromParticipant(participant);
-                    if (appRole != null) claims.Add(new Claim(ClaimTypes.Role, appRole));
-                    claims.Add(new Claim(ClaimTypes.GivenName, participant.FirstName));
-                    claims.Add(new Claim(ClaimTypes.Surname, participant.LastName));
-                    await _userClaimscache.SetAsync(username, claims);
-                    return claims;
-                }
-                
-                var getJusticeUserTask = _bookingsApiClient!.GetJusticeUserByUsernameAsync(username);
-                var user = await getJusticeUserTask;
-                claims = MapUserRoleToAppRole(user.UserRoles);
-                claims.Add(new Claim(ClaimTypes.GivenName, user.FirstName));
-                claims.Add(new Claim(ClaimTypes.Surname, user.Lastname));
-                await _userClaimscache.SetAsync(username, claims);
-                return claims;
-                
+                user = await _bookingsApiClient!.GetJusticeUserByUsernameAsync(username);
             }
             catch (BookingsApiException ex)
             {
@@ -90,33 +53,27 @@ namespace VideoWeb.Common
                 }
             }
 
-            return claims;
-        }
-
-        private static string GetRoleFromParticipant(ParticipantResponseV2 participant)
-        {
-            var userRole = participant.UserRoleName;
-            var appRole = userRole.ToLower() switch
+            if (user == null)
             {
-                "case admin" => AppRoles.CaseAdminRole,
-                "Video hearings officer" => AppRoles.VhOfficerRole,
-                "hearing facilitation support (ctrt clerk)" => AppRoles.VhOfficerRole,
-                "video hearings team lead" => AppRoles.VhOfficerRole,
-                "judge" => AppRoles.JudgeRole,
-                "individual" => AppRoles.CitizenRole,
-                "representative" => AppRoles.RepresentativeRole,
-                "staff member" => AppRoles.StaffMember,
-                "judicial office holder" => AppRoles.JudicialOfficeHolderRole,
-                _ => null
-            };
-            return appRole;
+                claims = new List<Claim>();
+            }
+            else
+            {
+                claims = MapUserRoleToAppRole(user.UserRoles);
+                claims.Add(new Claim(ClaimTypes.GivenName, user.FirstName));
+                claims.Add(new Claim(ClaimTypes.Surname, user.Lastname));
+                claims.Add(new Claim(ClaimTypes.Name, user.FullName));
+            }
+
+            await _userClaimscache.SetAsync(username, claims);
+            return claims;
         }
 
         public async Task ClearUserCache(string username)
         {
             await _userClaimscache.ClearFromCache(username);
         }
-        
+
         private static List<Claim> MapUserRoleToAppRole(List<JusticeUserRole> userRoles)
         {
             var claims = new List<Claim>();
