3b90087f8f909be444f54770f01d2f49ccbba223
Oliver Scott <oliver.scott@justice.gov.uk>
2024-05-22 14:56:21 +0100

    Fix for impersonating participants

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-space-routing.module.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-space-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConferenceGuard } from '../security/conference.guard';
 import { JudgeGuard } from '../security/judge.guard';
 import { JudicialOfficeHolderGuard } from '../security/judicial-office-holder.guard';
+import { ParticipantGuard } from '../security/participant.guard';
 import { ParticipantWaitingRoomGuard } from '../security/participant-waiting-room.guard';
 import { StaffMemberGuard } from '../security/staff-member.guard';
 import { BackLinkDetails } from '../shared/models/back-link-details';
@@ -17,7 +18,7 @@ const routes: Routes = [
     {
         path: `${pageUrls.ParticipantWaitingRoom}/:conferenceId`,
         component: ParticipantWaitingRoomComponent,
-        canActivate: [ParticipantWaitingRoomGuard],
+        canActivate: [ParticipantGuard, ParticipantWaitingRoomGuard],
         resolve: { loggedUser: LoggedUserResolveService },
         data: { title: 'Waiting room', backLink: new BackLinkDetails(returnToVideoHearingListText, pageUrls.ParticipantHearingList) }
     },
