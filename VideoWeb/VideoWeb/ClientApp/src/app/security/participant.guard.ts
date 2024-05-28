cfb5370d2675f9c39d0426582862f0471e76178a
Oliver Scott <oliver.scott@justice.gov.uk>
2024-05-28 16:33:28 +0100

    VIH-8648 Fix for impersonating roles in waiting room by changing url (#2144)
    * Fix issues impersonating judge or joh
    
    * Use logger prefix
    
    * Refactor
    
    * Fix for impersonating participants

--- VideoWeb/VideoWeb/ClientApp/src/app/security/participant.guard.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/security/participant.guard.ts
@@ -1,56 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
-import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
 import { ProfileService } from '../services/api/profile.service';
 import { Role } from '../services/clients/api-client';
-import { Logger } from '../services/logging/logger-base';
-import { AuthBaseGuard } from './auth-base.guard';
-import { SecurityServiceProvider } from './authentication/security-provider.service';
 import { LaunchDarklyService } from '../services/launch-darkly.service';
+import { Logger } from '../services/logging/logger-base';
+import { SecurityServiceProvider } from './authentication/security-provider.service';
+import { RoleGuard } from './role-guard';
 
 @Injectable({
     providedIn: 'root'
 })
-export class ParticipantGuard extends AuthBaseGuard {
-    constructor(
-        protected ldService: LaunchDarklyService,
-        securityServiceProviderService: SecurityServiceProvider,
-        protected userProfileService: ProfileService,
-        protected router: Router,
-        protected logger: Logger
-    ) {
-        super(securityServiceProviderService, router, logger, ldService);
-    }
+export class ParticipantGuard extends RoleGuard {
+    protected roles = [Role.Representative, Role.Individual, Role.QuickLinkParticipant, Role.QuickLinkObserver];
+    protected loggerPrefix = '[ParticipantGuard]';
 
-    async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-        return this.isUserAuthorized(next, state)
-            .pipe(take(1))
-            .toPromise()
-            .then(async (auth: boolean) => {
-                if (!auth) {
-                    this.router.navigate(['/login']);
-                    return false;
-                }
-                try {
-                    const profile = await this.userProfileService.getUserProfile();
-                    if (
-                        profile.roles.includes(Role.Representative) ||
-                        profile.roles.includes(Role.Individual) ||
-                        profile.roles.includes(Role.QuickLinkParticipant) ||
-                        profile.roles.includes(Role.QuickLinkObserver)
-                    ) {
-                        this.logger.debug('[ParticipantGuard] User is a representative or individual.');
-                        return true;
-                    } else {
-                        this.logger.debug('[ParticipantGuard] User is not a representative or individual. Going home.');
-                        this.router.navigate(['/home']);
-                        return false;
-                    }
-                } catch (err) {
-                    this.logger.error('[ParticipantGuard] Failed to get user profile. Logging out.', err);
-                    this.router.navigate(['/logout']);
-                    return false;
-                }
-            });
+    constructor(
+        securityServiceProviderService: SecurityServiceProvider,
+        userProfileService: ProfileService,
+        router: Router,
+        logger: Logger,
+        ldService: LaunchDarklyService
+    ) {
+        super(securityServiceProviderService, userProfileService, router, logger, ldService);
     }
 }
