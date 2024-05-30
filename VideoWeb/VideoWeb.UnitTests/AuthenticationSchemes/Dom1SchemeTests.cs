3f13badbffb58725d794a2d1bef4fc1873a1983f
Oliver Scott <oliver.scott@justice.gov.uk>
2024-05-30 12:56:09 +0100

    Auth scheme fixes

--- VideoWeb/VideoWeb.UnitTests/AuthenticationSchemes/Dom1SchemeTests.cs
+++ VideoWeb/VideoWeb.UnitTests/AuthenticationSchemes/Dom1SchemeTests.cs
@@ -1,10 +1,21 @@
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Autofac.Extras.Moq;
 using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Moq;
 using NUnit.Framework;
 using VideoWeb.AuthenticationSchemes;
+using VideoWeb.Common;
 using VideoWeb.Common.Configuration;
+using VideoWeb.Common.Models;
+using VideoWeb.UnitTests.Builders;
 
 namespace VideoWeb.UnitTests.AuthenticationSchemes;
 
@@ -12,6 +23,7 @@ public class Dom1SchemeTests
 {
     private Dom1Scheme _sut;
     private Dom1AdConfiguration _dom1Config;
+    private AutoMock _mocker;
 
     [SetUp]
     public void Setup()
@@ -25,6 +37,10 @@ public class Dom1SchemeTests
             TenantId = "Dom1Tenant"
         };
         _sut = new Dom1Scheme("eventHubPath", _dom1Config);
+        _mocker = AutoMock.GetLoose();
+        _mocker.Mock<IServiceProvider>()
+            .Setup(x => x.GetService(typeof(IAppRoleService)))
+            .Returns(_mocker.Mock<IAppRoleService>().Object);
     }
         
     [Test]
@@ -110,5 +126,70 @@ public class Dom1SchemeTests
 
         // Assert
         belongs.Should().BeTrue();
+    }
+    
+    [Test]
+    public async Task ShouldAddClaimsFromAppRoleService_WhenSecurityContextIs_JwtSecurityToken()
+    {
+        // arrange
+        var claimsPrincipal = new ClaimsPrincipalBuilder().Build();
+        var userClaimsPrincipal = new ClaimsPrincipalBuilder().Build();
+        var httpContext = new DefaultHttpContext()
+        {
+            User = userClaimsPrincipal,
+            RequestServices = _mocker.Mock<IServiceProvider>().Object
+        };
+        var options = new JwtBearerOptions();
+        _sut.SetJwtBearerOptions(options);
+        var tokenValidatedContext = new TokenValidatedContext(httpContext, new AuthenticationScheme("name", "displayName", typeof(AuthenticationHandler<JwtBearerOptions>)), options)
+        {
+            Principal = claimsPrincipal,
+            SecurityToken = new JwtSecurityToken(issuer: "Issuer", claims: claimsPrincipal.Claims)
+        };
+        
+        _mocker.Mock<IAppRoleService>().Setup(x => x.GetClaimsForUserAsync(It.IsAny<string>()))
+            .ReturnsAsync(new List<Claim>(){ new(ClaimTypes.Role, AppRoles.StaffMember) });
+        
+        // act
+        await _sut.GetClaimsPostTokenValidation(tokenValidatedContext, options);
+        // assert
+        claimsPrincipal.IsInRole(AppRoles.StaffMember).Should().BeTrue();
+    }
+
+    [Test]
+    public async Task ShouldAddClaimsFromAppRoleService_WhenSecurityContextIs_JsonWebToken()
+    {
+        // arrange
+        var claimsPrincipal = new ClaimsPrincipalBuilder().Build();
+        var userClaimsPrincipal = new ClaimsPrincipalBuilder().Build();
+        var httpContext = new DefaultHttpContext()
+        {
+            User = userClaimsPrincipal,
+            RequestServices = _mocker.Mock<IServiceProvider>().Object
+        };
+        var options = new JwtBearerOptions();
+        _sut.SetJwtBearerOptions(options);
+        var jwtSecurityToken = new JwtSecurityToken(issuer: "Issuer", claims: claimsPrincipal.Claims);
+        // convert to JsonWebToken
+        var handler = new JwtSecurityTokenHandler();
+        // Write the JwtSecurityToken to a string
+        var jwtString = handler.WriteToken(jwtSecurityToken);
+
+        // Create a new JsonWebToken from the string
+        var jsonWebToken = new JsonWebToken(jwtString);
+        var tokenValidatedContext = new TokenValidatedContext(httpContext, new AuthenticationScheme("name", "displayName", typeof(AuthenticationHandler<JwtBearerOptions>)), options)
+        {
+            Principal = claimsPrincipal,
+            SecurityToken = jsonWebToken
+        };
+
+        _mocker.Mock<IAppRoleService>().Setup(x => x.GetClaimsForUserAsync(It.IsAny<string>()))
+            .ReturnsAsync(new List<Claim>(){ new(ClaimTypes.Role, AppRoles.StaffMember) });
+
+        // act
+        await _sut.GetClaimsPostTokenValidation(tokenValidatedContext, options);
+
+        // assert
+        claimsPrincipal.IsInRole(AppRoles.StaffMember).Should().BeTrue();
     }
 }
