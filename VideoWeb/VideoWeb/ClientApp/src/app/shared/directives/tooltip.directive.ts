5503806c095b69bb83b69506eb12ff54c79e226f
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-06-19 14:03:50 +0100

    make tooltip above for participant panel

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/directives/tooltip.directive.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/directives/tooltip.directive.ts
@@ -42,6 +42,8 @@ export class TooltipDirective implements OnDestroy {
         this._isDesktopOnly = value;
     }
 
+    @Input() above: boolean = false;
+
     @HostListener('focus', ['$event']) onKeyDown($event: FocusEvent) {
         if (!this.tooltipKeyTab) {
             this.createTooltipKeyEvent($event);
@@ -112,10 +114,11 @@ export class TooltipDirective implements OnDestroy {
         const x = $event.clientX;
         const y = $event.clientY;
         const scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+        const defaultPosition = this.above ? -40 : 0;
         if (y < window.innerHeight / 2) {
-            this.tooltip.style.top = y + scrollPos + 'px';
+            this.tooltip.style.top = defaultPosition + y + scrollPos + 'px';
         } else {
-            this.tooltip.style.top = y + scrollPos - this.tooltip.clientHeight + 'px';
+            this.tooltip.style.top = defaultPosition + y + scrollPos - this.tooltip.clientHeight + 'px';
         }
         if (x < window.innerWidth / 2) {
             this.tooltip.style.left = x + 15 + 'px';
