b398f4807c34a7dba0d0a5a6e46b2a71452ae151
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-06-27 16:29:48 +0100

    housekeeping

--- VideoWeb/VideoWeb/ClientApp/src/main.ts
+++ VideoWeb/VideoWeb/ClientApp/src/main.ts
@@ -12,37 +12,6 @@ export function getBaseUrl() {
 
 const providers = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
-// TODO: reminder to remove this before merge
-
-// navigator.mediaDevices.getUserMedia = () => {
-//     console.warn('using shaed mock - get user media');
-//     return Promise.resolve(new MediaStream());
-// };
-// navigator.mediaDevices.enumerateDevices = () => {
-//     console.warn('using shaed mock - enumerate devices');
-//     return Promise.resolve([]);
-// };
-
-const originalGetUserMedia = navigator.mediaDevices.getUserMedia.bind(navigator.mediaDevices);
-const originalEnumerateDevices = navigator.mediaDevices.enumerateDevices.bind(navigator.mediaDevices);
-
-navigator.mediaDevices.getUserMedia = constraints => {
-    console.warn('using shaed mock - get user media');
-    return originalGetUserMedia(constraints).then(stream => {
-        // Get all video tracks from the stream
-        const videoTracks = stream.getVideoTracks();
-        // Remove each video track from the stream
-        videoTracks.forEach(track => stream.removeTrack(track));
-        // Return the modified stream without video tracks
-        return stream;
-    });
-};
-
-navigator.mediaDevices.enumerateDevices = () => {
-    console.warn('using shaed mock - enumerate devices');
-    return originalEnumerateDevices().then(devices => devices.filter(device => device.kind !== 'videoinput'));
-};
-
 if (environment.production) {
     enableProdMode();
 }
