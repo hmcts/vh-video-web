6e641ee82bf9e3264a6e579c50a28c843ad117e6
William Craig <mr.craig1991@gmail.com>
2024-08-15 16:37:23 +0100

    linting

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/venue-list/staff-member-venue-list/staff-member-venue-list.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/venue-list/staff-member-venue-list/staff-member-venue-list.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import {VideoWebService} from 'src/app/services/api/video-web.service';
-import {Logger} from 'src/app/services/logging/logger-base';
-import {VhoQueryService} from 'src/app/vh-officer/services/vho-query-service.service';
-import {pageUrls} from '../../page-url.constants';
-import {VenueListComponentDirective} from '../venue-list.component';
-import {ProfileService} from 'src/app/services/api/profile.service';
-import {LaunchDarklyService} from '../../../services/launch-darkly.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { VideoWebService } from 'src/app/services/api/video-web.service';
+import { Logger } from 'src/app/services/logging/logger-base';
+import { VhoQueryService } from 'src/app/vh-officer/services/vho-query-service.service';
+import { pageUrls } from '../../page-url.constants';
+import { VenueListComponentDirective } from '../venue-list.component';
+import { ProfileService } from 'src/app/services/api/profile.service';
+import { LaunchDarklyService } from '../../../services/launch-darkly.service';
 
 @Component({
     selector: 'app-staff-member-venue-list',
