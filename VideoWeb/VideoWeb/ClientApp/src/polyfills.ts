4a79146bd5342b146022ce88786d57d0e5e07491
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-08-20 18:32:22 +0100

    VIH-10898 update GDS import and remove ie polyfills

--- VideoWeb/VideoWeb/ClientApp/src/polyfills.ts
+++ VideoWeb/VideoWeb/ClientApp/src/polyfills.ts
@@ -18,25 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set'; // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es/reflect';
-
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 // import 'core-js/es7/reflect';
