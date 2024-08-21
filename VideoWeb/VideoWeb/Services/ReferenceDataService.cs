9c5c78441f89faf63fda598f930428455518d3d9
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-08-21 14:01:34 +0100

    VIH-10835 cache static data on start up

--- VideoWeb/VideoWeb/Services/ReferenceDataService.cs
+++ VideoWeb/VideoWeb/Services/ReferenceDataService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using BookingsApi.Client;
+using BookingsApi.Contract.V1.Responses;
 using Microsoft.Extensions.Caching.Memory;
 using VideoWeb.Common.Caching;
 using VideoWeb.Common.Models;
@@ -12,13 +13,21 @@ namespace VideoWeb.Services;
 
 public interface IReferenceDataService
 {
+    Task InitialiseCache();
     Task<List<InterpreterLanguage>> GetInterpreterLanguagesAsync(CancellationToken cancellationToken = default);
+    Task<List<HearingVenueResponse>> GetHearingVenuesForTodayAsync(CancellationToken cancellationToken = default);
+    
 }
 
 public class ReferenceDataService(IBookingsApiClient bookingsApiClient, IMemoryCache memoryCache): IReferenceDataService
 {
     private readonly string _interpreterLanguagesKey = "RefData_InterpreterLanguages";
-    
+
+    public async Task InitialiseCache()
+    {
+        await GetInterpreterLanguagesAsync();
+        await GetHearingVenuesForTodayAsync();
+    }
     public async Task<List<InterpreterLanguage>> GetInterpreterLanguagesAsync(CancellationToken cancellationToken = default)
     {
         return await memoryCache.GetOrCreateAsync(_interpreterLanguagesKey, async entry =>
@@ -27,6 +36,16 @@ public class ReferenceDataService(IBookingsApiClient bookingsApiClient, IMemoryC
             var interpreterLanguages = await bookingsApiClient.GetAvailableInterpreterLanguagesAsync(cancellationToken);
             var languages = interpreterLanguages.Select(x=> x.Map()).ToList();
             return languages;
+        });
+    }
+
+    public async Task<List<HearingVenueResponse>> GetHearingVenuesForTodayAsync(CancellationToken cancellationToken = default)
+    {
+        return await memoryCache.GetOrCreateAsync("RefData_HearingVenues", async entry =>
+        {
+            entry.AbsoluteExpiration = DateTimeOffset.UtcNow.AddHours(3);
+            var hearingVenues = await bookingsApiClient.GetHearingVenuesForHearingsTodayAsync(cancellationToken);
+            return hearingVenues.ToList();
         });
     }
 }
