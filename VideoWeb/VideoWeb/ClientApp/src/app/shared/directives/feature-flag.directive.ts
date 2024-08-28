fab47cc02f38cc40fe2bef9045ed036ba7400910
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2024-08-28 18:04:05 +0100

    VIH-10916 language change should be behind the feature flag 'interpreter-enhancements' (#2221)

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/directives/feature-flag.directive.ts
@@ -0,0 +1,33 @@
+import { Directive, Input, OnDestroy, TemplateRef, ViewContainerRef } from '@angular/core';
+
+import { Subscription } from 'rxjs';
+import { FEATURE_FLAGS, LaunchDarklyService } from 'src/app/services/launch-darkly.service';
+
+@Directive({
+    selector: '[appFeatureFlag]'
+})
+export class FeatureFlagDirective implements OnDestroy {
+    private subscription: Subscription;
+
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private launchDarklyService: LaunchDarklyService
+    ) {}
+
+    @Input() set appFeatureFlag(flagKey: keyof typeof FEATURE_FLAGS) {
+        this.subscription?.unsubscribe();
+
+        this.subscription = this.launchDarklyService.getFlag<boolean>(flagKey).subscribe(flagValue => {
+            if (flagValue) {
+                this.viewContainer.createEmbeddedView(this.templateRef);
+            } else {
+                this.viewContainer.clear();
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.subscription?.unsubscribe();
+    }
+}
