c9b2ce247c5b2f8e6a4b633d80ff1e27a47c0761
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-09-05 07:45:17 +0100

    lint fix

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/venue-list/venue-list.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/venue-list/venue-list.component.spec.ts
@@ -19,7 +19,7 @@ class MockedVenueListComponent extends VenueListComponentDirective {
         return true;
     }
 
-    goToHearingList() { }
+    goToHearingList() {}
 }
 
 describe('VenueListComponent', () => {
