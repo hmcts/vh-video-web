b769fd87cfa4ffa692d9e7e2613ceb473696799a
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-09-06 17:56:46 +0100

    handle possible nulls

--- VideoWeb/VideoWeb/Mappings/ConferenceForVhOfficerResponseMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ConferenceForVhOfficerResponseMapper.cs
@@ -53,19 +53,19 @@ public static class ConferenceForVhOfficerResponseMapper
         
         var response = new ConferenceForVhOfficerResponse();
         response.Id = conference.Id;
-        response.CaseName = caseInfo.Name;
-        response.CaseNumber = caseInfo.Number;
-        response.CaseType = hearingDetails.ServiceName;
+        response.CaseName = caseInfo?.Name;
+        response.CaseNumber = caseInfo?.Number;
+        response.CaseType = hearingDetails?.ServiceName;
         response.ScheduledDateTime = conference.ScheduledDateTime;
         response.ScheduledDuration = conference.ScheduledDuration;
         response.Status = Enum.Parse<ConferenceStatus>(conference.CurrentStatus.ToString());
-        response.HearingVenueName = hearingDetails.HearingVenueName;
+        response.HearingVenueName = hearingDetails?.HearingVenueName;
         response.Participants = conference.Participants.Select(ParticipantUserResponseMapper.Map).ToList();
         response.StartedDateTime = conference.StartedDateTime;
         response.ClosedDateTime = conference.ClosedDateTime;
         response.TelephoneConferenceId = conference.TelephoneConferenceId;
         response.TelephoneConferenceNumbers = conference.TelephoneConferenceNumbers;
-        response.CreatedDateTime = hearingDetails.CreatedDate;
+        response.CreatedDateTime = hearingDetails?.CreatedDate;
         response.HearingRefId = conference.HearingId;
         response.AllocatedCso = allocatedCso;
         response.AllocatedCsoId = hearingDetails?.AllocatedToId;
