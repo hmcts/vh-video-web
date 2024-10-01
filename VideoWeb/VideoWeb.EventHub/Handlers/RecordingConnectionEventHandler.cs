790bf989edc97630846046a0f301c33d6e43f692
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-10-01 11:10:14 +0100

    VIH-10936 - Handle the callback event (#2242)
    * Create new event handler, Create new eventType enum, Create UnitTest, Added IEventHubClient
    
    * Delete tmp.json
    
    * Update RecordingConnectionEventHandler.cs
    
    * Update RecordingConnectionEventHandlerTests.cs
    
    * Update EventHandlerFactoryTests.cs
    
    * added test
    
    * added asserts
    
    * Update EventComponentHelper.cs
    
    * sonar issue
    
    * re-added event publish
    
    * Update RecordingConnectionEventHandler.cs
    
    * Update values.dev.template.yaml
    
    * update pckg
    
    * Update EventComponentHelper.cs
    
    * merge fix
    
    * remove package
    
    * updated packages

---
+++ VideoWeb/VideoWeb.EventHub/Handlers/RecordingConnectionEventHandler.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using EventType = VideoWeb.EventHub.Enums.EventType;
+
+namespace VideoWeb.EventHub.Handlers
+{
+    public class RecordingConnectionEventHandler(
+        IHubContext<Hub.EventHub, IEventHubClient> hubContext,
+        IConferenceService conferenceService,
+        ILogger<EventHandlerBase> logger)
+        : EventHandlerBase(hubContext, conferenceService, logger)
+    {
+
+        public override EventType EventType => EventType.RecordingConnectionFailed;
+
+        protected override async Task PublishStatusAsync(CallbackEvent callbackEvent)
+        {
+            var conferenceId = SourceConference.Id;
+
+            var participantId = callbackEvent.ParticipantId;
+
+            Logger.LogTrace("Recording Connection Failed: Conference Id: {ConferenceId} - Participant id: {ParticipantId}", conferenceId, participantId);
+
+            await HubContext.Clients.Group(Hub.EventHub.VhOfficersGroupName)
+                .RecordingConnectionFailed(conferenceId, participantId);
+
+        }
+    }
+}
