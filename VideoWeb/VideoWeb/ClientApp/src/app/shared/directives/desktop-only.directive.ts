996d6ed12d850d5fe6c4c34b2eaf8854804f3623
Shaed Parkar <shaed.parkar@slimsolutions.company>
2024-10-14 15:55:56 +0100

    VIH-10893 hide screen share if the device is not a desktop

---
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/directives/desktop-only.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, TemplateRef, ViewContainerRef } from '@angular/core';
+import { DeviceTypeService } from 'src/app/services/device-type.service';
+
+@Directive({
+    selector: '[appDesktopOnly]'
+})
+export class DesktopOnlyDirective {
+    constructor(
+        private templateRef: TemplateRef<any>,
+        private viewContainer: ViewContainerRef,
+        private deviceTypeService: DeviceTypeService
+    ) {
+        if (this.deviceTypeService.isDesktop()) {
+            this.viewContainer.createEmbeddedView(this.templateRef);
+        } else {
+            this.viewContainer.clear();
+        }
+    }
+}
