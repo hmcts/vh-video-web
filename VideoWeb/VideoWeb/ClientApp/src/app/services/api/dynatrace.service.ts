45fa411bea933f0f01024ee0801e91cde31758fb
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-10-15 13:37:33 +0100

    summaries

--- VideoWeb/VideoWeb/ClientApp/src/app/services/api/dynatrace.service.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/services/api/dynatrace.service.ts
@@ -14,12 +14,25 @@ export class DynatraceService {
         this.renderer = rendererFactory.createRenderer(null, null);
     }
 
+    /**
+     * The function `addDynatraceScript` dynamically adds a script element with a specified URL to the
+     * document's head.
+     * @param scriptUrl - The `scriptUrl` parameter is a string that represents the URL of the
+     * Dynatrace script that you want to add dynamically to the HTML document.
+     */
     addDynatraceScript(scriptUrl) {
         const script = this.renderer.createElement('script');
         script.src = scriptUrl;
         this.document.head.appendChild(script);
     }
 
+    /**
+     * The function `addUserIdentifyScript` creates a script element to identify a user with a given
+     * ID.
+     * @param userIdendify - The `userIdendify` parameter is the user identifier that will be passed to
+     * the `dtrum.identifyUser` function in the `addUserIdentifyScript` method. This identifier is used
+     * to uniquely identify a user within the Dynatrace monitoring system.
+     */
     addUserIdentifyScript(userIdendify) {
         const script = this.renderer.createElement('script');
         script.text = "dtrum.identifyUser('" + userIdendify + "')";
