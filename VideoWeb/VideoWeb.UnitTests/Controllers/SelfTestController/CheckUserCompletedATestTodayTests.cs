a89dd8dfd47ffee511fde7e2162440dca0978562
William Craig <mr.craig1991@gmail.com>
2024-10-31 12:01:29 +0000

    Fixed test expected response type

--- VideoWeb/VideoWeb.UnitTests/Controllers/SelfTestController/CheckUserCompletedATestTodayTests.cs
+++ VideoWeb/VideoWeb.UnitTests/Controllers/SelfTestController/CheckUserCompletedATestTodayTests.cs
@@ -50,14 +50,14 @@ public class CheckUserCompletedATestTodayTests
     {
         await _testCallCache.AddTestCompletedForTodayAsync(_username);
         var result = await _controller.CheckUserCompletedATestTodayAsync();
-        result.Should().BeOfType<OkResult>().Which.Should().Be(true);
+        result.Should().BeOfType<OkObjectResult>().Which.Value.Should().Be(true);
     }
 
     [Test]
     public async Task should_return_ok_false_when_user_has_not_completed_a_test_call_today()
     {
         var result = await _controller.CheckUserCompletedATestTodayAsync();
-        result.Should().BeOfType<OkResult>().Which.Should().Be(false);
+        result.Should().BeOfType<OkObjectResult>().Which.Value.Should().Be(false);
     }
 
 }
