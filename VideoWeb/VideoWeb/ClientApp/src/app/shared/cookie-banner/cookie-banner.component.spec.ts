d701fb3becccafcde6310d595f28bea3bb62ebc2
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-12-12 13:37:51 +0000

    Update cookie-banner.component.spec.ts

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/cookie-banner/cookie-banner.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/cookie-banner/cookie-banner.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CookieBannerComponent } from './cookie-banner.component';
+import { cookies } from '../cookies.constants';
+import { TranslatePipeMock } from 'src/app/testing/mocks/mock-translation-pipe';
 
 describe('CookieBannerComponent', () => {
     let component: CookieBannerComponent;
@@ -8,9 +9,11 @@ describe('CookieBannerComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [CookieBannerComponent]
+            declarations: [CookieBannerComponent, TranslatePipeMock]
         }).compileComponents();
+    });
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(CookieBannerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
@@ -18,5 +21,24 @@ describe('CookieBannerComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should show banner if no cookie consent is found', () => {
+        localStorage.removeItem(cookies.cookieConsentKey);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        expect(component.isBannerVisible).toBeTrue();
+    });
+
+    it('should set cookie consent to accepted and hide banner when acceptCookies is called', () => {
+        component.acceptCookies();
+        expect(localStorage.getItem(cookies.cookieConsentKey)).toBe(cookies.cookieAccptedValue);
+        expect(component.isBannerVisible).toBeFalse();
+    });
+
+    it('should set cookie consent to rejected and hide banner when rejectCookies is called', () => {
+        component.rejectCookies();
+        expect(localStorage.getItem(cookies.cookieConsentKey)).toBe(cookies.cookieRejectedValue);
+        expect(component.isBannerVisible).toBeFalse();
     });
 });
