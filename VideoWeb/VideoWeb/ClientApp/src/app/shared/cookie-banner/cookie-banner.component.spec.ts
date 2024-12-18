70c75ae149253a6d5ca7c51c51db8620cb48e848
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-12-18 15:30:38 +0000

    added tests

--- VideoWeb/VideoWeb/ClientApp/src/app/shared/cookie-banner/cookie-banner.component.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/shared/cookie-banner/cookie-banner.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CookieBannerComponent } from './cookie-banner.component';
 import { TranslatePipeMock } from 'src/app/testing/mocks/mock-translation-pipe';
+import { cookies } from '../cookies.constants';
 
 describe('CookieBannerComponent', () => {
     let component: CookieBannerComponent;
@@ -16,9 +17,32 @@ describe('CookieBannerComponent', () => {
         fixture = TestBed.createComponent(CookieBannerComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        localStorage.clear();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should not emit cookieAnswered if cookie consent is found', () => {
+        const spy = spyOn(component.cookieAnswered, 'emit');
+        localStorage.setItem(cookies.cookieConsentKey, cookies.cookieAccptedValue);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+        expect(spy).not.toHaveBeenCalled();
+    });
+
+    it('should set cookie consent to accepted and emit cookieAnswered when acceptCookies is called', () => {
+        const spy = spyOn(component.cookieAnswered, 'emit');
+        component.acceptCookies();
+        expect(localStorage.getItem(cookies.cookieConsentKey)).toBe(cookies.cookieAccptedValue);
+        expect(spy).toHaveBeenCalled();
+    });
+
+    it('should set cookie consent to rejected and emit cookieAnswered when rejectCookies is called', () => {
+        const spy = spyOn(component.cookieAnswered, 'emit');
+        component.rejectCookies();
+        expect(localStorage.getItem(cookies.cookieConsentKey)).toBe(cookies.cookieRejectedValue);
+        expect(spy).toHaveBeenCalled();
     });
 });
