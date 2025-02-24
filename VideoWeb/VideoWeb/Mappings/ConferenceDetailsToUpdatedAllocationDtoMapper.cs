367c603bf1fc22f270188469a83d50465a402e6c
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2025-02-24 14:18:26 +0000

    VIH-11301 add conference object to UpdatedAllocationDto.cs (#2378)

--- VideoWeb/VideoWeb/Mappings/ConferenceDetailsToUpdatedAllocationDtoMapper.cs
+++ VideoWeb/VideoWeb/Mappings/ConferenceDetailsToUpdatedAllocationDtoMapper.cs
@@ -7,7 +7,7 @@ public static class ConferenceDetailsToUpdatedAllocationDtoMapper
 {
     public static UpdatedAllocationDto MapToUpdatedAllocationDto(Conference conference)
     {
-        return new UpdatedAllocationDto(conference.Id, conference.ScheduledDateTime, conference.CaseName,
+        return new UpdatedAllocationDto(conference.Id, ConferenceResponseMapper.Map(conference), conference.ScheduledDateTime, conference.CaseName,
             conference.GetJudge()?.DisplayName, conference.AllocatedCsoUsername, conference.AllocatedCso, conference.AllocatedCsoId);
     }
 }
