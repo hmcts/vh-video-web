3c937bb32c1f52e59381a808accef4960c7c8c1b
Shaed Parkar <shaed.parkar@slimsolutions.tech>
2025-02-24 13:32:45 +0000

    VIH-11301 add conference object to UpdatedAllocationDto.cs
    Signed-off-by: Shaed Parkar <shaed.parkar@slimsolutions.tech>

--- VideoWeb/VideoWeb.EventHub/Models/UpdatedAllocationDto.cs
+++ VideoWeb/VideoWeb.EventHub/Models/UpdatedAllocationDto.cs
@@ -1,9 +1,11 @@
 using System;
+using VideoWeb.Contract.Responses;
 
 namespace VideoWeb.EventHub.Models;
 
 public record UpdatedAllocationDto(
     Guid ConferenceId,
+    ConferenceResponse Conference,
     DateTime ScheduledDateTime,
     string CaseName,
     string JudgeDisplayName,
