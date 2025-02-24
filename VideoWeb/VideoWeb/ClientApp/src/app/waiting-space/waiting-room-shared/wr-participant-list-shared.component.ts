1d9be0e9bfc9ab51f13ea316538b47ba988548f7
marcogagliardi <marco.gagliardi@justice.gov.uk>
2025-02-24 11:59:06 +0000

    VIH-11263 - witness doesn’t have correct hierarchy on host waiting room page (#2374)
    * changed order
    
    * applied same sorting logic from CC to WR
    
    * added helper for sorting
    
    * added test
    
    * Update sorting-helper.spec.ts

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-room-shared/wr-participant-list-shared.component.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/waiting-room-shared/wr-participant-list-shared.component.ts
@@ -13,6 +13,7 @@ import { HearingRole } from '../models/hearing-role-model';
 import { FocusService } from 'src/app/services/focus.service';
 import { VHConference, VHEndpoint, VHParticipant } from '../store/models/vh-conference';
 import { ConsultationRules } from 'src/app/services/models/consultation-rules';
+import { SortingHelper } from 'src/app/shared/helpers/sorting-helper';
 
 @Directive()
 export abstract class WRParticipantStatusListDirective implements OnChanges {
@@ -180,7 +181,7 @@ export abstract class WRParticipantStatusListDirective implements OnChanges {
                     x.role !== Role.QuickLinkParticipant &&
                     x.hearingRole !== HearingRole.STAFF_MEMBER
             )
-            .sort((a, b) => a.role.localeCompare(b.role) || (a.name || a.displayName).localeCompare(b.name || b.displayName));
+            .sort(SortingHelper.orderByRoleThenName);
 
         nonJudgeParts = [
             ...nonJudgeParts,
