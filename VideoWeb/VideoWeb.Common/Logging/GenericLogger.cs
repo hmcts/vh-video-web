0c2bb5fe800f135feac457737111423deeddfdf1
marcogagliardi <marco.gagliardi@justice.gov.uk>
2025-04-17 13:12:19 +0100

    sonar fix

--- VideoWeb/VideoWeb.Common/Logging/GenericLogger.cs
+++ VideoWeb/VideoWeb.Common/Logging/GenericLogger.cs
@@ -24,25 +24,25 @@ namespace VideoWeb.Common.Logging
         public static partial void LogGetVenues(this ILogger logger);
     
         [LoggerMessage(
-            EventId = 3002,
+            EventId = 3003,
             Level = LogLevel.Debug,
             Message = "Starting request")]
         public static partial void LogStartingRequest(this ILogger logger);
 
         [LoggerMessage(
-            EventId = 3003,
+            EventId = 3004,
             Level = LogLevel.Error,
             Message = "An error occurred: {Message}")]
         public static partial void LogRequestError(this ILogger logger, Exception exception, string message);
 
         [LoggerMessage(
-            EventId = 3004,
+            EventId = 3005,
             Level = LogLevel.Debug,
             Message = "Handled request in {ElapsedMilliseconds}ms")]
         public static partial void LogHandledRequest(this ILogger logger, long elapsedMilliseconds);
 
          [LoggerMessage(
-            EventId = 3005,
+            EventId = 3006,
             Level = LogLevel.Debug,
             Message = "GetMessages for {ConferenceId}")]
         public static partial void LogGetMessagesForConference(this ILogger logger, Guid conferenceId);
