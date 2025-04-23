d9e655ade38434e9a25f39a96b620a084884cc3b
William Craig <mr.craig1991@gmail.com>
2025-04-23 15:42:14 +0100

    update moment in tests

--- VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/room-closing-toast.service.spec.ts
+++ VideoWeb/VideoWeb/ClientApp/src/app/waiting-space/services/room-closing-toast.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import * as moment from 'moment';
+import moment from 'moment';
 import { ToastrService, ActiveToast } from 'ngx-toastr';
 import { Subject } from 'rxjs';
 import { ConferenceStatus } from 'src/app/services/clients/api-client';
