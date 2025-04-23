e4ed2e3a353bc7a33b00eac96934550481685aa8
William Craig <mr.craig1991@gmail.com>
2025-04-23 13:20:36 +0100

    cleaned up Program.cs

--- VideoWeb/VideoWeb/Program.cs
+++ VideoWeb/VideoWeb/Program.cs
@@ -3,87 +3,73 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
 using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Extensions.Configuration.KeyPerFile;
 using Microsoft.Extensions.FileProviders;
-using OpenTelemetry.Logs;
 
-namespace VideoWeb
+namespace VideoWeb;
+
+internal static class Program
 {
-    internal static class Program
+    public static void Main(string[] args)
     {
-         public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        CreateWebHostBuilder(args).Build().Run();
+    }
 
-        private static IHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            var keyVaults=new List<string> {
-                "vh-bookings-api",
-                "vh-infra-core",
-                "vh-video-api",
-                "vh-video-web"
-            };
+    private static IHostBuilder CreateWebHostBuilder(string[] args)
+    {
+        var keyVaults=new List<string> {
+            "vh-bookings-api",
+            "vh-infra-core",
+            "vh-video-api",
+            "vh-video-web"
+        };
             
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((configBuilder) =>
-                {
-                    foreach (var keyVault in keyVaults)
-                    {
-                        var filePath = $"/mnt/secrets/{keyVault}";
-                        if (Directory.Exists(filePath))
-                        {
-                            configBuilder.Add(GetKeyPerFileSource(filePath));    
-                        }
-                    }
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
-                    webBuilder.UseIISIntegration();
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureOpenTelemetryLogging();
-                    webBuilder.ConfigureAppConfiguration(configBuilder =>
-                    {
-                        foreach (var keyVault in keyVaults)
-                        {
-                            var filePath = $"/mnt/secrets/{keyVault}";
-                            if (Directory.Exists(filePath))
-                            {
-                                configBuilder.Add(GetKeyPerFileSource(filePath));    
-                            }
-                        }
-                    });
-                });
-        }
-
-        private static void ConfigureOpenTelemetryLogging(this IWebHostBuilder webBuilder)
-        {
-            webBuilder.ConfigureLogging((hostingContext, logging) =>
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(configBuilder =>
             {
-                logging.AddEventSourceLogger();
-                logging.AddOpenTelemetry(options =>
+                foreach (var filePath in keyVaults.Select(keyVault => $"/mnt/secrets/{keyVault}").Where(Directory.Exists))
+                    configBuilder.Add(GetKeyPerFileSource(filePath));
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                webBuilder.UseIISIntegration();
+                webBuilder.UseStartup<Startup>();
+                webBuilder.ConfigureOpenTelemetryLogging();
+                webBuilder.ConfigureAppConfiguration(configBuilder =>
                 {
-                    options.IncludeFormattedMessage = true;
-                    options.ParseStateValues = true;
-                    options.IncludeScopes = true;
-                    options.AddConsoleExporter();
-                    options.AddAzureMonitorLogExporter(o => o.ConnectionString =
-                        hostingContext.Configuration["ApplicationInsights:ConnectionString"]);
+                    foreach (var filePath in keyVaults.Select(keyVault => $"/mnt/secrets/{keyVault}").Where(Directory.Exists))
+                        configBuilder.Add(GetKeyPerFileSource(filePath));
                 });
             });
-        }
+    }
 
-        private static KeyPerFileConfigurationSource GetKeyPerFileSource(string filePath)
+    private static void ConfigureOpenTelemetryLogging(this IWebHostBuilder webBuilder)
+    {
+        webBuilder.ConfigureLogging((hostingContext, logging) =>
         {
-            return new KeyPerFileConfigurationSource
+            logging.AddEventSourceLogger();
+            logging.AddOpenTelemetry(options =>
             {
-                FileProvider = new PhysicalFileProvider(filePath),
-                Optional = true,
-                ReloadOnChange = true,
-                SectionDelimiter = "--" // Set your custom delimiter here
-            };
-        }
+                options.IncludeFormattedMessage = true;
+                options.ParseStateValues = true;
+                options.IncludeScopes = true;
+                options.AddAzureMonitorLogExporter(o => o.ConnectionString =
+                    hostingContext.Configuration["ApplicationInsights:ConnectionString"]);
+            });
+        });
+    }
+
+    private static KeyPerFileConfigurationSource GetKeyPerFileSource(string filePath)
+    {
+        return new KeyPerFileConfigurationSource
+        {
+            FileProvider = new PhysicalFileProvider(filePath),
+            Optional = true,
+            ReloadOnChange = true,
+            SectionDelimiter = "--" // Set your custom delimiter here
+        };
     }
 }
